59cf89aa13da98e6264e79ab58eac18ecbcd6414
Brandon Pierce <brandon.pierce@slalom.com>
2017-02-24 18:40:24 +0000

    Removed - Deprecated module. In next version will be removed from repo. NodeJS ldap-client has intermittent bug that hard-fails server with core-dump.
    git-svn-id: https://svn.bcgsc.ca/svn/seqdev/ipr-api/trunk@127717 ad02020f-2a26-4133-947e-1829317c1416

--- app/libs/ldapAuth.js
+++ app/libs/ldapAuth.js
@@ -1,16 +1,8 @@
 "use strict";
 
-let Q = require('q'),
-    ldap = require('ldap-client');
+let Q = require('q');
 
-let client = new ldap({
-  uri: 'ldap://gsc-ldap.bcgsc.ca:389',
-  validatecert: false,
-  connect: (err) => {
-    if(err) console.log('Connection Error: ', err);
-    console.log('Connected to LDAP');
-  }
-});
+
 
 module.exports = {
 
@@ -24,26 +16,6 @@ module.exports = {
   authenticate: (user, pass) => {
 
     let deferred = Q.defer();
-
-    try {
-      // Attempt LDAP bind
-      client.bind({
-        binddn: 'uid=' + user + ',ou=Webusers,dc=bcgsc,dc=ca',
-        password: pass
-      }, (err) => {
-
-        if (err) {
-          deferred.reject({status: false, message: 'invalid credentials'});
-        }
-        if (!err) {
-          console.log('Successful LDAP auth');
-          deferred.resolve({status: true, message: 'success'});
-        }
-      });
-    } catch(error) {
-      console.log('Unable to attempt LDAP auth');
-      deferred.reject({status: false, message: 'unable to authenticate'});
-    }
 
     return deferred.promise;
   }
