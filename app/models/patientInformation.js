c84cc55df6834b73ef6a6458934ab35692259989
Brandon Pierce <brandon.pierce@slalom.com>
2017-04-26 17:05:07 -0700

    New - Moved patient information into higher namespace
    #UGNE-1533

---
+++ app/models/patientInformation.js
@@ -0,0 +1,74 @@
+"use strict";
+
+module.exports = (sequelize, Sq) => {
+  return sequelize.define('patientInformation', {
+    id: {
+      type: Sq.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    ident: {
+      type: Sq.UUID,
+      unique: false,
+      defaultValue: Sq.UUIDV4
+    },
+    dataVersion: {
+      type: Sq.INTEGER,
+      defaultValue: 0,
+    },
+    pog_id: {
+      type: Sq.INTEGER,
+      references: {
+        model: 'POGs',
+        key: 'id',
+      }
+    },
+    physician: {
+      type: Sq.STRING,
+      allowNull: false,
+    },
+    gender: {
+      type: Sq.STRING,
+      allowNull: true,
+    },
+    age: {
+      type: Sq.STRING,
+    },
+    POGID: {
+      type: Sq.STRING,
+    },
+    caseType: {
+      type: Sq.STRING,
+      allowNull: false
+    },
+    tumourType: {
+      type: Sq.STRING,
+    },
+    reportDate: {
+      type: Sq.STRING,
+    },
+    biopsySite: {
+      type: Sq.STRING,
+    },
+    tumourSample: {
+      type: Sq.STRING,
+    },
+    tumourProtocol: {
+      type: Sq.STRING,
+    },
+    constitutionalSample: {
+      type: Sq.STRING
+    },
+    constitutionalProtocol: {
+      type: Sq.STRING
+    }
+  }, {
+    // Table Name
+    tableName: 'pog_patient_information',
+    // Automatically create createdAt, updatedAt, deletedAt
+    timestamps: true,
+    // Use soft-deletes!
+    paranoid: true,
+  });
+};
+
