ec3e82239e850aedbaf0c7320f255e15359a309a
Brandon Pierce <brandon.pierce@slalom.com>
2017-05-09 13:12:01 -0700

    New - Token age fix
    Better checking for expired tokens, and auto refreshing newly stale tokens.

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -2,6 +2,7 @@
 
 let _ = require('lodash'),
     router = require('express').Router({mergeParams: true}),
+    moment = require('moment'),
     db = require(process.cwd() + '/app/models');
 
 let ignored = {
@@ -11,7 +12,7 @@ let ignored = {
 
 // Require Active Session Middleware
 module.exports = (req,res,next) => {
-  
+
   // Get Authorization Header
   let token = req.header('Authorization');
   
@@ -20,46 +21,44 @@ module.exports = (req,res,next) => {
     req.user = 1;
     return next();
   }
-  // Send Admin Account
-  if(token === "apitest0-e4af-437c-be95-1e3451d0c619" && process.env.NODE_ENV === 'development') {
-    db.models.user.findOne({where: {username: 'admin'}, attributes: {exclude: ['id', 'password', 'deletedAt']}}).then(
-      (user) => {
-        req.user = user;
-        next();
-        return;
-      },
-      (error) => {
-        console.log(error);
-        return res.status(403).json({error: { message: 'Failed to validate development token', code: 'failedDevelopmentToken'}});
-      }
-    );
-  } else {
-    // Check for header token
-    if(token === null || token === undefined) return res.status(403).json({error: { message: 'Invalid authorization token', code: 'invalidAuthorizationToken'}});
-    
-    // Lookup token
-    db.models.userToken.findOne({
-      where: {token: token},
-      include: [{model: db.models.user, as: 'user', attributes: {exclude:['password', 'deletedAt']}, include: [
-        {model: db.models.userGroup, as: 'groups', attributes: {exclude: ['id', 'user_id', 'owner_id', 'deletedAt', 'updatedAt', 'createdAt']}}
-      ]}]
-    }).then(
-      (result) => {
-        if(result === null) return res.status(403).json({error: { message: 'Invalid authorization token', code: 'invalidAuthorizationToken'}});
 
-        if(result.token) {
-          req.user = result.user;
+  // Check for header token
+  if(token === null || token === undefined) return res.status(403).json({error: { message: 'Invalid authorization token', code: 'invalidAuthorizationToken'}});
 
-          db.models.user.update({lastLogin: db.fn('NOW')}, {where: {ident: result.user.ident}});
-          next();
+  // Lookup token
+  db.models.userToken.findOne({
+    where: {token: token},
+    include: [{model: db.models.user, as: 'user', attributes: {exclude:['password', 'deletedAt']}, include: [
+      {model: db.models.userGroup, as: 'groups', attributes: {exclude: ['id', 'user_id', 'owner_id', 'deletedAt', 'updatedAt', 'createdAt']}}
+    ]}]
+  }).then(
+    (result) => {
+      if(result === null) return res.status(403).json({error: { message: 'Invalid authorization token', code: 'invalidAuthorizationToken'}});
 
-        }
-      },
-      (error) => {
-        console.log(error);
+      // Token has expired!
+      if(moment(result.expiresAt).diff(moment(), 'seconds') < 0 && !result.permanentToken) {
+        result.destroy();
         return res.status(403).json({error: { message: 'Invalid authorization token', code: 'invalidAuthorizationToken'}});
       }
-    );
-  }
+
+      // Refresh the token if it's more than 1 hour old.
+      if(moment(result.expiresAt).diff(moment(), 'seconds') < 82800 && !result.permanentToken) {
+        result.expiresAt = moment().add(24, 'hours');
+        result.save();
+      }
+
+      if(result.token) {
+        req.user = result.user;
+
+        db.models.user.update({lastLogin: db.fn('NOW')}, {where: {ident: result.user.ident}});
+        next();
+
+      }
+    },
+    (error) => {
+      console.log(error);
+      return res.status(403).json({error: { message: 'Invalid authorization token', code: 'invalidAuthorizationToken'}});
+    }
+  );
   
 };
