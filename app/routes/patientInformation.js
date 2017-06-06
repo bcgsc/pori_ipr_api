bf22c7dddf7dd20769266f06cca145d5d8a74ec7
Brandon Pierce <brandon.pierce@slalom.com>
2017-06-06 10:09:16 -0700

    Fix - Temporary bypass for patient information versioning

--- app/routes/patientInformation.js
+++ app/routes/patientInformation.js
@@ -37,10 +37,22 @@ router.route('/')
   })
   .put((req,res,next) => {
 
-    // Update DB Version for Entry
-    versionDatum(db.models.patientInformation, req.patientInformation, req.body, req.user, req.body.comment).then(
+    /**
+     *
+     * !!!!
+     * Bypass versioning for temporary patient information storage
+     * !!!!
+     *
+     */
+    db.models.patientInformation.update(req.patientInformation, {where: {pog_id: req.POG.id}}).then(
       (resp) => {
-        res.json(resp.data.create);
+
+        db.models.patientInformation.findOne({where: {pog_id: req.POG.id}}).then(
+          (resp) => {
+            res.json(resp);
+          }
+        );
+
       },
       (error) => {
         console.log(error);
