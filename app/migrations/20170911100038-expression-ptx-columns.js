8bfd16b7184d1d6b7c8ed4294e5b95755b275191
Brandon Pierce <brandon.pierce@slalom.com>
2017-09-11 11:02:58 -0700

    New - Added new columns to expression data tables

---
+++ app/migrations/20170911100038-expression-ptx-columns.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const Sq = require('sequelize');
+const db = require("../models");
+const _ = require('lodash');
+
+/**
+ * Add columns for tracking report & KB versions
+ *
+ */
+
+console.log('Loaded dependencies');
+
+// Make sure we're working on dev
+if(db.config.database !== 'ipr-dev') {
+  process.exit();
+}
+
+// Add new column to tables
+let addOutlierPtxCols2 = () => {
+  return new Promise((resolve, reject) => {
+    
+    console.log('[MIGRATION][addOutlierPtxCols2]', 'Starting migration');
+    
+    db.query('ALTER TABLE pog_analysis_reports_expression_outlier RENAME COLUMN "kIQR" TO "tcgakIQR";' +
+      'ALTER TABLE pog_analysis_reports_expression_outlier ' + 
+      'ADD COLUMN "tcgaPercCol" character varying DEFAULT null, ' +
+      'ADD COLUMN "tcgaAvgPerc" double precision DEFAULT null, ' +
+      'ADD COLUMN "tcgaAvgkIQR" double precision DEFAULT null, ' +
+      'ADD COLUMN "tcgaAvgQC" double precision DEFAULT null, ' +
+      'ADD COLUMN "ptxPercCol" character varying DEFAULT null, ' +
+      'ADD COLUMN "ptxTotSampObs" integer DEFAULT null, ' +
+      'ADD COLUMN "ptxPogPerc" double precision DEFAULT null, ' +
+      'ADD COLUMN "gtexComp" character varying DEFAULT null, ' +
+      'ADD COLUMN "gtexPerc" double precision DEFAULT null, ' +
+      'ADD COLUMN "gtexFC" double precision DEFAULT null, ' +
+      'ADD COLUMN "gtexkIQR" double precision DEFAULT null, ' +
+      'ADD COLUMN "gtexAvgPerc" double precision DEFAULT null, ' +
+      'ADD COLUMN "gtexAvgFC" double precision DEFAULT null, ' +
+      'ADD COLUMN "gtexAvgkIQR" double precision DEFAULT null, ' +
+      'ADD COLUMN "expression_class" character varying DEFAULT null, ' +
+      'ADD COLUMN "expType" character varying DEFAULT ' + "'rna';")
+      .then(
+        (result) => {
+          console.log('[MIGRATION][addOutlierPtxCols2]', 'Success');
+          resolve(true)
+        },
+        (err) => {
+          console.log('[MIGRATION][addOutlierPtxCols2]', 'Unable to add columns to table', err);
+          reject('addOutlierPtxCols2');
+        }
+      )
+    
+  });
+};
+
+console.log('[MIGRATION]', 'Starting Migration');
+
+// Start migration chain
+addOutlierPtxCols2()
+  .then((success) => {
+    console.log('[MIGRATION]', 'Finished migrations', success);
+  })
+  .catch((err) => {
+    console.log('[MIGRATION]', 'Failed migration', err);
+  });
--- app/models/reports/genomic/expressionAnalysis/outlier.js
+++ app/models/reports/genomic/expressionAnalysis/outlier.js
@@ -30,6 +30,10 @@ module.exports = (sequelize, Sq) => {
         key: 'id',
       }
     },
+    expType: {
+      type: Sq.STRING,
+      defaultValue: 'rna'
+    },
     outlierType: {
       type: Sq.ENUM('clinical', 'nostic', 'biological')
     },
@@ -57,10 +61,19 @@ module.exports = (sequelize, Sq) => {
     foldChange: {
       type: Sq.FLOAT,
     },
+    expression_class: {
+      type: Sq.STRING,
+      defaultValue: null,
+    },
+    
     tcgaPerc: {
       type: Sq.INTEGER,
     },
-    kIQR: {
+    tcgaPercCol: {
+      type: Sq.STRING,
+      defaultValue: null
+    },
+    tcgakIQR: {
       type: Sq.FLOAT,
       defaultValue: null,
     },
@@ -68,6 +81,20 @@ module.exports = (sequelize, Sq) => {
       type: Sq.FLOAT,
       defaultValue: null,
     },
+    tcgaAvgPerc: {
+      type: Sq.FLOAT,
+      defaultValue: null,
+    },
+    tcgaAvgkIQR: {
+      type: Sq.FLOAT,
+      defaultValue: null,
+    },
+    tcgaAvgQC: {
+      type: Sq.FLOAT,
+      defaultValue: null,
+    },
+    
+    
     tcgaNormPerc: {
       type: Sq.FLOAT,
       defaultValue: null,
@@ -76,6 +103,7 @@ module.exports = (sequelize, Sq) => {
       type: Sq.FLOAT,
       defaultValue: null,
     },
+    
     ptxPerc: {
       type: Sq.FLOAT,
       defaultValue: null,
@@ -83,7 +111,51 @@ module.exports = (sequelize, Sq) => {
     ptxkIQR: {
       type: Sq.FLOAT,
       defaultValue: null,
+    },
+    ptxPercCol: {
+      type: Sq.STRING,
+      defaultValue: null,
+    },
+    ptxTotSampObs: {
+      type: Sq.INTEGER,
+      defaultValue: null,
+    },
+    ptxPogPerc: {
+      type: Sq.FLOAT,
+      defaultValue: null,
+    },
+    
+    
+    gtexComp: {
+      type: Sq.STRING,
+      defaultValue: null,
+    },
+    gtexPerc: {
+      type: Sq.FLOAT,
+      defaultValue: null,
+    },
+    gtexFC: {
+      type: Sq.FLOAT,
+      defaultValue: null,
+    },
+    gtexkIQR: {
+      type: Sq.FLOAT,
+      defaultValue: null,
+    },
+    gtexAvgPerc: {
+      type: Sq.FLOAT,
+      defaultValue: null,
+    },
+    gtexAvgFC: {
+      type: Sq.FLOAT,
+      defaultValue: null,
+    },
+    gtexAvgkIQR: {
+      type: Sq.FLOAT,
+      defaultValue: null,
     }
+    
+    
   }, {
     // Table Name
     tableName: 'pog_analysis_reports_expression_outlier',
