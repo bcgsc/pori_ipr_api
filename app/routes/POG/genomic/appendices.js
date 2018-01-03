da059cca8d7b6c6a5d970e2afaa6ea906557c095
Brandon Pierce <brandon.pierce@slalom.com>
2018-01-03 11:46:25 -0800

    Fix - differentially provide tcga/gtex appendices data set

--- app/routes/POG/genomic/appendices.js
+++ app/routes/POG/genomic/appendices.js
@@ -2,13 +2,19 @@
 let express = require('express'),
   router = express.Router({mergeParams: true}),
   db = require(process.cwd() + '/app/models'),
-  tcga = require(process.cwd() + '/database/exp_matrix.v8.json');
+  tcga_v8 = require(process.cwd() + '/database/exp_matrix.v8.json');
+  tcga_v9 = require(process.cwd() + '/database/exp_matrix.v9.json');
 
 // Handle requests for alterations
 router.route('/tcga')
   .get((req,res,next) => {
-
-    res.json(tcga);
+    
+    console.log('Using set', req.report.expression_matrix);
+  
+    if(req.report.expression_matrix === 'v8') return res.json(tcga_v8);
+    if(req.report.expression_matrix === 'v9') return res.json(tcga_v9);
+    
+    res.json([]);
 
   });
 
