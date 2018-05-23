01a4be96e262810531763eaa3dad2736033e425b
Nikki Martin <nikjean.martin@gmail.com>
2018-05-23 16:22:09 -0700

    Fix - return updated user on user update; update fields specified in new user form when restoring deleted users

--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -60,8 +60,13 @@ router.route('/')
     db.models.user.findOne({where: {username: req.body.username, deletedAt: {$not: null}}, paranoid: false}).then(
       (existCheck) => {
         if(existCheck !== null) {
+
+          // set up user to restore with updated field values
+          let restoreUser = req.body;
+          restoreUser['deletedAt'] = null;
+
           // Restore!
-          db.models.user.update({deletedAt: null}, {paranoid:false,where:{ident: existCheck.ident}, returning: true}).then(
+          db.models.user.update(restoreUser, {paranoid:false,where:{ident: existCheck.ident}, returning: true}).then(
             (user) => {
 
               let response = {
@@ -215,14 +220,14 @@ router.route('/:ident([A-z0-9-]{36})')
     if(req.body.password && req.body.password.length > 7) updateBody.password = bcrypt.hashSync(req.body.password, 10);
 
     // Attempt user model update
-    db.models.user.update(updateBody, { where: {ident: req.body.ident}, limit: 1 }).then(
+    db.models.user.update(updateBody, { where: {ident: req.body.ident}, returning: true, limit: 1 }).then(
       (result) => {
         if(typeof result === 'Object') {
           res.json(result);
         } else {
           // Success, get user -- UGH
           db.models.user.findOne({
-            where: {ident: req.user.ident}, 
+            where: {ident: result[1][0].ident}, 
             attributes: {exclude: ['id', 'password', 'deletedAt']},
             include: [
               {as: 'groups', model: db.models.userGroup, attributes: {exclude: ['id', 'user_id', 'owner_id', 'deletedAt', 'updatedAt', 'createdAt']}},
