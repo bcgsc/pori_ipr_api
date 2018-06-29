da237d6f5712f97c4eac00aa94346ae1f4fd28af
Nikki Martin <nikjean.martin@gmail.com>
2018-06-29 14:18:25 -0700

    Override patient info in report loader if there is an existing entry
    - Update patient information check to return query result instead of boolean
    - If patient information check returns a non-null result, log warning and call
      patient delete function
    - Add helper function for deleting a patient information record given the row id
    - Always create new patient info record regardless of previous upload status

--- app/loaders/summary/patientInformation.js
+++ app/loaders/summary/patientInformation.js
@@ -10,86 +10,6 @@ let db = require(process.cwd() + '/app/models'),
     GenomicLoader = require('../index'),
     nconf = require('nconf').argv().env().file({file: process.cwd() + '/config/columnMaps.json'});
 
-
-/*
- * Parse Patient Information File
- *
- * 
- * @param object POG - POG model object
- *
- */
-/*
-module.exports = (report, dir, logger) => {
-
-  // Create promise
-  let deferred = Q.defer();
-  
-  // Read in file
-  let output = fs.createReadStream(dir + '/JReport_CSV_ODF/patient_info.csv');
-
-  // Setup Logger
-  let log = logger.loader(report.ident, 'Summary.PatientInformation');
-  
-  log('Found and read patient_info.csv file.');
-
-  
-  // Parse file!
-  let parser = parse({delimiter: ',', columns: true},
-    (err, result) => {
-      
-      // Was there a problem processing the file?
-      if(err) {
-        log('Unable to parse CSV file');
-        console.log(err);
-        deferred.reject({reason: 'parseCSVFail'});
-      }
-      
-      if(result.length > 1) return new Error('['+report.ident+'][Loader][Summary.PatientInformation] More than one patient history entry found.');
-    
-      // Remap results
-      let entry = _.head(remapKeys(result, nconf.get('summary:patientInformation')));
-      
-      // Map needed DB column values
-      entry.pog_id = report.pog_id;
-      entry.pog_report_id = report.id;
-
-      // Add to Database
-      db.models.patientInformation.create(entry).then(
-        (result) => {
-
-          log('Patient information loaded.', logger.SUCCESS);
-          
-          // Call Subloader
-          require('./sampleSummary.js')(report, dir, logger).then(
-            (success) => {
-              // Resolve Promise
-              deferred.resolve(entry);
-            },
-            (err) => {
-              deferred.reject({reason: 'sampleSummaryFailed'});
-            }
-          );
-          
-        },
-        (err) => {
-          log('Failed to load patient history.',logger.ERROR)
-        }
-      );
-    }
-  );
-  
-  // Pipe file through parser
-  output.pipe(parser);
-  
-  output.on('error', (err) => {
-    log('Unable to find required CSV file');
-    deferred.reject({reason: 'sourceFileNotFound'});
-  });
-  
-  return deferred.promise;
-  
-};
-*/
 /**
  * Patient Loader extends Genomic Loader
  *
@@ -120,35 +40,38 @@ class patientLoader {
       // Check if the patient Information exists yet
       this.checkPatientInformationExists().then(
         (exists) => {
-
-          // Don't need to create entry
+          // Delete patient info entry if it already exists
           if(exists) {
-            this.logging('Patient Information already loaded.', this.logger.SUCCESS);
-            resolve({name: 'patientInformation', result: null});
+            this.logging('Patient Information already loaded - overriding.', this.logger.WARNING);
+            return this.deleteEntry(exists.id);
           }
-
-          // Need to create entry
-          if(!exists) {
-            this.retrieveFileEntry()
-              .then(this.insertEntries.bind(this))
-              .then(this.runSampleSummary.bind(this)).then(
-              (result) => {
-                this.logging('Patient Information completed.', this.logger.SUCCESS);
-                resolve({name: 'patientInformation', result: true});
-              },
-              (err) => {
-                console.log(err);
-                this.logging('Patient Information was not able to complete.', this.logger.ERROR);
-                reject({name: 'patientInformation', result: false, message: 'Patient Information was not able to complete: ' + err.message});
-              }
-            )
-          }
+          
+          return;
+        }          
+      ).then(
+        () => {
+          // Create new patient info entry
+          this.retrieveFileEntry()
+            .then(this.insertEntries.bind(this))
+            .then(this.runSampleSummary.bind(this)).then(
+            (result) => {
+              this.logging('Patient Information completed.', this.logger.SUCCESS);
+              resolve({name: 'patientInformation', result: true});
+            },
+            (err) => {
+              console.log(err);
+              this.logging('Patient Information was not able to complete.', this.logger.ERROR);
+              reject({name: 'patientInformation', result: false, message: 'Patient Information was not able to complete: ' + err.message});
+            }
+          )
 
         },
         (err) => {
           reject({name: 'patientInformation', result: false, message: err.message});
         }
-      );
+      ).catch((err) => {
+
+      });
 
     });
   }
@@ -156,14 +79,13 @@ class patientLoader {
   /**
    * Check is a patient information entry exists yet
    *
-   * @returns {Promise|boolean}
+   * @returns {Promise}
    */
   checkPatientInformationExists() {
     return new Promise((resolve,reject) => {
       db.models.patientInformation.findOne({where: {pog_id: this.report.pog_id}}).then(
         (entry) => {
-          if(entry !== null) resolve(true);
-          if(entry === null) resolve(false);
+          resolve(entry);
         },
         (err) => {
           reject(err);
@@ -240,6 +162,27 @@ class patientLoader {
 
     });
   }
+
+  /**
+   * Remove an existing Patient Information entry
+   *
+   * @param {int} id - the id of the row to delete from the patient info table
+   * @returns {Promise}
+   */
+   deleteEntry(patient_id) {
+    return new Promise((resolve, reject) => {
+      db.models.patientInformation.destroy({where: {id: patient_id}}).then(
+        (result) => {
+          this.logging('Existing patient entry has been deleted', this.logger.SUCCESS)
+          resolve(result);
+        },
+        (err) => {
+          this.logging('Failed to delete existing patient entry.', this.logger.ERROR);
+          reject({reason: 'patientDeleteFailed'});
+        }
+      );
+    })
+   }
 
   /**
    * Run Sample Summary loader
