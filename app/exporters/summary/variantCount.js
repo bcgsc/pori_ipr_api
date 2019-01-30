a16567abc0ce54ee3329de99a190126c5ce2ffad
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-01-30 09:32:37 -0800

    Remove redundant entries array

--- app/exporters/summary/variantCount.js
+++ app/exporters/summary/variantCount.js
@@ -20,18 +20,23 @@ module.exports = async (pog, directory) => {
   };
 
   const results = await db.models.genomicAlterationsIdentified.findAll(opts);
-  const entries = [];
-
-  results.forEach((value) => {
-    entries.push(value.get().geneVariant);
-  });
 
   const filename = `${directory.export}/genomic_alt_identified.csv`;
   // Construct data
   let data = 'gene_variant_1,gene_variant_2,gene_variant_3,gene_variant_4,gene_variant_5\n';
-  while (entries.length > 0) {
-    data += `${entries.splice(0, 5).join(',')}\n`;
+  // Add results to data string
+  let newLine = '';
+  for (let i = 0; i < results.length;) {
+    newLine += `${results[i].get().geneVariant}`;
+    i++;
+    if (i % 5 === 0 || i >= results.length) {
+      data += `${newLine}\n`;
+      newLine = '';
+    } else {
+      newLine += ',';
+    }
   }
+
   unlinkAndWrite(filename, data);
 
   return {stage: 'summary.genomicAlterationsIdentified', status: true};
