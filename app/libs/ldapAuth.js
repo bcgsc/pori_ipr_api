92650f97cc4f78f1d65fb2ab408db54396455e81
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-02-06 10:43:27 -0800

    Lint IdapAuth.js

--- app/libs/ldapAuth.js
+++ app/libs/ldapAuth.js
@@ -1,15 +1,16 @@
 
 /**
+ * !! Need to add actual Authentication or delete file !!
+ *
  * Basic LDAP Authentication against BCGSC LDAP Service
  *
- * @param user - Username (cn)
- * @param pass - Password (secret)
- * @returns {Promise} - {promise.response, promise.status, promise.message}
+ * @param {object} username - Username (cn)
+ * @param {string} password - Password (secret)
+ * @returns {Promise.<boolean>} - Returns true if the authentication was successful
  */
-const authenticate = async (user, pass) => {
-
+const authenticate = async (username, password) => {
   return true;
-}
+};
 
 module.exports = {
   authenticate,
