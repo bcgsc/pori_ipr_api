235059141840e93cd8b94cddc9c0dda550f6b6dc
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-02-07 15:18:49 -0800

    Lint mavis.js

--- app/libs/mavis.js
+++ app/libs/mavis.js
@@ -1,25 +1,35 @@
 const fs = require('fs');
 const d3 = require('d3-dsv');
-const db = require('../../../app/models');
+const db = require('../models');
 
-/*
+/**
  * Parse MAVIS Summary File
  *
- * @param string mavisFile - name of TSV file for given MAVIS summary
- *
+ * @param {object} report - A report
+ * @param {string} mavisFile - Name of TSV file for given MAVIS summary
+ * @returns {Array.<object>} - Returns an array of mavis records
  */
 const parseMavisFile = async (report, mavisFile) => {
+  // Check that MAVIS summary exists
+  if (!fs.existsSync(mavisFile)) {
+    throw new Error(`Failed to find MAVIS summary file: ${mavisFile}`);
+  }
 
   // Read in TSV file
   const data = fs.readFileSync(mavisFile);
-    
+  
   // Parse TSV file
   const parsedMavisSummary = d3.tsvParse(data.toString());
     
   // Formatting summaries to be inserted into db
-  const mavisRecords = parsedMavisSummary.map(record => {
-      return {product_id: record.product_id.split(';')[0], pog_id: report.pog_id, pog_report_id: report.id, summary: JSON.stringify(record)};
-    }); 
+  const mavisRecords = parsedMavisSummary.map((record) => {
+    return {
+      product_id: record.product_id.split(';')[0],
+      pog_id: report.pog_id,
+      pog_report_id: report.id,
+      summary: JSON.stringify(record),
+    };
+  });
 
   return mavisRecords;
 };
@@ -30,62 +40,38 @@ const parseMavisFile = async (report, mavisFile) => {
  * Create DB entries for MAVIS summaries. Parse in CSV values, mutate, insert.
  *
  * @param {object} report - POG report model object
- * @param {list} source - list of file paths to MAVIS summaries
- * @param {list}   productIds - list of MAVIS product ids to add to database
+ * @param {list} sources - list of file paths to MAVIS summaries
+ * @param {list} productIds - list of MAVIS product ids to add to database
+ * @returns {Promise.<Array.<Model>>} - Returns an array of the mavis summary's created
  *
  */
-
 const addMavisSummary = async (report, sources, productIds) => {
-    // Parsing MAVIS files
-    // Wait for all promises to be resolved
-    Promise.all([]);
-    //??? Here I am
-    Q.all(_.map(sources, (source) => {
-      // Check that MAVIS summary exists
-      if(!fs.existsSync(source)) {
-        // Warn MAVIS summary was not found
-        return log('Failed to find MAVIS summary file: ' + source, logger.WARNING);
-      }
-    
+  // Parsing MAVIS files
+  // Wait for all promises to be resolved
+  const results = await Promise.all(
+    sources.map((source) => {
       // Create Promise
       return parseMavisFile(report, source);
-
-    }))
-    .then(
-      (results) => {
-
-        // Union all parsed MAVIS files based on their product ids
-        let mavisSummary;
-        _.each(results, function(summary) {
-          mavisSummary = _.unionBy(mavisSummary, summary, 'product_id');
-        });
-
-        // Filter results for records to insert based on product id
-        let createRecords = _.filter(mavisSummary, function(record) {
-          return _.includes(productIds, record.product_id);
-        });
-
-        // Load into Database
-        return db.models.mavis.bulkCreate(createRecords);
-      },
-      (error) => {
-        throw new Error('Unable to process MAVIS summary file.');
     })
-    .then(
-      (result) => {
-        deferred.resolve(result);  
-      },
-      // Problem creating DB entries
-      (err) => {
-          throw new Error('Unable to create MAVIS summary database entries.');
-          
-    })
-    .catch((error) => {
-      deferred.reject({message: error.message});
+  );
+
+  // Union all parsed MAVIS files based on their product ids
+  const mavisSummary = [];
+  let found;
+  results.forEach((summary) => {
+    found = mavisSummary.find((value) => {
+      return value.product_id === summary.product_id;
     });
-    
-    return deferred.promise;
-  }
+
+    // Filter results for records to insert based on product id
+    if (!found && productIds.includes(summary.product_id)) {
+      mavisSummary.push(summary);
+    }
+  });
+
+  // Load into Database
+  return db.models.mavis.bulkCreate(mavisSummary);
+};
 
 module.exports = {
   addMavisSummary,
