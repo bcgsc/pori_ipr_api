584228d0108abc20b13cd70b99921b5bee3fe224
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-02-13 16:02:23 -0800

    Add @property to JSDocs and replaced _variableName with variableName and changed methods to setters

--- app/middleware/acl.js
+++ app/middleware/acl.js
@@ -7,89 +7,97 @@ class ACL {
    *
    * @param {object} request - Express request object
    * @param {object} response - Express response object
+   * @property {Array.<string>} groups - Groups allowed to view
+   * @property {Array.<string>} nGroups - Groups explicitly not allowed
+   * @property {boolean} isPog - Is this a POG endpoint? Do we need to check relationship to POG?
+   * @property {boolean} authReq - Is the user required to be authenticated?
+   * @property {Array.<string>} read - Can they read this resource? (Default: true)
+   * @property {Array<string>} write - Can they edit this resource? (Default: false)
+   * @property {Array.<string>} delete - Can they remove this resource? (Default: false)
+   * @property {Array.<string>} pogEdit - Default editing state for POGs
    */
   constructor(request, response) {
-    this._req = request; // Express Request object
-    this._res = response; // Express response object
-    this._groups = []; // Groups allowed to view
-    this._nGroups = []; // Groups explicitly not allowed
-    this._isPog = false; // Is this a POG endpoint? Do we need to check relationship to POG?
-    this._authReq = true; // Is the user required to be authenticated?
-    this._read = ['*']; // Can they read this resource? (Default: true)
-    this._write = ['superUser', 'admin']; // Can they edit this resource? (Default: false)
-    this._delete = ['superUser', 'admin']; // Can they remove this resource? (Default: false)
-    this._pogEdit = ['analyst', 'reviewer', 'admin']; // Default editing state for POGs
+    this.req = request;
+    this.res = response;
+    this.groups = [];
+    this.nGroups = [];
+    this.isPog = false;
+    this.authReq = true;
+    this.read = ['*'];
+    this.write = ['superUser', 'admin'];
+    this.delete = ['superUser', 'admin'];
+    this.pogEdit = ['analyst', 'reviewer', 'admin'];
   }
 
   /**
    * Sets the list of groups allowed to view this endpoint
    *
-   * @param  {...string} readers - List of groups allowed to view this endpoint
+   * @param  {Array.<string>} readers - List of groups allowed to view this endpoint
    * @returns {undefined}
    */
-  read(...readers) {
-    this._read = readers;
+  set read(readers) {
+    this.read = readers;
   }
 
   /**
    * Sets the list of groups allowed to write at this endpoint
    *
-   * @param  {...string} writers - List of groups allowed to write at this endpoint
+   * @param  {Array.<string>} writers - List of groups allowed to write at this endpoint
    * @returns {undefined}
    */
-  write(...writers) {
-    this._write = writers;
+  set write(writers) {
+    this.write = writers;
   }
 
   /**
    * Sets the list of groups allowed to delete at this endpoint
    *
-   * @param  {...string} deleters - List of groups allowed to delete at this endpoint
+   * @param  {Array.<string>} deleters - List of groups allowed to delete at this endpoint
    * @returns {undefined}
    */
-  delete(...deleters) {
-    this._delete = deleters;
+  set delete(deleters) {
+    this.delete = deleters;
   }
 
   /**
    * Sets the list of groups allowed to write/edit at this POG endpoint
    *
-   * @param  {...string} editors - List of groups allowed to write/edit at this POG endpoint
+   * @param  {Array.<string>} editors - List of groups allowed to write/edit at this POG endpoint
    * @returns {undefined}
    */
-  pogEdit(...editors) {
-    this._pogEdit = editors;
+  set pogEdit(editors) {
+    this.pogEdit = editors;
   }
 
   /**
    * Set the list of groups NOT allowed to view this endpoint
    *
-   * @param  {...string} nGroups - Explicit list of groups NOT allowed to view this endpoint
+   * @param  {Array.<string>} nGroups - Explicit list of groups NOT allowed to view this endpoint
    * @returns {undefined}
    */
-  notGroups(...nGroups) {
-    this._nGroups = nGroups;
+  set notGroups(nGroups) {
+    this.nGroups = nGroups;
   }
 
   authReq() {
-    this._authReq = true;
+    this.authReq = true;
   }
 
   isPog() {
-    this._isPog = true;
+    this.isPog = true;
   }
 
   // Get project access
   async getProjectAccess() {
     const accessGroups = ['Full Project Access', 'admin', 'superUser'];
-    const userGroups = _.map(this._req.user.groups, 'name');
+    const userGroups = _.map(this.req.user.groups, 'name');
     const hasAccess = _.intersection(accessGroups, userGroups);
 
     if (hasAccess.length > 0) { // user has full project access
       return db.models.project.findAll();
     }
     // user does not have full project access - filter on user_project relation
-    return this._req.user.projects;
+    return this.req.user.projects;
   }
 
   check(skipStatus = false) {
@@ -97,61 +105,65 @@ class ACL {
     let allowed = false;
 
     // If any group is allowed
-    if (this._groups.includes('*')) {
+    if (this.groups.includes('*')) {
       allowed = true;
     }
 
     // Loop over groups and add to simple array
-    const userGroups = _.map(this._req.user.groups, g => g.name);
+    const userGroups = _.map(this.req.user.groups, g => g.name);
 
-    if (_.intersection(this._groups, this._nGroups).length > 0) {
+    if (_.intersection(this.groups, this.nGroups).length > 0) {
       throw new Error('Group(s) in both allowed and not allowed');
     }
 
     // Check for access to POG
-    if (this._isPog) {
+    if (this.isPog) {
       // Check this pog for user permissions specific to pog roles
 
       // Get this users roles for this POG
       const pogRole = [];
-      _.forEach(this._req.POG.POGUsers, (value) => {
-        if (value.user.ident === this._req.user.ident) {
+      _.forEach(this.req.POG.POGUsers, (value) => {
+        if (value.user.ident === this.req.user.ident) {
           pogRole.push(value.role);
         }
       });
 
       // Check if this is a write endpoint
-      if (['POST', 'PUT', 'DELETE'].includes(this._req.method)
-      && (_.intersection(pogRole, this._pogEdit).length > 0
-      || _.intersection(userGroups, this._pogEdit).length > 0)) {
+      if (['POST', 'PUT', 'DELETE'].includes(this.req.method)
+        && (_.intersection(pogRole, this.pogEdit).length > 0
+        || _.intersection(userGroups, this.pogEdit).length > 0)
+      ) {
         // Does this user have at least 1 pogRole that is allowed to edit?
         allowed = true;
       }
 
       // If read is not set to allow all, run check for pogRole access
-      if (this._req.method === 'GET'
-      && !this._read.includes('*')
-      && (_.intersection(pogRole, this._read).length > 0)) {
+      if (this.req.method === 'GET'
+        && !this.read.includes('*')
+        && (_.intersection(pogRole, this.read).length > 0)
+      ) {
         allowed = true;
       }
     }
 
     // If method is GET and
-    if (this._req.method === 'GET'
-    && (this._read.includes('*')
-    || _.intersection(userGroups, this._read).length > 0)) {
+    if (this.req.method === 'GET'
+      && (this.read.includes('*')
+      || _.intersection(userGroups, this.read).length > 0)
+    ) {
       allowed = true;
     }
 
     // Write Access
-    if (['POST', 'PUT', 'DELETE'].includes(this._req.method)
-    && ((this.write.includes('*')
-    || _.intersection(userGroups, this._write).length > 0))) {
+    if (['POST', 'PUT', 'DELETE'].includes(this.req.method)
+      && ((this.write.includes('*')
+      || _.intersection(userGroups, this.write).length > 0))
+    ) {
       allowed = true;
     }
 
     // Check Explicitly Not Allowed Groups
-    const nintersect = _.intersection(this._nGroups, userGroups);
+    const nintersect = _.intersection(this.nGroups, userGroups);
 
     // Not allowed groups
     if (nintersect.length > 0) {
@@ -161,7 +173,7 @@ class ACL {
     // Access is not allowed
     if (!allowed) {
       if (!skipStatus) {
-        this._res.status(403).json({status: false, message: 'You are not authorized to view this resource.'});
+        this.res.status(403).json({status: false, message: 'You are not authorized to view this resource.'});
       }
       return false;
     }
