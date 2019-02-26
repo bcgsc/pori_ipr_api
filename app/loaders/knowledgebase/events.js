4187a7653aa54a4213307dfa6c3cb0e22a6daf80
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-02-26 14:50:19 -0800

    Lint events.js

--- app/loaders/knowledgebase/events.js
+++ app/loaders/knowledgebase/events.js
@@ -1,35 +1,27 @@
-"use strict";
 
-// Dependencies
-let db = require(process.cwd() + '/app/models'),
-  fs = require('fs'),
-  dl = require('datalib'),
-  remapKeys = require(process.cwd() + '/app/libs/remapKeys'),
-  _ = require('lodash'),
-  Q = require('q'),
-  nconf = require('nconf').argv().env().file({file: process.cwd() + '/config/kbColumns.json'});
+const dl = require('datalib');
+const db = require('../../models');
 
+const {logger} = process;
 
-let remapStatus = (inputStatus) => {
-  if(inputStatus === null) return "REQUIRES-REVIEW";
-  if(inputStatus.match(/(v[0-9]*.[0-9]*.[0-9]*)/) !== null) return 'APPROVED';
-
+const remapStatus = (inputStatus) => {
+  if (!inputStatus) {
+    return 'REQUIRES-REVIEW';
+  }
+  if (inputStatus.match(/(v[0-9]*.[0-9]*.[0-9]*)/)) {
+    return 'APPROVED';
+  }
   return inputStatus;
-
 };
-let mapUser = (inputUser) => {
-  if(inputUser === null) return null;
 
-  switch(inputUser) {
+const mapUser = (inputUser) => {
+  switch (inputUser) {
     case 'CRR':
       return 4;
-      break;
     case 'MRJ':
       return 3;
-      break;
     default:
       return null;
-      break;
   }
 };
 
@@ -37,59 +29,34 @@ let mapUser = (inputUser) => {
 /**
  * Parse KB Entries file
  *
- * @param {object} dir - Directory to locate the KB exports in
- * @param {object} logger - Logging utility instance
- * @param {object} options
+ * @param {string} dir - Directory to locate the KB exports in
+ * @param {object} options - Contains the events filename
  *
- * @returns {promise|object} - Resolves with object, Rejects with object
+ * @returns {Promise.<object>} - Resolves with object, Rejects with object
  */
-module.exports = (dir, logger, options) => {
+module.exports = async (dir, options) => {
+  const file = options.events;
 
-  // Create promise
-  let deferred = Q.defer();
+  if (!file) {
+    throw new Error('No events filename specified');
+  }
 
-  // Setup Logger
-  let log = logger.loader('KB-Import', 'Events');
-  let file = options.events;
-  
-  if(!file) return reject({message: 'No events filename specified'});
-  
   // Read in event entries
-  let entries = dl.tsv(`${dir}/${file}`);
-
-  _.forEach(entries, (e, i)=> {
-
+  const entries = dl.tsv(`${dir}/${file}`);
+  entries.forEach((entry, index) => {
     // Ignore deleted entries.
-    if(e.status === 'RESOLVED-CAN-DELETE') return delete entries[i];
-
+    if (entry.status === 'RESOLVED-CAN-DELETE') {
+      return delete entries[index];
+    }
     // Do some remapping
-    e.status = remapStatus(e.status);
-    e.reviewedBy_id = mapUser(e.last_modified_by);
-    e.in_version = (e.status !== null && e.status.match(/(v[0-9]*.[0-9]*.[0-9]*)/) !== null) ? e.status : null;
-
-    // Write updated entry to array
-    entries[i] = e;
+    entry.in_version = (entry.status && entry.status.match(/(v[0-9]*.[0-9]*.[0-9]*)/)) ? entry.status : null;
+    entry.status = remapStatus(entry.status);
+    entry.reviewedBy_id = mapUser(entry.last_modified_by);
   });
 
-  log('Reading in '+ entries.length + ' entries.');
-
-  //process.exit();
-
   // Add to Database
-  db.models.kb_event.bulkCreate(entries).then(
-    (result) => {
-      log('Finished loading KB events table.', logger.SUCCESS);
+  const result = await db.models.kb_event.bulkCreate(entries);
+  logger.info('Finished loading KB events table.');
 
-      // Resolve Promise
-      deferred.resolve(result);
-    },
-    (err) => {
-      console.log(err.message);
-      log('Failed to load KB events into DB', logger.ERROR);
-      deferred.reject('Failed to load KB events into database.');
-    }
-  );
-
-  return deferred.promise;
-
+  return result;
 };
