cd30247e9bfee05e0ed2c3170bed0aff63f5704a
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-02-26 14:52:43 -0800

    Lint outlier.js

--- app/loaders/expressionAnalysis/outlier.js
+++ app/loaders/expressionAnalysis/outlier.js
@@ -1,77 +1,49 @@
-"use strict";
+const fs = require('fs');
+const _ = require('lodash');
+const parse = require('csv-parse/lib/sync');
+const nconf = require('nconf').argv().env().file({file: '../../../config/columnMaps.json'});
+const db = require('../../models');
+const remapKeys = require('../../libs/remapKeys');
+const p2s = require('../../libs/pyToSql');
 
-// Dependencies
-let db = require(process.cwd() + '/app/models'),
-  fs = require('fs'),
-  parse = require('csv-parse'),
-  remapKeys = require(process.cwd() + '/app/libs/remapKeys'),
-  _ = require('lodash'),
-  Q = require('q'),
-  p2s = require(process.cwd() + '/app/libs/pyToSql'),
-  nconf = require('nconf').argv().env().file({file: process.cwd() + '/config/columnMaps.json'});
+const {logger} = process;
 
 let baseDir;
 
 /**
  * Parse Expression Outliers File
  *
- *
  * @param {object} report - POG report model object
- * @param {string} expressionOutlierFile - name of CSV file for given small mutation type
- * @param {string} outlierType - outlierType of these entries (clinical, nostic, biological)
- * @param {object} log - /app/libs/logger instance
+ * @param {string} expressionOutlierFile - Name of CSV file for given small mutation type
+ * @param {string} outlierType - OutlierType of these entries (clinical, nostic, biological)
+ * @returns {Promise.<Array.<object>>} - Returns expression outlier values from parsed file
  *
  */
-let parseExpressionOutlierFile = (report, expressionOutlierFile, outlierType, log) => {
-
-  // Create promise
-  let deferred = Q.defer();
-  
+const parseExpressionOutlierFile = async (report, expressionOutlierFile, outlierType) => {
   // First parse in therapeutic
-  let output = fs.createReadStream(baseDir + '/JReport_CSV_ODF/' + expressionOutlierFile, {'delimiter': ','});
+  const output = fs.readFileSync(`${baseDir}/JReport_CSV_ODF/${expressionOutlierFile}`, {delimiter: ','});
 
   // Parse file!
-  let parser = parse({delimiter: ',', columns: true},
-    (err, result) => {
+  const result = parse(output, {delimiter: ',', columns: true});
 
-      // Was there a problem processing the file?
-      if(err) {
-        log('Unable to parse Expression Outlier CSV file');
-        console.log(err);
-        deferred.reject({loader: 'expressionOutlier', message: 'Unable to parse the CSV: ' + baseDir + '/JReport_CSV_ODF/' + expressionOutlierFile, result: false});
-      }
+  // Remap results
+  const entries = remapKeys(result, nconf.get('expressionAnalysis:outlier'));
 
-      // Remap results
-      let entries = remapKeys(result, nconf.get('expressionAnalysis:outlier'));
-
-      // Add new values for DB
-      entries.forEach((v, k) => {
-        // Map needed DB column values
-        entries[k] = p2s(v, ['rnaReads', 'foldChange', 'ptxPogPerc', 'ptxTotSampObs', 'ptxkIQR', 'ptxPerc']);
-        entries[k].pog_id = report.pog_id;
-        entries[k].pog_report_id = report.id;
-        entries[k].outlierType = outlierType;
-        entries[k].expType = 'rna';
-      });
-
-      // Log progress
-      log('Parsed .csv for: ' + outlierType);
-
-      // Resolve Promise
-      deferred.resolve(entries);
-    }
-  );
-
-  // Pipe file through parser
-  output.pipe(parser);
-
-  output.on('error', (err) => {
-    log('Unable to find required CSV file: ' + expressionOutlierFile);
-    deferred.reject({loader: 'expressionOutlier', message: 'Unable to find the CSV: ' + baseDir + '/JReport_CSV_ODF/' + expressionOutlierFile, result: false});
+  // Add new values for DB
+  const expOutEntries = entries.map((value) => {
+    // Map needed DB column values
+    const newValue = p2s(value, ['rnaReads', 'foldChange', 'ptxPogPerc', 'ptxTotSampObs', 'ptxkIQR', 'ptxPerc']);
+    newValue.pog_id = report.pog_id;
+    newValue.pog_report_id = report.id;
+    newValue.outlierType = outlierType;
+    newValue.expType = 'rna';
+    return newValue;
   });
 
-  return deferred.promise;
+  // Log progress
+  logger.info(`Parsed .csv for: ${outlierType}`);
 
+  return expOutEntries;
 };
 
 /**
@@ -79,73 +51,43 @@ let parseExpressionOutlierFile = (report, expressionOutlierFile, outlierType, lo
  *
  * Load values for "Expression Analysis"
  * sources:
- *  - exp_biol.csv   -Biological
+ *  - exp_biol.csv          -Biological
  *  - exp_pot_clin_rel.csv  -Clinical
- *  - exp_prog_diag.csv  -Nostic
+ *  - exp_prog_diag.csv     -Nostic
  *
  * Create DB entries for Expression Outliers. Parse in CSV values, mutate, insert.
  *
  * @param {object} report - POG report model object
  * @param {string} dir - Base directory to load from
  * @param {object} logger - Logging utility
- *
+ * @returns {Promise.<object>} - Returns an object with the results of the created db entries
  */
-module.exports = (report, dir, logger) => {
-
+module.exports = async (report, dir) => {
   baseDir = dir;
 
-  // Create promise
-  let deferred = Q.defer();
-
-  // Setup Logger
-  let log = logger.loader(report.ident, 'Exp.Outlier');
-
   // Small Mutations to be processed
-  let sources = [
+  const sources = [
     {file: 'exp_biol.csv', type: 'biological'},
     {file: 'exp_pot_clin_rel.csv', type: 'clinical'},
     {file: 'exp_prog_diag.csv', type: 'nostic'},
   ];
 
-  // Promises Array
-  let promises = [];
-
   // Loop over sources and collect promises
-  sources.forEach((input) => {
-    promises.push(parseExpressionOutlierFile(report, input.file, input.type, log));
+  const promises = sources.map((input) => {
+    return parseExpressionOutlierFile(report, input.file, input.type);
   });
 
   // Wait for all promises to be resolved
-  Q.all(promises)
-    .then((results) => {
-      // Log progress
-      log('Expression Outliers collected: ' + _.flattenDepth(results, 2).length);
+  const results = await Promise.all(promises);
+  const flatResults = _.flattenDepth(results, 2);
+  // Log progress
+  logger.info(`Expression Outliers collected: ${flatResults.length}`);
 
-      // Load into Database
-      db.models.outlier.bulkCreate(_.flattenDepth(results, 2)).then(
-        (result) => {
+  // Load into Database
+  const result = await db.models.outlier.bulkCreate(flatResults);
 
-          // Successful create into DB
-          log('Database entries created.', logger.SUCCESS);
+  // Successful create into DB
+  logger.info('Database entries created.');
 
-          // Done!
-          deferred.resolve({loader: 'expressionOutliers', result: true, data: result});
-        },
-        // Problem creating DB entries
-        (err) => {
-          console.log(err);
-          log('Unable to create database entries.', logger.ERROR);
-          deferred.reject({loader: 'expressionOutlier', message: 'Unable to create the database entries.', result: false});
-          new Error('Unable to create expression outliers database entries.');
-        }
-      );
-
-    },
-    (err) => {
-      console.log(err);
-      log('Unable to load a CSV file', logger.ERROR);
-      deferred.reject({loader: 'expressionOutlier', message: 'Unable to load a CSV file: ' + err.message, result: false});
-    });
-
-  return deferred.promise;
+  return {loader: 'expressionOutliers', result: true, data: result};
 };
