ffee3128567c7903121aa87bc3f1ab2a0a2276ea
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-03-06 10:13:50 -0800

    Lint validate.js

--- app/routes/knowledgebase/validate.js
+++ app/routes/knowledgebase/validate.js
@@ -1,25 +1,21 @@
-"use strict";
+const express = require('express');
+const {execSync} = require('child_process');
 
-// app/routes/genomic/detailedGenomicAnalysis.js
-let express = require('express'),
-  router = express.Router({mergeParams: true}),
-  db = require(process.cwd() + '/app/models'),
-  _ = require('lodash'),
-  acl = require(process.cwd() + '/app/middleware/acl'),
-  exec = require('child_process').exec;
+const router = express.Router({mergeParams: true});
+const {logger} = process;
 
 
 // Test python library wrapper
 router.route('/events')
-  .post((req,res) => {
-
-    // Take input and call child
-    exec('/projects/tumour_char/analysis_scripts/python/centos06/anaconda3_v4.3.0/envs/python3.4/bin/python ' + process.cwd() + '/app/libs/kbSanitationWrapper.py --input "' + req.body.events_expression + '"', (err, stdout, stderr) => {
-      if(err) return res.status(400).json({error: {message: "Unable to validate the provided input", code: "KBValidationFailed"}});
-
-      let output = JSON.parse(stdout);
-      res.json({valid: req.body.events_expression});
-    });
+  .post((req, res) => {
+    try {
+      // Take input and call child
+      execSync(`/projects/tumour_char/analysis_scripts/python/centos06/anaconda3_v4.3.0/envs/python3.4/bin/python${process.cwd()}/app/libs/kbSanitationWrapper.py --input "${req.body.events_expression}"`);
+      return res.json({valid: req.body.events_expression});
+    } catch (error) {
+      logger.error(error);
+      return res.status(400).json({error: {message: 'Unable to validate the provided input', code: 'KBValidationFailed'}});
+    }
   });
 
 module.exports = router;
