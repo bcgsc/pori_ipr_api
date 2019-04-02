e92a4db02556b55e7ab46d0306d9a47b86caa64f
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-04-02 10:24:51 -0700

    Add radix to parseInt

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -117,8 +117,8 @@ router.route('/')
       // limits and offsets are causing the query to break due to the public scope and subqueries
       // i.e. fields are not available for joining onto subquery selection
       // dealing w/ applying the pagination here
-      const limit = parseInt(req.query.limit) || 25; // Gotta parse those ints because javascript is javascript!
-      const offset = parseInt(req.query.offset) || 0;
+      const limit = parseInt(req.query.limit, 10) || 25; // Gotta parse those ints because javascript is javascript!
+      const offset = parseInt(req.query.offset, 10) || 0;
 
       // apply limit and offset to results
       const start = offset;
