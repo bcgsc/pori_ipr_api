651c192a9656599ea36b33201aedbb7291c03e9a
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-04-02 09:27:39 -0700

    Lint germline_small_mutation.middleware

--- app/modules/germine_small_mutation/middleware/germline_small_mutation.middleware.js
+++ app/modules/germine_small_mutation/middleware/germline_small_mutation.middleware.js
@@ -1,35 +1,29 @@
-const _                       = require('lodash');
-const db                      = require(process.cwd() + '/app/models');
-const MiddlewareNotFound      = require('../../../middleware/exceptions/MiddlewareNotFound');
-const MiddlewareQueryFailed   = require('../../../middleware/exceptions/MiddlewareQueryFailed');
+const db = require('../../../models');
+const MiddlewareNotFound = require('../../../middleware/exceptions/MiddlewareNotFound');
+const MiddlewareQueryFailed = require('../../../middleware/exceptions/MiddlewareQueryFailed');
+
+const {logger} = process;
 
 // Lookup POG middleware
-module.exports = (req,res,next,ident) => {
-  
-  let opts = { where: {} };
-  
-  opts.attributes = {
-    exclude: ['deletedAt']
-  };
-  
-  // Check if it's a UUID
-  opts.where.ident = ident;
-  
-  // Lookup POG first
-  db.models.germline_small_mutation.scope('public').findOne(opts).then(
-    (result) => {
-      // Nothing found?
-      if(result === null) throw new MiddlewareNotFound("Unable to find the germline report", req, res, "germlineReport");
-      
-      // POG found, next()
-      if(result !== null) {
-        req.report = result;
-        next();
-      }
+module.exports = async (req, res, next, ident) => {
+  const opts = {
+    where: {
+      ident,
     },
-    (error) => {
-      console.log(error);
-      throw new MiddlewareQueryFailed("Unable to find the requested germline report.", req, res, "failedTrackingStateTaskMiddlewareQuery");
+    attributes: {
+      exclude: ['deletedAt'],
+    },
+  };
+
+  try {
+    const result = await db.models.germline_small_mutation.scope('public').findOne(opts);
+    if (!result) {
+      throw new MiddlewareNotFound('Unable to find the germline report', req, res, 'germlineReport');
     }
-  );
+    req.report = result;
+    return next();
+  } catch (error) {
+    logger.error(error);
+    throw new MiddlewareQueryFailed('Unable to find the requested germline report.', req, res, 'failedTrackingStateTaskMiddlewareQuery');
+  }
 };
