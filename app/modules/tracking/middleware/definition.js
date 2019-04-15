33bc0c636e537813c4d018e80a4832927ef24a13
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-04-15 13:59:26 -0700

    Lint definitions.js

--- app/modules/tracking/middleware/definition.js
+++ app/modules/tracking/middleware/definition.js
@@ -1,29 +1,27 @@
-const _                       = require('lodash');
-const db                      = require(process.cwd() + '/app/models');
-const MiddlewareNotFound      = require('../../../middleware/exceptions/MiddlewareNotFound');
-const MiddlewareQueryFailed   = require('../../../middleware/exceptions/MiddlewareQueryFailed');
+const db = require('../../../models');
+const MiddlewareNotFound = require('../../../middleware/exceptions/MiddlewareNotFound');
+const MiddlewareQueryFailed = require('../../../middleware/exceptions/MiddlewareQueryFailed');
+
+const {logger} = process;
 
 // Lookup POG middleware
-module.exports = (req,res,next,ident) => {
+module.exports = async (req, res, next, ident) => {
+  let trackingStateDef;
+  try {
+    trackingStateDef = await db.models.tracking_state_definition.scope('public').findOne({
+      where: {ident},
+      limit: 1,
+    });
+  } catch (error) {
+    logger.error(`Error while finding tracking state definition ${error}`);
+    throw new MiddlewareQueryFailed('Error while finding tracking state definition', req, res, 'failedTrackingStateDefinitionMiddlewareQuery');
+  }
 
-  // Lookup POG first
-  db.models.tracking_state_definition.scope('public').findOne({
-    where: {ident: ident},
-    limit: 1
-  }).then(
-    (result) => {
-      // Nothing found?
-      if(result === null) throw new MiddlewareNotFound("Unable to find the tracking state definition", req, res, "trackingStateDefinitionNotFound");
+  if (!trackingStateDef) {
+    logger.error('Unable to find the tracking state definition');
+    throw new MiddlewareNotFound('Unable to find the tracking state definition', req, res, 'trackingStateDefinitionNotFound');
+  }
 
-      // POG found, next()
-      if(result !== null) {
-        req.definition = result;
-        next();
-      }
-    },
-    (error) => {
-      console.log(error);
-      throw new MiddlewareQueryFailed("Unable to looking the requested state definition.", req, res, "failedTrackingStateDefinitionMiddlewareQuery");
-    }
-  );
+  req.definition = trackingStateDef;
+  return next();
 };
