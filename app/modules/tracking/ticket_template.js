26352aa2c0b98064018b9018542c3cb8dbf18a47
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-05-23 10:15:08 -0700

    Lint tracking/ticket_template.js

--- app/modules/tracking/ticket_template.js
+++ app/modules/tracking/ticket_template.js
@@ -1,152 +1,143 @@
-"use strict";
+const db = require('../../models/');
 
-const _                       = require('lodash');
-const moment                  = require('moment');
-const db                      = require('../../models/');
-const InvalidStateStatus      = require('./exceptions/InvalidStateStatus');
-const FailedCreateQuery       = require('../../models/exceptions/FailedCreateQuery');
-const logger                  = require(process.cwd() + '/lib/log');
+const logger = require('../../../lib/log');
 
-module.exports = class Ticket_Template {
-  
+class TicketTemplate {
   /**
    * Initialize tracking state object
    *
    * @param {string|object} init - Pass in either ident string or new object
-   * @param {object} options - Options object
    */
-  constructor(init, options={}) {
+  constructor(init) {
     this.instance = null;
     this.model = db.models.tracking_ticket_template;
-    
+
     // Existing instance
-    if(typeof init === "object" && typeof init.ident === "string") {
+    if (typeof init === 'object' && typeof init.ident === 'string') {
       this.instance = init;
     }
-    
-    if(init === undefined || this.instance === null) throw new Error('Unable to instantiate State Tracking Ticket Template');
-    
+
+    if (!init || !this.instance) {
+      logger.error('Unable to initiate State Tracking Ticket Template');
+      throw new Error('Unable to initiate State Tracking Ticket Template');
+    }
   }
-  
+
   /**
    * Create new ticket template
    *
    * A new template that can be invoked when generating a new ticket.
    *
-   * @param {string} state - State ID
+   * @param {string} ident - State ID
    * @param {string} name - Name of ticket template
    * @param {string} body - Body of ticket template
    *
-   * @returns {Promise} - Resolves with updated instance
+   * @returns {Promise.<object>} - Returns updated instance
    */
-  create(state, name, body) {
-    
-    return new Promise((resolve, reject) => {
-      
-      // Convert state ident into model object
-      db.models.tracking_state_definition.findOne({where: {ident: state}})
-        // Handle found state
-        .then((state) => {
-          return new Promise((resolve, reject) => {
-            if(state === null) return reject({message: 'failed to find the requested state'});
-            resolve(state);
-          });
-        })
-        .then((state) => {
-          // Create new instance
-          let template = {
-            state_id: state.id,
-            name: name,
-            body: body
-          };
-          
-          // Return model create promise
-          return this.model.create(template);
-        })
-        .then((template) => {
-          // All done, save instance and send template
-          this.instance = template;
-          resolve(template);
-        })
-        .catch((err) => {
-          console.log('Failed to create new ticket template', err);
-          reject({message: 'failed to create new ticket template: ' + err.message, cause: err});
-        });
-      
-    });
-    
+  async create(ident, name, body) {
+    // Convert state ident into model object
+    let state;
+    try {
+      state = await db.models.tracking_state_definition.findOne({where: {ident}});
+    } catch (error) {
+      logger.error(`Failed to get tracking state definition ${error}`);
+      throw new Error(`Failed to get tracking state definition error: ${error}`);
+    }
+
+    if (!state) {
+      logger.error('Failed to find the requested state');
+      throw new Error('Failed to find the requested state');
+    }
+
+    const template = {
+      state_id: state.id,
+      name,
+      body,
+    };
+
+    let newInstance;
+    try {
+      newInstance = await this.model.create(template);
+    } catch (error) {
+      logger.error(`Failed to create new ticket template ${error}`);
+      throw new Error(`Failed to create new ticket template error: ${error}`);
+    }
+
+    this.instance = newInstance;
+    return this.instance;
   }
-  
-  
+
   /**
    * Update the current ticket template
    *
    * @param {string} template - Template object
-   *
-   * @returns {Promise}
+   * @returns {Promise.<object>} - Returns updated instance
    */
-  update(template) {
-    return new Promise((resolve, reject) => {
-  
-      if(template.name) this.instance.name = template.name;
-      if(template.body) this.instance.body = template.body;
-      if(template.summary) this.instance.summary = template.summary;
-      if(template.project) this.instance.project = template.project;
-      if(template.components) this.instance.components = template.components;
-      if(template.tags) this.instance.tags = template.tags;
-      if(template.priority) this.instance.priority = template.priority;
-      if(template.issueType) this.instance.issueType = template.issueType;
-      if(template.security) this.instance.security = template.security;
-      
-      this.model.update(template, {where: { ident: this.instance.ident}})
-        .then((instance) => {
-          resolve(this.instance);
-        })
-        .catch((err) => {
-          reject({message: 'Failed to update ticket template: ' + err.message, cause: err});
-        });
-      
-    });
-    
+  async update(template) {
+    if (template.name) {
+      this.instance.name = template.name;
+    }
+    if (template.body) {
+      this.instance.body = template.body;
+    }
+    if (template.summary) {
+      this.instance.summary = template.summary;
+    }
+    if (template.project) {
+      this.instance.project = template.project;
+    }
+    if (template.components) {
+      this.instance.components = template.components;
+    }
+    if (template.tags) {
+      this.instance.tags = template.tags;
+    }
+    if (template.priority) {
+      this.instance.priority = template.priority;
+    }
+    if (template.issueType) {
+      this.instance.issueType = template.issueType;
+    }
+    if (template.security) {
+      this.instance.security = template.security;
+    }
+
+    try {
+      await this.model.update(template, {where: {ident: this.instance.ident}});
+      return this.instance;
+    } catch (error) {
+      logger.error(`Failed to update ticket template ${error}`);
+      throw new Error(`Failed to update ticket template error: ${error}`);
+    }
   }
-  
- 
-  
+
   /**
    * Get full public version of this instance
    *
-   * @returns {Promise}
+   * @returns {Promise.<object>} - Returns public instance
    */
-  getPublic() {
-    return new Promise((resolve, reject) => {
-      
-      let opts = {
-        where: {
-          ident: this.instance.ident,
-        },
-        attributes: {
-          exclude: ['deletedAt', 'id', 'definition_id']
-        },
-        include: [
-          {as: 'definition', model: db.models.tracking_state_definition.scope('public')},
-        ]
-      };
-      
-      // Get updated public state with nested tasks
-      this.model.scope('public').findOne(opts).then(
-        (template) => {
-          resolve(template);
-        },
-        (err) => {
-          console.log(err);
-          reject({message: 'Unable to get updated state.'});
-          throw new Error('failed to get updated state.');
-        }
-      );
-      
-    });
-    
+  async getPublic() {
+    const opts = {
+      where: {
+        ident: this.instance.ident,
+      },
+      attributes: {
+        exclude: ['deletedAt', 'id', 'definition_id'],
+      },
+      include: [
+        {as: 'definition', model: db.models.tracking_state_definition.scope('public')},
+      ],
+    };
+
+    // Get updated public state with nested tasks
+    try {
+      const template = await this.model.scope('public').findOne(opts);
+      return template;
+    } catch (error) {
+      logger.error(`Failed to get updated state ${error}`);
+      throw new Error(`Failed to get updated state ${error}`);
+    }
   }
-  
-  
-};
+}
+
+module.exports = TicketTemplate;
