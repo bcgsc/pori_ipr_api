a57d710700c6e147c6c4bfc4ecfee6188c09aebb
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-06-10 08:59:03 -0700

    Remove template comments

--- migrations/20190607210854-pog_patient_information-foreign-key-constraint-migration.js
+++ migrations/20190607210854-pog_patient_information-foreign-key-constraint-migration.js
@@ -2,17 +2,10 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return queryInterface.addConstraint('pog_patient_information', ['pog_report_id'], {
       type: 'foreign key',
       name: 'pog_report_id_foreign_key_constraint',
-      references: { //Required field
+      references: {
         table: 'pog_analysis_reports',
         field: 'id',
       },
@@ -22,13 +15,6 @@ module.exports = {
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
    return queryInterface.removeConstraint('pog_patient_information', 'pog_report_id_foreign_key_constraint', {});
   }
 };
