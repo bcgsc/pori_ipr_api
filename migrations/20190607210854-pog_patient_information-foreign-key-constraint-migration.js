e84cd5d89b58b2e204f17f5e0fe6fdec5e19ba91
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-06-11 14:45:07 -0700

    Add not null constraint to pog_report_id column in migration file

--- migrations/20190607210854-pog_patient_information-foreign-key-constraint-migration.js
+++ migrations/20190607210854-pog_patient_information-foreign-key-constraint-migration.js
@@ -2,19 +2,31 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.addConstraint('pog_patient_information', ['pog_report_id'], {
-      type: 'foreign key',
-      name: 'pog_report_id_foreign_key_constraint',
-      references: {
-        table: 'pog_analysis_reports',
-        field: 'id',
-      },
-      onDelete: 'cascade',
-      onUpdate: 'cascade',
-    });
+    return Promise.all([
+      queryInterface.addConstraint('pog_patient_information', ['pog_report_id'], {
+        type: 'foreign key',
+        name: 'pog_report_id_foreign_key_constraint',
+        references: {
+          table: 'pog_analysis_reports',
+          field: 'id',
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      }),
+      queryInterface.changeColumn('pog_patient_information', 'pog_report_id', {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      })
+    ]);
   },
 
   down: function (queryInterface, Sequelize) {
-   return queryInterface.removeConstraint('pog_patient_information', 'pog_report_id_foreign_key_constraint', {});
+   return Promise.all([
+     queryInterface.removeConstraint('pog_patient_information', 'pog_report_id_foreign_key_constraint', {}),
+     queryInterface.changeColumn('pog_patient_information', 'pog_report_id', {
+       type: Sequelize.INTEGER,
+       allowNull: true,
+     }),
+   ]);
   }
 };
