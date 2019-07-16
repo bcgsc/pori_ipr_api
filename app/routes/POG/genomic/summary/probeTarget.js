fb9125f74aae494c10ae092e95d21904756c02da
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-07-16 15:05:15 -0700

    Lint probe target

--- app/routes/POG/genomic/summary/probeTarget.js
+++ app/routes/POG/genomic/summary/probeTarget.js
@@ -1,78 +1,76 @@
-// app/routes/summary/probeTarget.js
-let express = require('express'),
-    router = express.Router({mergeParams: true}),
-    db = require(process.cwd() + '/app/models'),
-  logger = require(process.cwd() + '/app/libs/logger');
+const express = require('express');
 
-router.param('target', (req,res,next,altIdent) => {
-   db.models.probeTarget.findOne({ where: {ident: altIdent}, attributes: {exclude: ['id', '"deletedAt"']}}).then(
-      (result) => {
-        if(result == null) return res.status(404).json({error: {message: 'Unable to locate the requested resource.', code: 'failedMiddlewareProbeTargetLookup'} });
-        
-        req.target = result;
-        next();
-        
-      },
-      (error) => {
-        return res.status(500).json({error: {message: 'Unable to process the request.', code: 'failedMiddlewareProbeTargetQuery'} });
-      }
-    );
+const router = express.Router({mergeParams: true});
+const db = require('../../../../models');
+
+const logger = require('../../../../../lib/log');
+
+router.param('target', async (req, res, next, altIdent) => {
+  let result;
+  try {
+    result = await db.models.probeTarget.findOne({where: {ident: altIdent}, attributes: {exclude: ['id', '"deletedAt"']}});
+  } catch (error) {
+    logger.error(`Unable to find probe target ${error}`);
+    return res.status(500).json({error: {message: 'Unable to find probe target', code: 'failedMiddlewareProbeTargetQuery'}});
+  }
+
+  if (!result) {
+    logger.error('Unable to locate probe target');
+    return res.status(404).json({error: {message: 'Unable to locate probe target', code: 'failedMiddlewareProbeTargetLookup'}});
+  }
+
+  req.target = result;
+  return next();
 });
 
 // Handle requests for alterations
 router.route('/:target([A-z0-9-]{36})')
-  .get((req,res,next) => {
-    
-    res.json(req.alteration);
-    
+  .get((req, res) => {
+    return res.json(req.alteration);
   })
-  .put((req,res,next) => {
-
+  .put(async (req, res) => {
     // Update DB Version for Entry
-    versionDatum(db.models.probeTarget, req.target, req.body, req.user).then(
-      (resp) => {
-        res.json(resp.data.create);
-      },
-      (error) => {
-        console.log(error);
-        res.status(500).json({error: {message: 'Unable to version the resource', code: 'failedMutationSummaryVersion'}});
-      }
-    );
+    try {
+      const result = await db.models.probeTarget.update(req.body, {
+        where: {
+          ident: req.target.ident,
+        },
+        paranoid: true,
+        returning: true,
+      });
 
+      return res.json(result);
+    } catch (error) {
+      logger.error(`Unable to update probe target ${error}`);
+      return res.status(500).json({error: {message: 'Unable to update probe target', code: 'failedMutationSummaryVersion'}});
+    }
   })
-  .delete((req,res,next) => {
+  .delete(async (req, res) => {
     // Soft delete the entry
-    db.models.probeTarget.destroy({ where: {ident: req.target.ident}}).then(
-      (result) => {
-        res.status(204).send();
-      },
-      (error) => {
-        res.status(500).json({error: {message: 'Unable to remove resource', code: 'failedProbeTargetremove'} });
-      }
-    );
-      
-          
+    try {
+      await db.models.probeTarget.destroy({where: {ident: req.target.ident}});
+      return res.status(204).send();
+    } catch (error) {
+      logger.error(`Unable to remove probe target ${error}`);
+      return res.status(500).json({error: {message: 'Unable to remove probe target', code: 'failedProbeTargetremove'}});
+    }
   });
 
 // Routing for Alteration
 router.route('/')
-  .get((req,res,next) => {
-
-    let options = {
+  .get(async (req, res) => {
+    const options = {
       where: {pog_report_id: req.report.id},
     };
-    
+
     // Get all rows for this POG
-    db.models.probeTarget.scope('public').findAll(options).then(
-      (result) => {
-        res.json(result);
-      },
-      (error) => {
-        console.log(error);
-        res.status(500).json({error: {message: 'Unable to retrieve resource', code: 'failedProbeTargetlookup'} });
-      }
-    );
-      
-  })
-  
+    try {
+      const result = await db.models.probeTarget.scope('public').findAll(options);
+      return res.json(result);
+    } catch (error) {
+      logger.error(`Unable to get probe targets ${error}`);
+      return res.status(500).json({error: {message: 'Unable to get probe targets', code: 'failedProbeTargetlookup'}});
+    }
+  });
+
 module.exports = router;
