d0be8b6e1c49c880db16e8687cc1f6b857689c38
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-07-23 15:09:04 -0700

    Add version info to swagger doc

--- app/routes/swagger/swaggerSpec.js
+++ app/routes/swagger/swaggerSpec.js
@@ -3,6 +3,8 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
 const swaggerDocument = YAML.load('./swagger.yaml');
+// Add API version info to swagger doc
+swaggerDocument.info.version = process.env.npm_package_version;
 
 const router = express.Router({mergeParams: true});
 
--- app/routes/swagger/swaggerSpecJson.js
+++ app/routes/swagger/swaggerSpecJson.js
@@ -2,6 +2,8 @@ const express = require('express');
 const YAML = require('yamljs');
 
 const swaggerDocument = YAML.load('./swagger.yaml');
+// Add API version info to swagger doc
+swaggerDocument.info.version = process.env.npm_package_version;
 
 const router = express.Router({mergeParams: true});
 
