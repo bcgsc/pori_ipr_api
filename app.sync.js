83ab6fe6c2ab092b2f14aa048c10c01a0bec2fbd
Caralyn Reisle <creisle@bcgsc.ca>
2019-09-03 16:25:49 -0700

    Lint and remove commented out code

--- app.sync.js
+++ app.sync.js
@@ -1,10 +1,11 @@
-"use strict";
+
+
 /*
  IPR-API - Integrated Pipeline Reports API
 
  COPYRIGHT 2016 MICHAEL SMITH GENOME SCIENCES CENTRE
  CONFIDENTIAL -- FOR RESEARCH PURPOSES ONLY
- 
+
  Author: Brandon Pierce <bpierce@bcgsc.ca>
  Support JIRA ticket space: DEVSU
 
@@ -18,29 +19,23 @@
 
 
 // API Version
-const API_VERSION   = '1.0';
-const minimist      = require('minimist');
+const API_VERSION = '1.0';
+const minimist = require('minimist');
 
 
 // Set environment based on config first.
-if(process.env.NODE_ENV === undefined || process.env.NODE_ENV === null) {
+if (process.env.NODE_ENV === undefined || process.env.NODE_ENV === null) {
   // Get from command line args
   const args = minimist(process.argv.slice(2));
-  if(args.env) process.env.NODE_ENV = args.env;
-  if(!args.env) process.env.NODE_ENV = 'production';
+  if (args.env) process.env.NODE_ENV = args.env;
+  if (!args.env) process.env.NODE_ENV = 'production';
 }
-const CONFIG        = require('./config/'+process.env.NODE_ENV+'.json');
+const CONFIG = require(`./config/${process.env.NODE_ENV}.json`);
 
 // Call packages required
-const bodyParser    = require('body-parser');   // Body parsing lib
-const colors        = require('colors');        // Console colours
-const fs            = require('fs');            // File System access
-const nconf         = require('nconf').argv().env().file({file: './config/config.json'});
+const bodyParser = require('body-parser'); // Body parsing lib
+const colors = require('colors'); // Console colours
+const fs = require('fs'); // File System access
+const nconf = require('nconf').argv().env().file({file: './config/config.json'});
 
-//const limsPathology = require(process.cwd() + '/app/modules/tracking/syncronizers/limsPathology');
-//const limsSequencing = require(process.cwd() + '/app/modules/tracking/syncronizers/limsSequencing');
-//const bioappsSync = require(process.cwd() + '/app/modules/tracking/syncronizers/BioApps');
-//const TestSyncro = require(process.cwd() + '/app/synchronizer/test');
-
-const TrackingSync = require(process.cwd() + '/app/modules/tracking/synchronizers')();
-
+const TrackingSync = require(`${process.cwd()}/app/modules/tracking/synchronizers`)();
