73cf28b19611046b714ba129f21090ab3ae6c7e1
Caralyn Reisle <creisle@bcgsc.ca>
2019-10-24 11:31:06 -0700

    Remove obsolete method

--- app/routes/routingInterface.js
+++ app/routes/routingInterface.js
@@ -27,45 +27,12 @@ class RoutingInterface {
   }
 
   /**
-   * Bind a valid Express router object to the current router
-   *
-   * @param {string} path - Route to use specified middleware
-   * @param {object} middleware - Middleware function to use
-   *
-   * @returns {Router} - Returns the instance of the express router
-   */
-  bindRouteObject(path, middleware) {
-    logger.info(`Route Registered: ${path}`);
-    return this.router.use((this.root) ? this.root : path, middleware);
-  }
-
-  /**
    * Retrieve prepared router
    *
    * @returns {Router} - Returns the instance of the express router
    */
   getRouter() {
     return this.router;
-  }
-
-  /**
-   * Register a single route with the router
-   *
-   * Provided map a URL and method to a handler function
-   *
-   * @param {string} method - HTTP method such as GET, PUT, POST, and so on, in lowercase
-   * @param {string} path - Route to use
-   * @param {function} handler - Handler function for endpoint
-   *
-   * @returns {Router} - Returns the instance of the express router
-   */
-  router[method](path, handler) {
-    if (!this.allowedMethods.includes(method)) {
-      throw new MethodNotAllowed(`The requested method: ${method} is not allowed. Allowable methods: ${this.allowedMethods.join(',')}`);
-    }
-
-    logger.info(`Route Registered: ${path}`);
-    return this.router[method](path, handler);
   }
 
 
