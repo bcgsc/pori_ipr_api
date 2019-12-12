2c74a67b672088c231c2e2da89dfa5083831b5f9
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2019-12-12 14:47:55 -0800

    Add exclude file with common fields to exclude from a schema

---
+++ app/schemas/exclude.js
@@ -0,0 +1,7 @@
+const BASE_EXCLUDE = ['id', 'ident', 'createdAt', 'updatedAt', 'deletedAt'];
+const REPORT_EXCLUDE = BASE_EXCLUDE.concat(['pog_id', 'pog_report_id']);
+
+module.exports = {
+  BASE_EXCLUDE,
+  REPORT_EXCLUDE,
+};
