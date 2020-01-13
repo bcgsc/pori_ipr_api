fd5366164018573f07ad961e4da57b41a82b7066
Raphael Pletz <rpletz@bcgsc.ca>
2020-01-13 13:59:00 -0800

    Migrate changeHistory to jest

--- test/changeHistory.test.js
+++ test/changeHistory.test.js
@@ -1,14 +1,9 @@
 process.env.NODE_ENV = 'test';
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const request = require('supertest');
 const uuidv4 = require('uuid/v4');
 const getPort = require('get-port');
 
-chai.should();
-
-chai.use(chaiHttp);
-chai.use(require('chai-things'));
 
 // get test user info
 const CONFIG = require('../app/config');
@@ -31,7 +26,7 @@ const update = {
 
 let server;
 // Start API
-before(async () => {
+beforeAll(async () => {
   const port = await getPort({port: CONFIG.get('web:port')});
   server = await listen(port);
 });
@@ -40,79 +35,76 @@ before(async () => {
 describe('Tests for update changes', () => {
   let ident;
 
-  before(async () => {
+  beforeAll(async () => {
     // create project record
-    let res = await chai.request(server)
+    let res = await request(server)
       .post('/api/1.0/project')
       .auth(username, password)
       .type('json')
-      .send(projectData);
-    res.should.have.status(201);
+      .send(projectData)
+      .expect(201);
     ident = res.body.ident;
 
 
     // check that the created project record exists
-    res = await chai.request(server)
+    res = await request(server)
       .get(`/api/1.0/project/search?query=${projectData.name}`)
       .auth(username, password)
-      .type('json');
+      .type('json')
+      .expect(200);
 
-    res.should.have.status(200);
     res.body = res.body[0];
 
-    res.body.should.have.property('ident');
-    res.body.should.have.property('name');
-    res.body.should.have.property('createdAt');
-    res.body.should.have.property('updatedAt');
+    expect(res.body).toHaveProperty('ident');
+    expect(res.body).toHaveProperty('name');
+    expect(res.body).toHaveProperty('createdAt');
+    expect(res.body).toHaveProperty('updatedAt');
 
-    res.body.ident.should.equal(ident);
+    expect(res.body.ident).toEqual(ident);
   });
 
   // Test update changes
-  it('Test update changes', async () => {
+  test('Test update changes', async () => {
     // update project name for given ident
-    let res = await chai.request(server)
+    let res = await request(server)
       .put(`/api/1.0/project/${ident}`)
       .auth(username, password)
       .type('json')
-      .send({...update});
-
-    res.should.have.status(200);
+      .send({...update})
+      .expect(200);
 
     // get updated project and compare to update values
-    res = await chai.request(server)
+    res = await request(server)
       .get(`/api/1.0/project/search?query=${update.name}`)
       .auth(username, password)
-      .type('json');
+      .type('json')
+      .expect(200);
 
-    res.should.have.status(200);
-    res.body.should.be.a('array');
+    expect(Array.isArray(res.body));
     res.body = res.body[0];
 
     // Should equal updated values
-    res.body.name.should.equal(update.name);
+    expect(res.body.name).toEqual(update.name);
   });
 
   // Remove newly created/updated project
-  after(async () => {
+  afterAll(async () => {
     // delete newly created project
-    let res = await chai.request(server)
+    let res = await request(server)
       .delete(`/api/1.0/project/${ident}`)
       .auth(username, password)
-      .type('json');
-
-    res.should.have.status(204);
+      .type('json')
+      .expect(204);
 
     // verify project is deleted
-    res = await chai.request(server)
+    res = await request(server)
       .get(`/api/1.0/POG/project/search?query=${projectData.name}`)
       .auth(username, password)
-      .type('json');
-
-    res.should.have.status(404);
+      .type('json')
+      .expect(404);
   });
 });
 
-after(async () => {
+afterAll(async () => {
   await server.close();
 });
