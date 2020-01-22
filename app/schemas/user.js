07c15688a05ba6de271b820a6f19a5198aba916b
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-01-22 10:17:01 -0800

    Do not allow additional properties in user schema and fix constant variable syntax

--- app/schemas/user.js
+++ app/schemas/user.js
@@ -6,9 +6,9 @@ const schemaConfig = require('../../config/schema');
 const schemaManager = new JsonSchemaManager();
 
 // user specific excluded fields from model
-const userExcludes = ['password', 'jiraToken', 'jiraXsrf', 'settings', 'access', 'lastLogin'];
+const USER_EXCLUDES = ['password', 'jiraToken', 'jiraXsrf', 'settings', 'access', 'lastLogin'];
 
-const exclude = BASE_EXCLUDE.concat(userExcludes);
+const exclude = BASE_EXCLUDE.concat(USER_EXCLUDES);
 
 const schema = schemaManager.generate(db.models.user, new JsonSchema7Strategy(), {
   exclude,
@@ -16,5 +16,6 @@ const schema = schemaManager.generate(db.models.user, new JsonSchema7Strategy(),
 });
 
 schema.$schema = schemaConfig.version;
+schema.additionalProperties = false;
 
 module.exports = schema;
