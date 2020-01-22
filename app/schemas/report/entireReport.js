11932a0808e2bf426e20aed95973aa46aa800e2b
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-01-22 11:18:54 -0800

    Add entire report upload schema

--- app/schemas/report/entireReport.js
+++ app/schemas/report/entireReport.js
@@ -1,17 +1,50 @@
 const {JsonSchemaManager, JsonSchema7Strategy} = require('@alt3/sequelize-to-json-schemas');
 const db = require('../../models');
-//const {BASE_EXCLUDE} = require('./exclude');
+const {BASE_EXCLUDE} = require('../exclude');
+const schemaGenerator = require('./basicReportSchemaGenerator');
 const schemaConfig = require('../../../config/schema');
 
 const schemaManager = new JsonSchemaManager();
 
+const REPORT_EXCLUDE = ['pog_id', 'analysis_id', 'createdBy_id'];
+const exclude = BASE_EXCLUDE.concat(REPORT_EXCLUDE);
+
 const schema = schemaManager.generate(db.models.analysis_report, new JsonSchema7Strategy(), {
-  //exclude: BASE_EXCLUDE,
+  exclude,
   associations: true,
-  //includeAssociations: ['smallMutations'],
+  excludeAssociations: ['ReportUserFilter', 'createdBy', 'probe_signature', 'presentation_discussion', 'presentation_slides', 'users', 'analystComments'],
 });
 
+// set schema version and don't allow additional properties
 schema.$schema = schemaConfig.version;
 schema.additionalProperties = false;
+
+// inject all required associations into schema
+schema.properties.project = {
+  type: 'string',
+  description: 'Project name',
+};
+schema.required.push('project');
+
+// inject image directory
+schema.properties.imagesDirectory = {
+  type: 'string',
+  description: 'Absolute path to images directory',
+};
+
+// get report associations
+const {ReportUserFilter, createdBy, probe_signature, presentation_discussion, presentation_slides, users, analystComments, ...associations} = db.models.analysis_report.associations;
+
+schema.definitions = {};
+// add all associated schemas
+Object.values(associations).forEach((association) => {
+  const model = association.target.name;
+
+  const generatedSchema = schemaGenerator(db.models[model]);
+  // remove association schema draft versions
+  delete generatedSchema.$schema;
+
+  schema.definitions[model] = generatedSchema;
+});
 
 module.exports = schema;
