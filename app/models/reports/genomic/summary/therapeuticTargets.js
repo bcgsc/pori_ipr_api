76a491afcae1e379b3fa76df2cc789100c24eedf
Caralyn Reisle <creisle@bcgsc.ca>
2020-01-23 14:50:02 -0800

    Set up new columns

--- app/models/reports/genomic/summary/therapeuticTargets.js
+++ app/models/reports/genomic/summary/therapeuticTargets.js
@@ -2,57 +2,81 @@ const Sq = require('sequelize');
 
 const {DEFAULT_COLUMNS, DEFAULT_OPTIONS} = require('../../../base');
 
-module.exports = sequelize => sequelize.define('therapeuticTarget', {
-  ...DEFAULT_COLUMNS,
-  pog_id: {
-    type: Sq.INTEGER,
-    references: {
-      model: 'POGs',
-      key: 'id',
+module.exports = (sequelize) => {
+  return sequelize.define('therapeuticTarget', {
+    ...DEFAULT_COLUMNS,
+    report_id: {
+      type: Sq.INTEGER,
+      references: {
+        model: 'pog_analysis_reports',
+        key: 'id',
+      },
+    },
+    type: {
+      type: Sq.ENUM('therapeutic', 'chemoresistance'),
+      allowNull: false,
+    },
+    rank: {
+      type: Sq.INTEGER,
+      defaultValue: 0,
+    },
+    gene: {
+      type: Sq.STRING,
+      allowNull: true,
+      defaultValue: null,
+    },
+    gene_graphkb_id: {
+      type: Sq.STRING,
+      defaultValue: null,
+      allowNull: true,
+    },
+    variant: {
+      type: Sq.STRING,
+      allowNull: false,
+    },
+    variant_graphkb_id: {
+      type: Sq.STRING,
+      defaultValue: null,
+      allowNull: true,
+    },
+    therapy: {
+      type: Sq.STRING,
+      allowNull: false,
+    },
+    therapy_graphkb_id: {
+      type: Sq.STRING,
+      defaultValue: null,
+      allowNull: true,
+    },
+    context: {
+      type: Sq.STRING,
+      allowNull: false,
+    },
+    context_graphkb_id: {
+      type: Sq.STRING,
+      allowNull: true,
+      defaultValue: null,
+    },
+    evidence_level: {
+      type: Sq.STRING,
+      allowNull: false,
+    },
+    evidence_level_graphkb_id: {
+      type: Sq.STRING,
+      allowNull: true,
+      defaultValue: null,
+    },
+    notes: {
+      type: Sq.TEXT,
     },
   },
-  report_id: {
-    type: Sq.INTEGER,
-    references: {
-      model: 'pog_analysis_reports',
-      key: 'id',
+  {
+    ...DEFAULT_OPTIONS,
+    tableName: 'pog_analysis_reports_therapeutic_targets',
+    scopes: {
+      public: {
+        attributes: {exclude: ['id', 'deletedAt', 'report_id']},
+      },
     },
-  },
-  type: {
-    type: Sq.ENUM('therapeutic', 'chemoresistance'),
-    allowNull: false,
-  },
-  rank: {
-    type: Sq.INTEGER,
-    defaultValue: 0,
-  },
-  target: {
-    type: Sq.JSONB,
-    allowNull: true,
-  },
-  targetContext: {
-    type: Sq.TEXT,
-    allowNull: true,
-  },
-  resistance: {
-    type: Sq.TEXT,
-    allowNull: true,
-  },
-  biomarker: {
-    type: Sq.JSONB,
-    allowNull: true,
-    defaultValue: [],
-  },
-  notes: {
-    type: Sq.TEXT,
-  },
-},
-{
-  ...DEFAULT_OPTIONS,
-  tableName: 'pog_analysis_reports_therapeutic_targets',
-  scopes: {
-    public: {
-      attributes: {exclude: ['id', 'deletedAt', 'report_id', 'pog_id']},
-    },
-  },
-});
+  });
+};
