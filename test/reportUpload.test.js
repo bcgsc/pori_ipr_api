3d71cc37673bc9675c1dcceb464a138603ccd8c8
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-01-23 14:40:57 -0800

    Convert report upload test to be compatable with jest

--- test/reportUpload.test.js
+++ test/reportUpload.test.js
@@ -1,16 +1,10 @@
 process.env.NODE_ENV = 'test';
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const supertest = require('supertest');
 const getPort = require('get-port');
 const db = require('../app/models');
 
 const mockReportData = require('./mockReportData.json');
-
-const {expect} = chai;
-
-chai.use(chaiHttp);
-chai.use(require('chai-things'));
 
 const CONFIG = require('../app/config');
 const {listen} = require('../app');
@@ -20,11 +14,13 @@ CONFIG.set('env', 'test');
 const {username, password} = CONFIG.get('testing');
 
 let server;
+let request;
 
 // Start API
-before(async () => {
+beforeAll(async () => {
   const port = await getPort({port: CONFIG.get('web:port')});
   server = await listen(port);
+  request = supertest(server);
 });
 
 // Tests for uploading a report and all of its components
@@ -33,40 +29,38 @@ describe('Tests for uploading a report and all of its components', () => {
   let pogId;
   let reportIdent;
 
-  before(async () => {
+  beforeAll(async () => {
     // create report
-    let res = await chai.request(server)
+    let res = await request
       .post('/api/1.0/reports')
       .auth(username, password)
       .send(mockReportData)
-      .type('json');
+      .type('json')
+      .expect(200);
 
-    expect(res).to.have.status(200);
-    expect(res.body).to.be.an('object');
+    expect(typeof res.body).toBe('object');
 
     reportIdent = res.body.ident;
 
     // check that the report was created
-    res = await chai.request(server)
+    res = await request
       .get(`/api/1.0/reports/${reportIdent}`)
       .auth(username, password)
-      .type('json');
-
-    expect(res).to.have.status(200);
+      .type('json')
+      .expect(200);
 
     // get report id from patient info. because it's excluded in public view
     reportId = res.body.patientInformation.report_id;
   });
 
   // Test that all components were created
-  it('Test that all components were created', async () => {
+  test('Test that all components were created', async () => {
     // check that the POG was created by searching by POGID
-    const res = await chai.request(server)
+    const res = await request
       .get(`/api/1.0/POG/${mockReportData.pog.POGID}`)
       .auth(username, password)
-      .type('json');
-
-    expect(res).to.have.status(200);
+      .type('json')
+      .expect(200);
 
     pogId = res.body.id;
 
@@ -74,8 +68,8 @@ describe('Tests for uploading a report and all of its components', () => {
     // based on pog_id and analysis_biopsy
     const pogAnalysis = await db.models.pog_analysis.findOne({where: {pog_id: pogId, analysis_biopsy: mockReportData.analysis.analysis_biopsy}});
 
-    expect(pogAnalysis).to.be.an('object');
-    expect(pogAnalysis).to.not.be.null;
+    expect(pogAnalysis).not.toBeNull();
+    expect(typeof pogAnalysis).toBe('object');
 
     // for all components, do a find where report_id
     // is the same as the created report id
@@ -92,27 +86,26 @@ describe('Tests for uploading a report and all of its components', () => {
 
     // results should be a non-empty array
     components.forEach((component) => {
-      expect(component).to.be.an('array');
-      expect(component).to.not.be.empty;
+      expect(typeof component).toBe('array');
+      expect(component.length).toBeGreaterThan(0);
     });
   });
 
   // delete report
-  after(async () => {
+  afterAll(async () => {
     // delete newly created report and all of it's components
     // indirectly by hard deleting newly created patient
     await db.models.POG.destroy({where: {POGID: mockReportData.pog.POGID}, force: true});
 
     // verify report is deleted
-    const res = await chai.request(server)
+    await request
       .get(`/api/1.0/reports/${reportIdent}`)
       .auth(username, password)
-      .type('json');
-
-    expect(res).to.have.status(404);
+      .type('json')
+      .expect(404);
   });
 });
 
-after(async () => {
+afterAll(async () => {
   await server.close();
 });
