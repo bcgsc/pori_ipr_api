7deedf57eb3c5110e9377a7dc48edb235ecb1197
Raphael Pletz <rpletz@bcgsc.ca>
2020-01-27 09:23:05 -0800

    Test /me endpoint

--- test/user.test.js
+++ test/user.test.js
@@ -50,6 +50,22 @@ describe('/user', () => {
     expect(res.body[0]).toHaveProperty('firstName');
     expect(res.body[0]).toHaveProperty('lastName');
   });
+  // Test for GET /user/me 200 endpoint
+  test('GET current user /me', async () => {
+    const res = await request
+      .get('/api/1.0/user/me')
+      .auth(username, password)
+      .type('json')
+      .expect(200);
+
+    expect(typeof (res.body)).toBe('object');
+    expect(res.body).toHaveProperty('id');
+    expect(res.body).toHaveProperty('ident');
+    expect(res.body).toHaveProperty('username');
+    expect(res.body).toHaveProperty('type');
+    expect(res.body).toHaveProperty('email');
+    expect(res.body).toHaveProperty('firstName');
+    expect(res.body).toHaveProperty('lastName');
 });
 
 afterAll(async () => {
