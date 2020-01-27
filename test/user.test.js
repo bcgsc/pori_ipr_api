00509444c9c2d3aaf6ff1cba3839797855b3ccc5
Raphael Pletz <rpletz@bcgsc.ca>
2020-01-27 09:26:18 -0800

    Test 4XX endpoints

--- test/user.test.js
+++ test/user.test.js
@@ -78,6 +78,89 @@ describe('/user', () => {
 
     newUserIdent = res.body.ident;
   });
+  // Test for POST /user 409 endpoint
+  test('POST new user - Already existing', async () => {
+    await request
+      .post('/api/1.0/user')
+      .auth(username, password)
+      .type('json')
+      .send(newUser)
+      .expect(409);
+  });
+  // Test for POST /user 400 endpoint
+  test('POST new user - Password is required for local', async () => {
+    await request
+      .post('/api/1.0/user')
+      .auth(username, password)
+      .type('json')
+      .send({
+        username: 'Testuser',
+        type: 'local',
+        email: 'testuser@test.com',
+        firstName: 'FirstNameTest',
+        lastName: 'LastNameTest',
+      })
+      .expect(400);
+  });
+  // Test for POST /user 400 endpoint
+  test('POST new user - Password longer than 8 characters', async () => {
+    await request
+      .post('/api/1.0/user')
+      .auth(username, password)
+      .type('json')
+      .send({
+        username: 'Testuser',
+        type: 'local',
+        password: 'SMLLPD',
+        email: 'testuser@test.com',
+        firstName: 'FirstNameTest',
+        lastName: 'LastNameTest',
+      })
+      .expect(400);
+  });
+  // Test for POST /user 400 endpoint
+  test('POST new user - Email should be valid', async () => {
+    await request
+      .post('/api/1.0/user')
+      .auth(username, password)
+      .type('json')
+      .send({
+        username: 'Testuser',
+        type: 'bcgsc',
+        email: 'NOT_EMAIL',
+        firstName: 'FirstNameTest',
+        lastName: 'LastNameTest',
+      })
+      .expect(400);
+  });
+  // Test for POST /user 400 endpoint
+  test('POST new user - Type should be eitheir local or bcgsc', async () => {
+    await request
+      .post('/api/1.0/user')
+      .auth(username, password)
+      .type('json')
+      .send({
+        username: 'Testuser',
+        type: 'NOT_VALID_TYPE',
+        email: 'testuser@test.com',
+        firstName: 'FirstNameTest',
+        lastName: 'LastNameTest',
+      })
+      .expect(400);
+  });
+  // Test for POST /user 400 endpoint
+  test('POST new user - All fields are required', async () => {
+    await request
+      .post('/api/1.0/user')
+      .auth(username, password)
+      .type('json')
+      .send({
+        username: 'Testuser',
+        type: 'bcgsc',
+        email: 'testuser@test.com',
+      })
+      .expect(400);
+  });
   // Test for DELETE /user/ident 204 endpoint
   test('DELETE user - Success', async () => {
     await request
