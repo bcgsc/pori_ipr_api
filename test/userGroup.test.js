bd3a8d459e56afca61509511553ece6ee47b6aac
Raphael Pletz <rpletz@bcgsc.ca>
2020-02-04 13:37:57 -0800

    Test /user/group GET endpoint

--- test/userGroup.test.js
+++ test/userGroup.test.js
@@ -23,8 +23,30 @@ beforeAll(async () => {
 });
 
 // Tests for user related endpoints
-describe('', () => {
+describe('/user/group', () => {
+  describe('GET', () => {
+    // Test for GET /user 200 endpoint
+    test('GET / list of groups', async () => {
+      const res = await request
+        .get('/api/1.0/user/group')
+        .auth(username, password)
+        .type('json')
+        .expect(HTTP_STATUS.OK);
 
+      expect(res.body).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            ident: expect.any(String),
+            name: expect.any(String),
+            owner_id: expect.any(Number),
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String),
+            users: expect.any(Array),
+          }),
+        ])
+      );
+    });
+  });
 });
 
 afterAll(async () => {
