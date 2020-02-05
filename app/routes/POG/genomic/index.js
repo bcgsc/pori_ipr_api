c240df536e78667e94beb74b0784422bc6e8cec1
Caralyn Reisle <creisle@bcgsc.ca>
2020-02-04 18:04:24 -0800

    Maintain probe-specific paths
    - Until the client can update, allow both the probe and genomic paths to
      the same route handler (otherwise identical)

--- app/routes/POG/genomic/index.js
+++ app/routes/POG/genomic/index.js
@@ -10,18 +10,29 @@ const presentation = require('./presentation');
 const somaticMutations = require('./somaticMutations');
 const structuralVariation = require('./structuralVariation');
 const therapeuticTargets = require('./therapeuticTargets');
+const probeTestInformation = require('./probeTestInformation');
+const genomicEventsTherapeutic = require('./summary/genomicEventsTherapeutic');
 
 const router = express.Router({mergeParams: true});
 
 router.use('/summary', summary);
+
+// TODO: remove line below when the client has updated to match
+router.use('/genomicEventsTherapeutic', genomicEventsTherapeutic);
+
 router.use('/appendices', appendices);
 router.use('/copyNumberAnalyses', copyNumberAnalyses);
-router.use('/detailedGenomicAnalysis', detailedGenomicAnalysis);
+router.use('/detailedGenomicAnalysis', detailedGenomicAnalysis); // TODO: remove line when the client has been updated to match
 router.use('/expressionAnalysis', expressionAnalysis);
 router.use('/mavis', mavis);
 router.use('/presentation', presentation);
 router.use('/somaticMutations', somaticMutations);
 router.use('/structuralVariation', structuralVariation);
 router.use('/therapeuticTargets', therapeuticTargets);
+
+// TODO: remove line below when the client has updated to match
+router.use('/testInformation', probeTestInformation);
+
+router.use('/probeTestInformation', probeTestInformation);
 
 module.exports = router;
