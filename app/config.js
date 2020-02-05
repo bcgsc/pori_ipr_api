2664a2826c910d3d2c8d55abd51f09e53425ec2a
Raphael Pletz <rpletz@bcgsc.ca>
2020-02-05 10:51:12 -0800

    Revert config changes back to dev

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ const {merge} = require('lodash');
 const ENV = process.env.NODE_ENV || 'local';
 
 // set the default db name based on the node-env
-let DEFAULT_DB_NAME = 'DEVSU-803-temp-ipr-sync-dev'; // TODO: set it back to regular db and change package.json
+let DEFAULT_DB_NAME = 'ipr-sync-dev';
 
 if (ENV === 'production') {
   DEFAULT_DB_NAME = 'ipr';
