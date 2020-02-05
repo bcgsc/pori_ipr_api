98572ad7d309a70d52d0cc0921a909c1693cf04a
Caralyn Reisle <creisle@bcgsc.ca>
2020-02-04 18:05:37 -0800

    retain one-off probe-only route

--- app/routes/POG/base.js
+++ app/routes/POG/base.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const genomic = require('./genomic');
-const probe = require('./probe');
 const image = require('./image');
 const _export = require('./export');
+const signature = require('./probe/signature');
 
 const router = express.Router({mergeParams: true});
 
-router.use('/probe', probe);
 router.use('/:reportType(genomic|probe)', genomic);
+router.use('/probe/signature', signature);
 router.use('/image', image);
 router.use('/export', _export);
 
