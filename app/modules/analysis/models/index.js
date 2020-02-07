6ddd30a40f48d5a1d1ffa4904a732f43066bdd31
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-02-07 09:44:19 -0800

    Remove redundant analysis models

--- app/modules/analysis/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = (sequelize) => {
-  const analysis = sequelize.import('./analysis.model');
-  analysis.belongsTo(sequelize.models.POG, {as: 'pog', foreignKey: 'pog_id', onDelete: 'CASCADE'});
-};
