ab68efc00efacec84b6b5b97458f43da0dd6d5a1
Raphael Pletz <rpletz@bcgsc.ca>
2020-02-11 10:29:47 -0800

    Create initial setup and teardown + PUT test

--- test/analystComments.test.js
+++ test/analystComments.test.js
@@ -2,6 +2,9 @@ process.env.NODE_ENV = 'test';
 
 const supertest = require('supertest');
 const getPort = require('get-port');
+const db = require('../app/models');
+
+const mockReportData = require('./testData/mockReportData.json');
 
 // get test user info
 const CONFIG = require('../app/config');
@@ -22,9 +25,54 @@ beforeAll(async () => {
   request = supertest(server);
 });
 
-describe('', () => {
-  test('', async () => {
+describe('/POG/{POGID}/report/{REPORTID}/genomic/summary/analystComments endpoint testing', () => {
+  const pogId = mockReportData.pog.POGID;
+  let reportIdent;
 
+  beforeAll(async () => {
+    // create report
+    // TODO: Use models to create the report
+    const res = await request
+      .post('/api/1.0/reports')
+      .auth(username, password)
+      .send(mockReportData)
+      .type('json')
+      .expect(200);
+
+    expect(typeof res.body).toBe('object');
+    reportIdent = res.body.ident;
+  });
+
+  test('PUT / new comment - 200 Success', async () => {
+    // check that the report was created
+    const res = await request
+      .put(`/api/1.0/POG/${pogId}/report/${reportIdent}/genomic/summary/analystComments`)
+      .auth(username, password)
+      .type('json')
+      .send({comments: 'This is a sample comment'})
+      .expect(200);
+
+    expect(res.body).toEqual(expect.objectContaining({
+      ident: expect.any(String),
+      id: expect.any(Number),
+      comments: 'This is a sample comment',
+      pog_id: expect.any(Number),
+      report_id: expect.any(Number),
+    }));
+  });
+
+  // delete report
+  afterAll(async () => {
+    // delete newly created report and all of it's components
+    // indirectly by hard deleting newly created patient
+    await db.models.POG.destroy({where: {POGID: mockReportData.pog.POGID}, force: true});
+
+    // verify report is deleted
+    await request
+      .get(`/api/1.0/reports/${reportIdent}`)
+      .auth(username, password)
+      .type('json')
+      .expect(404);
   });
 });
 
