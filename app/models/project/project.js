04ea08e21904483b98f1d5f597291e9965fa9fbb
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-02-13 12:19:54 -0800

    Remove creating pog project relation on new project

--- app/models/project/project.js
+++ app/models/project/project.js
@@ -1,8 +1,6 @@
 const Sq = require('sequelize');
 const {DEFAULT_COLUMNS, DEFAULT_OPTIONS} = require('../base');
 
-const logger = require('../../log');
-
 module.exports = sequelize => sequelize.define('project', {
   ...DEFAULT_COLUMNS,
   name: {
@@ -29,16 +27,6 @@ module.exports = sequelize => sequelize.define('project', {
       attributes: {
         exclude: ['id', 'deletedAt'],
       },
-    },
-  },
-  hooks: {
-    afterCreate(project) {
-      // every new project should be associated with control sample COLO829
-      sequelize.models.POG.findOne({where: {POGID: 'COLO829'}})
-        .then(pog => sequelize.models.pog_project.create({project_id: project.id, pog_id: pog.id}))
-        .catch((err) => {
-          logger.error(`Unable to bind COLO829 to project: ${err}`);
-        });
     },
   },
 });
