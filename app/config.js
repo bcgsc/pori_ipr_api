53b3cfec33b457d2d7e4d376c17346d48d311027
Caralyn Reisle <creisle@bcgsc.ca>
2020-02-20 10:55:13 -0800

    Swap db back to dev

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ const {merge} = require('lodash');
 const ENV = process.env.NODE_ENV || 'local';
 
 // set the default db name based on the node-env
-let DEFAULT_DB_NAME = 'DEVSU-859-migrate-metadata-2';
+let DEFAULT_DB_NAME = 'ipr-sync-dev';
 if (ENV === 'production') {
   DEFAULT_DB_NAME = 'ipr';
 } else if (ENV === 'staging') {
