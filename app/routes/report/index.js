b759770ae91737d5559fb541da712fac0f90cc07
Raphael Pletz <rpletz@bcgsc.ca>
2020-02-26 10:58:23 -0800

    Cleanup comments

--- app/routes/report/index.js
+++ app/routes/report/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const summary = require('./summary');
 const appendices = require('./appendices');
 const copyNumberAnalyses = require('./copyNumberAnalyses');
-const detailedGenomicAnalysis = require('./detailedGenomicAnalysis');
 const expressionAnalysis = require('./expressionAnalysis');
 const mavis = require('./mavis');
 const presentation = require('./presentation');
@@ -11,14 +10,10 @@ const somaticMutations = require('./somaticMutations');
 const structuralVariation = require('./structuralVariation');
 const therapeuticTargets = require('./therapeuticTargets');
 const probeTestInformation = require('./probeTestInformation');
-const genomicEventsTherapeutic = require('./summary/genomicEventsTherapeutic');
 
 const router = express.Router({mergeParams: true});
 
 router.use('/summary', summary);
-
-// TODO: remove line below when the client has updated to match
-router.use('/genomic-events-therapeutic', genomicEventsTherapeutic);
 
 router.use('/appendices', appendices);
 router.use('/copy-number-analyses', copyNumberAnalyses);
@@ -29,13 +24,6 @@ router.use('/somatic-mutations', somaticMutations);
 router.use('/structural-variation', structuralVariation);
 router.use('/therapeutic-targets', therapeuticTargets);
 
-// TODO: remove line below when the client has updated to match
-router.use('/testInformation', probeTestInformation);
-
 router.use('/probe-test-information', probeTestInformation);
-
-// TODO: modify 2 lines below when the client has been updated to match
-router.use('/detailedGenomicAnalysis', detailedGenomicAnalysis); // genomic report pattern
-router.use('/', detailedGenomicAnalysis); // probe report pattern
 
 module.exports = router;
