b3f99c43c6b85b210cb82e1b23888b0727f5acdd
Raphael Pletz <rpletz@bcgsc.ca>
2020-02-26 09:12:10 -0800

    Move swagger.json to routes/swagger

--- app/routes/swagger/swaggerSpec.js
+++ app/routes/swagger/swaggerSpec.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../../../swagger.json');
+const swaggerDocument = require('./swagger.json');
 
 // Add API version info to swagger doc
 swaggerDocument.info.version = process.env.npm_package_version;
--- app/routes/swagger/swaggerSpecJson.js
+++ app/routes/swagger/swaggerSpecJson.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const swaggerDocument = require('../../../swagger.json');
+const swaggerDocument = require('./swagger.json');
 
 // Add API version info to swagger doc
 swaggerDocument.info.version = process.env.npm_package_version;
