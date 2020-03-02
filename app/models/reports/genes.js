4bbc3eb48cf941f6525325cdd42f42ad58e503dd
Caralyn Reisle <creisle@bcgsc.ca>
2020-03-02 13:30:21 -0800

    Add gene model

---
+++ app/models/reports/genes.js
@@ -0,0 +1,64 @@
+const Sq = require('sequelize');
+
+const {DEFAULT_COLUMNS, DEFAULT_OPTIONS} = require('../base');
+
+module.exports = (sequelize) => {
+  return sequelize.define(
+    'reports_genes',
+    {
+      ...DEFAULT_COLUMNS,
+      reportId: {
+        type: Sq.INTEGER,
+        unique: false,
+        allowNull: false,
+        field: 'report_id',
+        name: 'reportId',
+        references: {
+          model: 'pog_analysis_reports',
+          key: 'id',
+        },
+      },
+      name: {
+        type: Sq.TEXT,
+        notNull: true,
+      },
+      tumourSuppressor: {
+        type: Sq.BOOL,
+        defaultValue: false,
+        field: 'tumour_suppressor',
+        name: 'tumourSuppressor',
+      },
+      oncogene: {
+        type: Sq.BOOL,
+        defaultValue: false,
+      },
+      cancerGene: {
+        name: 'cancerGene',
+        field: 'cancer_gene',
+        type: Sq.BOOL,
+        defaultValue: false,
+      },
+      drugTargetable: {
+        name: 'drugTargetable',
+        field: 'drug_targetable',
+        type: Sq.BOOL,
+        defaultValue: false,
+      },
+    },
+    {
+      ...DEFAULT_OPTIONS,
+      indexes: [
+        ...DEFAULT_OPTIONS.indexes,
+        {
+          unique: true,
+          fields: ['report_id', 'name'],
+          where: {
+            deleted_at: {
+              [Sq.Op.eq]: null,
+            },
+          },
+        },
+      ],
+    }
+  );
+};
