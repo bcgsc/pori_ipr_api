bc1bae1a6c6f2439d4491af7fc586f08766b86f4
Raphael Pletz <rpletz@bcgsc.ca>
2020-03-02 09:26:51 -0800

    Import schemas on swaggerSpec

--- app/routes/swagger/swaggerSpec.js
+++ app/routes/swagger/swaggerSpec.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const schemas = require('../../schemas/index');
 
 // Add API version info to swagger doc
 swaggerDocument.info.version = process.env.npm_package_version;
