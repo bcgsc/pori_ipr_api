8f232f805d73d42989276a5de3347fff91998400
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-03-03 13:48:34 -0800

    Add partially unique ident index for updated tables

--- migrations/20200228172732-DEVSU-793-rename-db-tables-2.js
+++ migrations/20200228172732-DEVSU-793-rename-db-tables-2.js
@@ -5,11 +5,19 @@ const camelCaseTables = ['germline_small_mutations', 'germline_small_mutations_r
 ];
 
 const objectsToRemoveUniqueIdent = [
-  {table: 'germline_small_mutations', constraint: 'pog_analysis_germline_small_mutations_ident_key'}, 
+  {table: 'germline_small_mutations', constraint: 'pog_analysis_germline_small_mutations_ident_key'},
   {table: 'germline_small_mutations_review', constraint: 'pog_analysis_germline_small_mutations_review_ident_key'},
   {table: 'germline_small_mutations_variant', constraint: 'pog_analysis_germline_small_mutations_variant_ident_key'},
   {table: 'user_groups', constraint: 'userGroups_ident_key'},
   {table: 'reports_users', constraint: 'POGUsers_ident_key'},
+];
+
+const tablesToAddPartialUniqueIdent = [
+  'germline_small_mutations', 'germline_small_mutations_review',
+  'germline_small_mutations_variant', 'user_groups', 'reports_users',
+  'reports_mavis_summary', 'reports_presentation_discussion',
+  'reports_presentation_slides', 'reports_image_data',
+  'reports_probe_test_information',
 ];
 
 const tablesToDelete = ['notifications', 'pog_recent_reports', 'user_tokens'];
@@ -53,11 +61,28 @@ module.exports = {
 
         // add delete and update column and update camel case columns to snake case
         const missingDelAndUpdColumnTable = 'flash_tokens';
-        return Promise.all([
+        await Promise.all([
           queryInterface.renameColumn(missingDelAndUpdColumnTable, 'createdAt', 'created_at', {transaction}),
           queryInterface.addColumn(missingDelAndUpdColumnTable, 'updated_at', {type: Sequelize.DATE, defaultValue: Sequelize.NOW}, {transaction}),
           queryInterface.addColumn(missingDelAndUpdColumnTable, 'deleted_at', {type: Sequelize.DATE}, {transaction}),
         ]);
+
+        // add partial unique index for tables missing it
+        return Promise.all(
+          tablesToAddPartialUniqueIdent.map((table) => {
+            return queryInterface.addIndex(table, {
+              name: `${table}_ident_index`,
+              unique: true,
+              fields: ['ident'],
+              where: {
+                deleted_at: {
+                  [Sequelize.Op.eq]: null,
+                },
+              },
+              transaction,
+            });
+          })
+        );
       });
     } catch (error) {
       throw error;
