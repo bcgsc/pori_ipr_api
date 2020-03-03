65db440adea0b075f6a1d1e3fa6cb820b6003c23
Raphael Pletz <rpletz@bcgsc.ca>
2020-03-03 13:36:48 -0800

    Add base schema loader and OpenAPI converter

--- app/routes/swagger/schemas.js
+++ app/routes/swagger/schemas.js
@@ -1,9 +1,22 @@
-const {JsonSchemaManager, JsonSchema7Strategy} = require('@alt3/sequelize-to-json-schemas');
+const {JsonSchemaManager, OpenApi3Strategy} = require('@alt3/sequelize-to-json-schemas');
 const db = require('../../models');
 const {BASE_EXCLUDE} = require('../../schemas/exclude');
 
 const schemaManager = new JsonSchemaManager({secureSchemaUri: false});
 
+const user = schemaManager.generate(db.models.user, new OpenApi3Strategy(), {
+  title: 'user',
+  associations: false,
+  exclude: [],
+});
 
+const newUser = schemaManager.generate(db.models.user, new OpenApi3Strategy(), {
+  title: 'newUser',
+  associations: false,
+  exclude: [],
+});
 
-module.exports = {};
+module.exports = {
+  user,
+  newUser,
+};
