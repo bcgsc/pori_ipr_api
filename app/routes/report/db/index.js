a413c55880096cf492fc39fd04a89df60f3d35e3
Caralyn Reisle <creisle@bcgsc.ca>
2020-03-11 13:47:42 -0700

    PR#435: Move subroutine logic out of reports route

---
+++ app/routes/report/db/index.js
@@ -0,0 +1,129 @@
+const db = require('../../../models');
+const logger = require('../../../log');
+const {GENE_LINKED_VARIANT_MODELS} = require('../../../constants');
+
+/**
+ * Creates a new section for the report adding the report and gene foreign keys where required
+ *
+ * @param {Number} reportId the report Id for the report these sections belongto
+ * @param {Object} genesRecordsByName mapping of gene names to their corresponding records
+ * @param {string} modelName name of the model for this section
+ * @param {Array|Object} sectionContent the record or records to be created for this section
+ *
+ * @returns {Array.<Object>} the newly created records in this section
+ */
+const createReportSection = async (reportId, genesRecordsByName, modelName, sectionContent) => {
+  const records = Array.isArray(sectionContent)
+    ? sectionContent
+    : [sectionContent];
+
+  try {
+    if (modelName === 'sv') {
+      // add the gene FK associations
+      await db.models[modelName].bulkCreate(
+        records.map(({gene1, gene2, ...newEntry}) => {
+          return {
+            ...newEntry,
+            reportId,
+            gene1Id: genesRecordsByName[gene1],
+            gene2Id: genesRecordsByName[gene2],
+          };
+        })
+      );
+    } else if (GENE_LINKED_VARIANT_MODELS.includes(modelName)) {
+      // add the gene FK association
+      await db.models[modelName].bulkCreate(
+        records.map(({gene, ...newEntry}) => {
+          return {
+            ...newEntry,
+            reportId,
+            geneId: genesRecordsByName[gene],
+          };
+        })
+      );
+    } else {
+      await db.models[modelName].bulkCreate(
+        records.map((newEntry) => {
+          return {...newEntry, reportId};
+        })
+      );
+    }
+  } catch (err) {
+    logger.error(`Error creating section (${modelName}): ${err}`);
+    throw err;
+  }
+};
+
+/**
+ * Given the content for a report to be created, pull gene names from
+ * the variant sections and create the genes which will be used as
+ * foreign keys in these records
+ *
+ * @param {Object} report the current report record
+ * @param {Object} content the report upload content
+ *
+ * @returns {Object} mapping of gene names to the gene records for this report
+ */
+const createReportGenes = async (report, content) => {
+  // create the genes first since they will need to be linked to the variant records
+  const geneDefns = {};
+  for (const model of GENE_LINKED_VARIANT_MODELS) {
+    for (const variant of content[model] || []) {
+      if (model === 'sv') {
+        if (variant.gene1) {
+          geneDefns[variant.gene1] = {name: variant.gene1};
+        }
+        if (variant.gene2) {
+          geneDefns[variant.gene2] = {name: variant.gene2};
+        }
+      } else {
+        geneDefns[variant.gene] = {name: variant.gene};
+      }
+    }
+  }
+
+  for (const gene of content.genes || []) {
+    geneDefns[gene.name] = gene;
+  }
+
+  logger.debug(`creating gene definitions for the report ${report.ident}`);
+  const genes = await db.models.genes.bulkCreate(Object.values(geneDefns).map((newEntry) => {
+    return {...newEntry, reportId: report.id};
+  }));
+  for (const gene of genes) {
+    geneDefns[gene.name] = gene.id;
+  }
+  return geneDefns;
+};
+
+
+/**
+ * Delete a report and any content associated with it (since cascade cannot be used with paranoid mode)
+ *
+ * @param {Number} reportId the ID of the report to be destroyed
+ * @param {Object} options options to be passed to the destroy calls (ex. force)
+ * @param {boolean} options.force flag to turn off paranoid deletion
+ *
+ * @returns {Promise} if no errors are thrown the report was deleted ok
+ */
+const destroyReport = async (reportId, options = {force: false}) => {
+  try {
+    const promises = Object.values(db.models.analysis_report.associations).map(async (association) => {
+      await association.target.destroy({...options, where: {reportId}});
+    });
+
+    promises.push(async () => {
+      return db.models.analysis_report.destroy({...options, where: {id: reportId}});
+    });
+
+    await Promise.all(promises);
+  } catch (err) {
+    logger.error(`Failed to destory parts of the report: ${reportId}`);
+    throw err;
+  }
+};
+
+
+module.exports = {
+  createReportGenes, createReportSection, destroyReport,
+};
