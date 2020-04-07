8563166953669d37c18d3aa3ffdc90adfef0ee67
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-04-07 14:55:38 -0700

    Update migration file to copy comments to new table before dropping genomic events table

--- migrations/20200403165243-DEVSU-934-drop-therapeutic-table.js
+++ migrations/20200403165243-DEVSU-934-drop-therapeutic-table.js
@@ -1,6 +1,26 @@
+const PROBE_RESULTS_TABLE = 'reports_probe_results';
+const GENOMIC_EVENTS_TABLE = 'reports_summary_genomic_events_therapeutic';
+
 module.exports = {
-  up: (queryInterface) => {
-    return queryInterface.dropTable('reports_summary_genomic_events_therapeutic');
+  up: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.sequelize.transaction(async (transaction) => {
+        // add comments column to probe results table
+        await queryInterface.addColumn(PROBE_RESULTS_TABLE, 'comments', Sequelize.TEXT, {transaction});
+
+        // add comments from events table to probe results table
+        await queryInterface.sequelize.query(`
+          UPDATE ${PROBE_RESULTS_TABLE} AS probe 
+          SET comments = events."comments" 
+          FROM ${GENOMIC_EVENTS_TABLE} AS events 
+          WHERE (probe.gene || ' (' || probe.variant || ')' = events."genomicEvent") and events."comments" != ''
+        `, {transaction});
+
+        return queryInterface.dropTable(GENOMIC_EVENTS_TABLE, {transaction});
+      });
+    } catch (error) {
+      throw error;
+    }
   },
 
   down: () => {
