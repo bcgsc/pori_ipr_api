aaa9cb433b9113b12da59f1150d4bdbf97bd94d3
Raphael Pletz <rpletz@bcgsc.ca>
2020-04-09 10:31:57 -0700

    Update column names + backfill kbUrl

--- migrations/20200406182519-DEVSU-935-update-migrations.js
+++ migrations/20200406182519-DEVSU-935-update-migrations.js
@@ -1,21 +1,31 @@
+const TABLE = 'reports';
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const transaction = await queryInterface.sequelize.transaction();
+    const kbUrlColumn = 'kb_url';
+
     try {
       await queryInterface.removeColumn('reports_patient_information', 'POGID', {transaction});
       await queryInterface.renameColumn('reports_patient_information', 'tumourType', 'diagnosis', {transaction});
 
-      await queryInterface.addColumn('reports', 'kbDiseaseMatch', {
+      await queryInterface.addColumn('reports', 'kb_disease_match', {
         type: Sequelize.STRING,
         defaultValue: null,
       }, {transaction});
 
-      await queryInterface.addColumn('reports', 'kbUrl', {
+      await queryInterface.addColumn('reports', 'kb_url', {
         type: Sequelize.STRING,
         defaultValue: null,
       }, {transaction});
 
-      await queryInterface.addColumn('reports_genes', 'knownFusionPartner', {
+      await queryInterface.sequelize.query(
+        `UPDATE ${TABLE} SET ${kbUrlColumn} = 'https://ipr.bcgsc.ca/knowledgebase/references'
+        WHERE ${kbUrlColumn} IS NULL`,
+        {transaction}
+      );
+
+      await queryInterface.addColumn('reports_genes', 'known_fusion_partner', {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
       }, {transaction});
