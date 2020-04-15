554b2e25d88e914260cb1d5693859fc68fc4e8f0
Raphael Pletz <rpletz@bcgsc.ca>
2020-04-15 10:39:07 -0700

    Point db to dev

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ const {merge} = require('lodash');
 const ENV = process.env.NODE_ENV || 'local';
 
 // set the default db name based on the node-env
-let DEFAULT_DB_NAME = 'DEVSU-935-update-migrations-for-final-requirements';
+let DEFAULT_DB_NAME = 'ipr-sync-dev';
 
 if (ENV === 'production') {
   DEFAULT_DB_NAME = 'ipr';
