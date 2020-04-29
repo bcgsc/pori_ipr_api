04b38a6ca94f7366096316c81e9568f2539fbe97
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-04-29 09:38:09 -0700

    Add migration to fix missing foreign key cascades

---
+++ migrations/20200429154614-DEVSU-1010-missing-cascade.js
@@ -0,0 +1,32 @@
+const TABLE = 'reports_genes';
+const FOREIGN_KEY = 'reports_genes_report_id_fkey';
+
+module.exports = {
+  up: async (queryInterface) => {
+    try {
+      await queryInterface.sequelize.transaction(async (transaction) => {
+        // Remove old foreign key
+        await queryInterface.removeConstraint(TABLE, FOREIGN_KEY, {transaction});
+
+        // Add new correct foreign key
+        await queryInterface.addConstraint(TABLE, ['report_id'], {
+          type: 'foreign key',
+          name: FOREIGN_KEY,
+          references: {
+            table: 'reports',
+            field: 'id',
+          },
+          onDelete: 'cascade',
+          onUpdate: 'cascade',
+          transaction,
+        });
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  down: () => {
+    throw new Error('Not Implemented!');
+  },
+};
