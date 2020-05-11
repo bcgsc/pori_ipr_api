6a95b162e6d40e22a9f7eb2a3896da4d930474ff
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-05-11 10:02:35 -0700

    Remove some id fields from the report middleware

--- app/middleware/analysis_report.js
+++ app/middleware/analysis_report.js
@@ -7,13 +7,14 @@ module.exports = async (req, res, next, ident) => {
     where: {ident},
     attributes: {exclude: ['config', 'deletedAt']},
     include: [
-      {model: db.models.patientInformation, as: 'patientInformation', attributes: {exclude: ['id', 'deletedAt']}},
+      {model: db.models.patientInformation, as: 'patientInformation', attributes: {exclude: ['id', 'reportId', 'deletedAt']}},
       {model: db.models.tumourAnalysis.scope('public'), as: 'tumourAnalysis'},
       {model: db.models.user.scope('public'), as: 'createdBy'},
       {
         model: db.models.analysis_reports_user,
         as: 'users',
         separate: true,
+        attributes: {exclude: ['id', 'reportId', 'user_id', 'addedBy_id', 'deletedAt']},
         include: [
           {model: db.models.user.scope('public'), as: 'user'},
         ],
