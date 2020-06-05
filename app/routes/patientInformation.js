b3e505ac02dafff93b311ad89ecd0974ca301176
Raphael Pletz <rpletz@bcgsc.ca>
2020-06-04 23:00:35 -0700

    Remove error code from patientInformation

--- app/routes/patientInformation.js
+++ app/routes/patientInformation.js
@@ -13,12 +13,12 @@ router.use('/', async (req, res, next) => {
     result = await db.models.patientInformation.scope('public').findOne({where: {reportId: req.report.id}});
   } catch (error) {
     logger.error(`Unable to query Patient Information for report ${req.report.ident} error: ${error}`);
-    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: `Unable to lookup the patient information for report ${req.report.ident}`, code: 'failedPatientInformationQuery'}});
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: `Unable to lookup the patient information for report ${req.report.ident}`}});
   }
 
   if (!result) {
     logger.error(`Unable to find the patient information for report ${req.report.ident}`);
-    return res.status(HTTP_STATUS.NOT_FOUND).json({error: {message: `Unable to find the patient information for report ${req.report.ident}`, code: 'failedPatientInformationLookup'}});
+    return res.status(HTTP_STATUS.NOT_FOUND).json({error: {message: `Unable to find the patient information for report ${req.report.ident}`}});
   }
 
   // Found the patient information
