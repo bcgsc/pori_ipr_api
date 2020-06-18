13bf2abd8b3fe26c957592e6abf03a69610d23cf
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-06-18 10:08:24 -0700

    Update probe results model and routes

--- app/routes/report/probeResults.js
+++ app/routes/report/probeResults.js
@@ -2,14 +2,16 @@ const HTTP_STATUS = require('http-status-codes');
 const express = require('express');
 
 const router = express.Router({mergeParams: true});
-const db = require('../../models');
 
+const db = require('../../models');
 const logger = require('../../log');
 
 router.param('target', async (req, res, next, altIdent) => {
   let result;
   try {
-    result = await db.models.probeResults.scope('public').findOne({where: {ident: altIdent}});
+    result = await db.models.probeResults.scope('middleware').findOne({
+      where: {ident: altIdent},
+    });
   } catch (error) {
     logger.error(`Unable to find probe target ${error}`);
     return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to find probe target'}});
@@ -20,36 +22,22 @@ router.param('target', async (req, res, next, altIdent) => {
     return res.status(HTTP_STATUS.NOT_FOUND).json({error: {message: 'Unable to locate probe target'}});
   }
 
+  // Add probe result to request
   req.target = result;
   return next();
 });
 
-// Handle requests for alterations
+// Handle requests for probe results
 router.route('/:target([A-z0-9-]{36})')
   .get((req, res) => {
-    return res.json(req.target);
+    return res.json(req.target.view('public'));
   })
   .put(async (req, res) => {
-    // Update DB Version for Entry
+    // Update db entry
     try {
-      const result = await db.models.probeResults.update(req.body, {
-        where: {
-          ident: req.target.ident,
-        },
-        individualHooks: true,
-        paranoid: true,
-        returning: true,
-      });
-
-      // Get updated model data from update
-      const [, [{dataValues}]] = result;
-
-      // Remove id's and deletedAt properties from returned model
-      const {
-        id, reportId, deletedAt, ...publicModel
-      } = dataValues;
-
-      return res.json(publicModel);
+      await req.target.update(req.body);
+      await req.target.reload();
+      return res.json(req.target.view('public'));
     } catch (error) {
       logger.error(`Unable to update probe target ${error}`);
       return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to update probe target'}});
@@ -58,7 +46,7 @@ router.route('/:target([A-z0-9-]{36})')
   .delete(async (req, res) => {
     // Soft delete the entry
     try {
-      await db.models.probeResults.destroy({where: {ident: req.target.ident}});
+      await req.target.destroy();
       return res.status(HTTP_STATUS.NO_CONTENT).send();
     } catch (error) {
       logger.error(`Unable to remove probe target ${error}`);
@@ -69,17 +57,12 @@ router.route('/:target([A-z0-9-]{36})')
 
 router.route('/')
   .get(async (req, res) => {
-    // Setup where clause
-    const where = {reportId: req.report.id};
-
-    const options = {
-      where,
-      order: [['geneId', 'ASC']],
-    };
-
     // Get all targeted genes for this report
     try {
-      const result = await db.models.probeResults.scope('public').findAll(options);
+      const result = await db.models.probeResults.scope('public').findAll({
+        where: {reportId: req.report.id},
+        order: [['geneId', 'ASC']],
+      });
       return res.json(result);
     } catch (error) {
       logger.error(`Unable to retrieve resource ${error}`);
