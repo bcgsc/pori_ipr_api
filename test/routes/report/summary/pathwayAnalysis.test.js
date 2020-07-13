a81a30012cb055a00658887a58cb62bbd4f2c834
Raphael Pletz <rpletz@bcgsc.ca>
2020-07-13 10:27:21 -0700

    Update data for tests

--- test/routes/report/summary/pathwayAnalysis.test.js
+++ test/routes/report/summary/pathwayAnalysis.test.js
@@ -1,9 +1,11 @@
 const HTTP_STATUS = require('http-status-codes');
 
+const path = require('path');
 const supertest = require('supertest');
 const getPort = require('get-port');
 const db = require('../../../../app/models');
 
+const mockReportData = require('../../../testData/mockReportData.json');
 
 const CONFIG = require('../../../../app/config');
 const {listen} = require('../../../../app');
@@ -25,19 +27,31 @@ beforeAll(async () => {
   request = supertest(server);
 });
 
-// Tests for /kb-matches endpoint
 describe('', () => {
-  beforeAll(async () => {
+  let report;
+
+  beforeEach(async () => {
+    // Create Report and Mutation Summary
+    report = await db.models.analysis_report.create({
+      patientId: mockReportData.patientId,
+    });
   }, LONGER_TIMEOUT);
 
   describe('GET', () => {
     test('', async () => {
+      const res = await request
+        .put(`/api/reports/LVZPX/summary/pathway-analysis`)
+        .auth(username, password)
+        .type('json')
+        .attach('pathway', path.join(__dirname, '/../../../testData/pathwayAnalysisData.txt'))
+        .expect(HTTP_STATUS.OK);
+
     });
   });
 
   // delete report
-  afterAll(async () => {
-
+  afterEach(async () => {
+    await db.models.analysis_report.destroy({where: {id: report.id}, force: true});
   }, LONGER_TIMEOUT);
 });
 
