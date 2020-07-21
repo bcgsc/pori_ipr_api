9acb01beecb428a30a6ce683a2c79fd29d470cc3
Raphael Pletz <rpletz@bcgsc.ca>
2020-07-21 13:28:58 -0700

    Create and use custom swagger exclude

--- app/routes/swagger/schemas.js
+++ app/routes/swagger/schemas.js
@@ -1,10 +1,11 @@
 const {JsonSchemaManager, OpenApi3Strategy} = require('@alt3/sequelize-to-json-schemas');
 const db = require('../../models');
-const {REPORT_EXCLUDE} = require('../../schemas/exclude');
 
 const schemaManager = new JsonSchemaManager({secureSchemaUri: false});
 
 const schemas = {};
+
+const SWAGGER_EXCLUDE = ['id', 'reportId', 'geneId', 'deletedAt'];
 
 const {
   germlineReportsToProjects, userGroupMember, reportProject, user_project, ...models
@@ -14,37 +15,37 @@ for (const property of Object.keys(models)) {
   schemas[property] = schemaManager.generate(db.models[property], new OpenApi3Strategy(), {
     title: `${property}`,
     associations: false,
-    exclude: [...REPORT_EXCLUDE],
+    exclude: [...SWAGGER_EXCLUDE],
   });
 }
 
 schemas.newUser = schemaManager.generate(db.models.user, new OpenApi3Strategy(), {
   title: 'newUser',
   associations: false,
-  exclude: [...REPORT_EXCLUDE, 'jiraToken', 'jiraXsrf', 'lastLogin', 'settings'],
+  exclude: [...SWAGGER_EXCLUDE, 'jiraToken', 'jiraXsrf', 'lastLogin', 'settings'],
 });
 
 schemas.user = schemaManager.generate(db.models.user, new OpenApi3Strategy(), {
   title: 'user',
   associations: false,
-  exclude: [...REPORT_EXCLUDE, 'user_projects', 'userGroupMembers'],
+  exclude: [...SWAGGER_EXCLUDE, 'user_projects', 'userGroupMembers'],
 });
 
 schemas.group = schemaManager.generate(db.models.userGroup, new OpenApi3Strategy(), {
   title: 'group',
   associations: true,
-  exclude: [...REPORT_EXCLUDE, 'owner_id', 'jiraToken', 'jiraXsrf', 'lastLogin', 'settings'],
+  exclude: [...SWAGGER_EXCLUDE, 'owner_id', 'jiraToken', 'jiraXsrf', 'lastLogin', 'settings'],
 });
 
 schemas.project = schemaManager.generate(db.models.project, new OpenApi3Strategy(), {
   title: 'project',
   associations: false,
-  exclude: [...REPORT_EXCLUDE],
+  exclude: [...SWAGGER_EXCLUDE],
 });
 
 schemas.analysis_report = schemaManager.generate(db.models.analysis_report, new OpenApi3Strategy(), {
   title: 'analysis_report',
-  exclude: ['createdBy_id', ...REPORT_EXCLUDE],
+  exclude: ['createdBy_id', ...SWAGGER_EXCLUDE],
   associations: true,
   excludeAssociations: ['ReportUserFilter', 'createdBy', 'probe_signature', 'presentation_discussion', 'presentation_slides', 'users', 'analystComments', 'projects'],
 });
@@ -52,7 +53,7 @@ schemas.analysis_report = schemaManager.generate(db.models.analysis_report, new 
 schemas.germline_small_mutation = schemaManager.generate(db.models.germline_small_mutation, new OpenApi3Strategy(), {
   title: 'germline_small_mutation',
   associations: false,
-  exclude: [...REPORT_EXCLUDE],
+  exclude: [...SWAGGER_EXCLUDE],
 });
 
 schemas.project.properties.users = schemas.user;
