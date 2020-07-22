a6557a4e61a1e5ee686396387e749aeed10caa9c
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-07-22 14:43:25 -0700

    Add cite to allowed tags

--- app/libs/helperFunctions.js
+++ app/libs/helperFunctions.js
@@ -39,7 +39,7 @@ const sanitizeHtml = (html) => {
   return sanitize(html, {
     allowedTags: ['h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
       'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'abbr', 'code', 'hr', 'br', 'div',
-      'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img'],
+      'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img', 'cite'],
   });
 };
 
