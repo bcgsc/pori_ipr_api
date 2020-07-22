03b701ad675270a6b7e8482c02ade9addba82eb9
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-07-22 15:03:51 -0700

    Allow cite propety on blockquote tag

--- app/libs/helperFunctions.js
+++ app/libs/helperFunctions.js
@@ -40,6 +40,9 @@ const sanitizeHtml = (html) => {
     allowedTags: ['h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
       'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'abbr', 'code', 'hr', 'br', 'div',
       'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img', 'cite'],
+    allowedAttributes: {
+      blockquote: ['cite'],
+    },
   });
 };
 
