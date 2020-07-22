f18117066eaa064791c9a1bd0d39e37d8f868f84
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-07-22 13:19:57 -0700

    Add html sanitize function with our sanitization rules

--- app/libs/helperFunctions.js
+++ app/libs/helperFunctions.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const sanitize = require('sanitize-html');
 
 /**
  * Checks that all target values exist
@@ -27,7 +28,23 @@ const caseInsensitiveIntersect = (array1, array2) => {
   return _.intersectionBy(array1, array2, _.lowerCase);
 };
 
+/**
+ * Sanitizes "dirty" html with our sanitization rules and
+ * returns the "clean"/sanitized html
+ *
+ * @param {string} html - A string of "dirty" (hasn't been sanitized) html
+ * @returns {string} - A string of "clean" (sanitized) html
+ */
+const sanitizeHtml = (html) => {
+  return sanitize(html, {
+    allowedTags: ['h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+      'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'abbr', 'code', 'hr', 'br', 'div',
+      'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img'],
+  });
+};
+
 module.exports = {
   includesAll,
   caseInsensitiveIntersect,
+  sanitizeHtml,
 };
