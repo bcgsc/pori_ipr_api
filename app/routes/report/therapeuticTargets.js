532f5966803c32a87711d737dec1ec71fa5660b9
Raphael Pletz <rpletz@bcgsc.ca>
2020-07-23 11:33:41 -0700

    Update POST spec and error codes

--- app/routes/report/therapeuticTargets.js
+++ app/routes/report/therapeuticTargets.js
@@ -84,7 +84,7 @@ router.route('/')
       return res.json(result.view('public'));
     } catch (error) {
       logger.error(`Unable to create new therapeutic target entry ${error}`);
-      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to create new therapeutic target entry'}});
+      return res.status(HTTP_STATUS.CONFLICT).json({error: {message: 'Rank already exists'}});
     }
   })
   .put(async (req, res) => {
@@ -110,7 +110,7 @@ router.route('/')
       return res.json({updated: true});
     } catch (error) {
       logger.error(`Unable to update therapeutic target rank ${error}`);
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({error: {message: 'Unable to update therapeutic target rank'}});
+      return res.status(HTTP_STATUS.CONFLICT).json({error: {message: 'Unable to update therapeutic target rank'}});
     }
   });
 
