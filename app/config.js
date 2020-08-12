22036d74e6f242fbdf5a7b8d98f706c7189194bc
Raphael Pletz <rpletz@bcgsc.ca>
2020-08-12 13:02:20 -0700

    Point to test db

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ const {merge} = require('lodash');
 const ENV = process.env.NODE_ENV || 'local';
 
 // set the default db name based on the node-env
-let DEFAULT_DB_NAME = 'ipr-sync-dev';
+let DEFAULT_DB_NAME = 'DEVSU-1224-add-kbstatementids-field-to-therapeutic';
 
 if (ENV === 'production') {
   DEFAULT_DB_NAME = 'ipr';
