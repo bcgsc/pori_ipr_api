c9b522bd0b8c7841c8ae716fbe3a7108ff528991
Raphael Pletz <rpletz@bcgsc.ca>
2020-08-12 16:55:03 -0700

    Simplify promise return

--- migrations/20200812220419-DEVSU-1224-add-kbstatementId-column-to-therapeutic-options.js
+++ migrations/20200812220419-DEVSU-1224-add-kbstatementId-column-to-therapeutic-options.js
@@ -3,9 +3,7 @@ const THERAPEUTIC_TABLE = 'reports_therapeutic_targets';
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (transaction) => {
-      return Promise.all([
-        queryInterface.addColumn(THERAPEUTIC_TABLE, 'kb_statement_id', Sequelize.TEXT, {transaction}),
-      ]);
+      return queryInterface.addColumn(THERAPEUTIC_TABLE, 'kb_statement_ids', Sequelize.TEXT, {transaction});
     });
   },
 
