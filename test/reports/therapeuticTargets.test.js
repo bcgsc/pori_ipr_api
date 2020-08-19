afb6aaf2f71281e3ac0576226910a366f725397e
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-08-19 14:23:15 -0700

    Fix targets test and mock data

--- test/reports/therapeuticTargets.test.js
+++ test/reports/therapeuticTargets.test.js
@@ -21,7 +21,6 @@ const FAKE_TARGET = {
   geneGraphkbId: '#2:3',
   context: 'resistance',
   therapy: 'EGFR inhibitors',
-  rank: 0,
   evidenceLevel: 'OncoKB 1',
 };
 
@@ -155,7 +154,7 @@ describe('/therapeutic-targets', () => {
           // create a new therapeutic target
           ({dataValues: newTarget} = await db.models.therapeuticTarget.create({
             ...FAKE_TARGET,
-            rank: -900,
+            rank: 1,
             reportId: report.id,
           }));
 
@@ -191,8 +190,8 @@ describe('/therapeutic-targets', () => {
             .put(`/api/reports/${report.ident}/therapeutic-targets`)
             .auth(username, password)
             .send([
-              {ident: original.ident, rank: -500},
-              {ident: newTarget.ident, rank: -500},
+              {ident: original.ident, rank: 2},
+              {ident: newTarget.ident, rank: 2},
             ])
             .type('json')
             .expect(HTTP_STATUS.BAD_REQUEST);
@@ -260,7 +259,6 @@ describe('/therapeutic-targets', () => {
       .type('json')
       .expect(HTTP_STATUS.NOT_FOUND);
   });
-
 });
 
 afterAll(async () => {
