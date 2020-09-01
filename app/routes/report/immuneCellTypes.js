967d80aa46fc20b0658f55b51ec74a39c76ce170
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-09-01 11:18:51 -0700

    Add immune cell types routes

---
+++ app/routes/report/immuneCellTypes.js
@@ -0,0 +1,90 @@
+const HTTP_STATUS = require('http-status-codes');
+const express = require('express');
+
+const router = express.Router({mergeParams: true});
+
+const db = require('../../models');
+const logger = require('../../log');
+
+const schemaGenerator = require('../../schemas/schemaGenerator');
+const validateAgainstSchema = require('../../libs/validateAgainstSchema');
+const {REPORT_UPDATE_BASE_URI} = require('../../constants');
+
+// Generate schema's
+const updateSchema = schemaGenerator(db.models.immuneCellTypes, {
+  baseUri: REPORT_UPDATE_BASE_URI, nothingRequired: true,
+});
+
+// Middleware
+router.param('ict', async (req, res, next, ictIdent) => {
+  let result;
+  try {
+    result = await db.models.immuneCellTypes.findOne({
+      where: {ident: ictIdent},
+    });
+  } catch (error) {
+    logger.error(`Unable to get immune cell type ${error}`);
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to get immune cell type'}});
+  }
+
+  if (!result) {
+    logger.error('Unable to locate immune cell type');
+    return res.status(HTTP_STATUS.NOT_FOUND).json({error: {message: 'Unable to locate immune cell type'}});
+  }
+
+  // Add immune cell type to request
+  req.immuneCellType = result;
+  return next();
+});
+
+// Handle requests for specific immune cell type
+router.route('/:ict([A-z0-9-]{36})')
+  .get((req, res) => {
+    return res.json(req.immuneCellType.view('public'));
+  })
+  .put(async (req, res) => {
+    // Validate request against schema
+    try {
+      validateAgainstSchema(updateSchema, req.body);
+    } catch (error) {
+      logger.error(`Error while validating immune cell type update request ${error}`);
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({error: {message: `Error while validating immune cell type update request ${error}`}});
+    }
+
+    // Update db entry
+    try {
+      await req.immuneCellType.update(req.body);
+      return res.json(req.immuneCellType.view('public'));
+    } catch (error) {
+      logger.error(`Unable to update immune cell type ${error}`);
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to update immune cell type'}});
+    }
+  })
+  .delete(async (req, res) => {
+    // Soft delete the entry
+    try {
+      await req.immuneCellType.destroy();
+      return res.status(HTTP_STATUS.NO_CONTENT).send();
+    } catch (error) {
+      logger.error(`Unable to remove immune cell type ${error}`);
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to remove immune cell type'}});
+    }
+  });
+
+// Routing requests for all immune cell types
+router.route('/')
+  .get(async (req, res) => {
+    // Get all immune cell types for this report
+    try {
+      const results = await db.models.immuneCellTypes.scope('public').findAll({
+        where: {reportId: req.report.id},
+      });
+      return res.json(results);
+    } catch (error) {
+      logger.error(`Unable to retrieve immune cell types ${error}`);
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to retrieve immune cell types'}});
+    }
+  });
+
+
+module.exports = router;
