21736b7065ac108a8a3f36753f8538d060c0e9a6
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-09-03 09:29:51 -0700

    Add microbial routes

--- app/routes/report/summary/microbial.js
+++ app/routes/report/summary/microbial.js
@@ -5,12 +5,24 @@ const logger = require('../../../log');
 
 const router = express.Router({mergeParams: true});
 
+const schemaGenerator = require('../../../schemas/schemaGenerator');
+const validateAgainstSchema = require('../../../libs/validateAgainstSchema');
+const {REPORT_CREATE_BASE_URI, REPORT_UPDATE_BASE_URI} = require('../../../constants');
+
+// Generate schema's
+const createSchema = schemaGenerator(db.models.summary_microbial, {
+  baseUri: REPORT_CREATE_BASE_URI,
+});
+const updateSchema = schemaGenerator(db.models.summary_microbial, {
+  baseUri: REPORT_UPDATE_BASE_URI, nothingRequired: true,
+});
+
 // Middleware for microbial summary
-router.use('/', async (req, res, next) => {
+router.param('microbial', async (req, res, next, micIdent) => {
   try {
     // Add microbial summary to request
     req.microbial = await db.models.summary_microbial.findOne({
-      where: {reportId: req.report.id},
+      where: {ident: micIdent},
     });
     return next();
   } catch (error) {
@@ -19,13 +31,88 @@ router.use('/', async (req, res, next) => {
   }
 });
 
-// Handle requests for microbial summary
-router.route('/')
+// Handle requests for a single microbial summary
+router.route('/:microbial([A-z0-9-]{36})')
   .get((req, res) => {
     if (req.microbial) {
       return res.json(req.microbial.view('public'));
     }
     return res.json(null);
+  })
+  .put(async (req, res) => {
+    if (!req.microbial) {
+      logger.error('There is no microbial data to update');
+      return res.status(HTTP_STATUS.NOT_FOUND).json({error: {message: 'There is no microbial data to update'}});
+    }
+
+    // Validate request against schema
+    try {
+      validateAgainstSchema(updateSchema, req.body);
+    } catch (error) {
+      const message = `Error while validating microbial update request ${error}`;
+      logger.error(message);
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({error: {message}});
+    }
+
+    // Update db entry
+    try {
+      await req.microbial.update(req.body);
+      return res.json(req.microbial.view('public'));
+    } catch (error) {
+      logger.error(`Unable to update microbial data ${error}`);
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to update microbial data'}});
+    }
+  })
+  .delete(async (req, res) => {
+    if (!req.microbial) {
+      logger.error('There is no microbial data to delete');
+      return res.status(HTTP_STATUS.NOT_FOUND).json({error: {message: 'There is no microbial data to delete'}});
+    }
+
+    // Soft delete the entry
+    try {
+      await req.microbial.destroy();
+      return res.status(HTTP_STATUS.NO_CONTENT).send();
+    } catch (error) {
+      logger.error(`Error while trying to delete microbial data ${error}`);
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Error while trying to delete microbial data'}});
+    }
   });
+
+// Handle requests for all report microbial data
+router.route('/')
+  .get(async (req, res) => {
+    try {
+      const results = await db.models.summary_microbial.scope('public').findAll({
+        where: {reportId: req.report.id},
+      });
+      return res.status(HTTP_STATUS.OK).json(results);
+    } catch (error) {
+      logger.error(`Unable to get all report microbial data ${error}`);
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to get all report microbial data'}});
+    }
+  })
+  .post(async (req, res) => {
+    // Validate request against schema
+    try {
+      validateAgainstSchema(createSchema, req.body);
+    } catch (error) {
+      const message = `Error while validating microbial create request ${error}`;
+      logger.error(message);
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({error: {message}});
+    }
+
+    // Add new microbial data to report
+    try {
+      req.body.reportId = req.report.id;
+
+      const result = await db.models.summary_microbial.create(req.body);
+      return res.status(HTTP_STATUS.CREATED).json(result.view('public'));
+    } catch (error) {
+      logger.error(`Unable to create microbial data entry ${error}`);
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Unable to create microbial data entry'}});
+    }
+  });
+
 
 module.exports = router;
