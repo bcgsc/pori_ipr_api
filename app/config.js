cbdc7e1e47617aeb8d4262fbba504754b83a41a5
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-10-15 13:38:33 -0700

    Point config back to dev db

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ const {merge} = require('lodash');
 const ENV = process.env.NODE_ENV || 'local';
 
 // set the default db name based on the node-env
-let DEFAULT_DB_NAME = 'DEVSU-1316-update-kb-matches-category-enum';
+let DEFAULT_DB_NAME = 'ipr-sync-dev';
 
 if (ENV === 'production') {
   DEFAULT_DB_NAME = 'ipr';
