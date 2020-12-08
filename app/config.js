dba497dc1e91a159a3e0d82a3fb2bccf33a4b01e
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-12-08 12:10:15 -0800

    Point config to temp db

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ const {merge} = require('lodash');
 const ENV = process.env.NODE_ENV || 'local';
 
 // set the default db name based on the node-env
-let DEFAULT_DB_NAME = 'DEVSU-1342-add-tables-and-figure-key-for-msi-data';
+let DEFAULT_DB_NAME = 'DEVSU-1386-create-table-to-store-report-template';
 
 if (ENV === 'production') {
   DEFAULT_DB_NAME = 'ipr';
