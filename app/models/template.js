45d7da007ed440baba153a2c9702fe4c5c842d6a
Brayden Pellegrini <bpellegrini@bcgsc.ca>
2020-12-15 14:26:27 -0800

    Add all template sections to example

--- app/models/template.js
+++ app/models/template.js
@@ -21,8 +21,10 @@ module.exports = (sequelize) => {
             type: 'string',
           },
           example: [
-            'microbial', 'hla-types', 'small-mutation', 'mutation-burden',
-            'therapeutic-targets', 'expression-variants', 'structural-variants',
+            'summary', 'analyst-comments', 'pathway-analysis', 'therapeutic-targets',
+            'kb-matches', 'slides', 'discussion', 'microbial', 'expression-correlation',
+            'mutation-signatures', 'mutation-burden', 'immune', 'small-mutations',
+            'copy-number', 'structural-variants', 'expression', 'appendices',
           ],
           description: 'list of sections to display on the client',
         },
