6101334d73738da75e468dcbe03aa1eafdee964f
Caralyn Reisle <creisle@bcgsc.ca>
2021-01-30 21:05:25 -0800

    Add debugging logging

--- test/userGroup.test.js
+++ test/userGroup.test.js
@@ -61,7 +61,12 @@ describe('/user/group', () => {
         .get('/api/user/group')
         .auth(username, password)
         .type('json')
-        .expect(HTTP_STATUS.OK);
+        .expect(function(res) {
+          if(res.status != HTTP_STATUS.OK){
+            console.log(JSON.stringify(res.body, null, 2));
+            }
+        })
+        .expect(HTTP_STATUS.OK)
 
       expect(res.body).toEqual(
         expect.arrayContaining([
