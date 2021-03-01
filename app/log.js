7b91950d92a97c2232bc6579bf0011f81fedd546
Brayden Pellegrini <bpellegrini@bpellegrini01.phage.bcgsc.ca>
2021-03-01 10:18:02 -0800

    Add colour to logger

--- app/log.js
+++ app/log.js
@@ -1,12 +1,24 @@
 const {createLogger, format, transports} = require('winston');
 const nconf = require('./config');
 
+const colorizer = format.colorize({
+  colors: {
+    error: 'red',
+    warn: 'yellow',
+    info: 'white',
+    debug: 'cyan',
+  },
+});
+
 const logger = createLogger({
   level: nconf.get('log:level'),
   format: format.combine(
     format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
     format.printf(({level, message, timestamp, ...meta}) => {
-      return `[${timestamp}][${level.toUpperCase()}] ${message} ${(meta && Object.keys(meta).length) ? `\n\t${JSON.stringify(meta)}` : ''}`;
+      return colorizer.colorize(
+        level,
+        `[${timestamp}][${level.toUpperCase()}] ${message} ${(meta && Object.keys(meta).length) ? `\n\t${JSON.stringify(meta)}` : ''}`
+      );
     }),
   ),
   transports: [new transports.Console()],
