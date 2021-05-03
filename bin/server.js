93157cf91400ffb71fe557fbd72d4f6cc58da9c9
Brayden Pellegrini <bpellegrini@bpellegrini01.phage.bcgsc.ca>
2021-05-03 09:17:19 -0700

    Remove in-code clustering

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,3 @@
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 const {listen} = require('../app');
 const logger = require('../app/log');
 
@@ -9,24 +7,9 @@ process.on('unhandledRejection', (reason, p) => {
   process.exit(1);
 });
 
-if (cluster.isMaster) {
-  // Fork workers
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('online', (worker) => {
-    logger.info(`Worker ${worker.process.pid} is online`);
+// Start app
+listen()
+  .catch((error) => {
+    logger.error(error);
+    process.exit(1);
   });
-
-  cluster.on('exit', (worker, code, signal) => {
-    logger.info(`Worker ${worker.process.pid} died\nCode: ${code}\nSignal: ${signal}`);
-  });
-} else {
-  // Start app
-  listen()
-    .catch((error) => {
-      logger.error(error);
-      process.exit(1);
-    });
-}
