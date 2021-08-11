5889bd3acc50b2392cc1d215bac0b68fead22177
Brayden Pellegrini <bpellegrini@bpellegrini01.phage.bcgsc.ca>
2021-08-11 11:00:49 -0700

    Update gene routes and add PUT request validation

--- app/routes/report/gene.js
+++ app/routes/report/gene.js
@@ -7,6 +7,15 @@ const cache = require('../../cache');
 
 const router = express.Router({mergeParams: true});
 
+const schemaGenerator = require('../../schemas/schemaGenerator');
+const validateAgainstSchema = require('../../libs/validateAgainstSchema');
+const {REPORT_UPDATE_BASE_URI} = require('../../constants');
+
+// Generate schemas
+const updateSchema = schemaGenerator(db.models.genes, {
+  baseUri: REPORT_UPDATE_BASE_URI, nothingRequired: true,
+});
+
 // Middleware for genes
 router.param('geneName', async (req, res, next, geneName) => {
   let result;
@@ -18,13 +27,17 @@ router.param('geneName', async (req, res, next, geneName) => {
       },
     });
   } catch (error) {
-    logger.error(`Unable to find gene ${error}`);
-    return res.status(HTTP_STATUS.BAD_REQUEST).json({error: {message: 'Unable to find gene'}});
+    logger.error(`Error while trying to find gene ${error}`);
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      error: {message: 'Error while trying to find gene'},
+    });
   }
 
   if (!result) {
     logger.error('Unable to locate gene');
-    return res.status(HTTP_STATUS.NOT_FOUND).json({error: {message: 'Unable to locate gene'}});
+    return res.status(HTTP_STATUS.NOT_FOUND).json({
+      error: {message: 'Unable to locate gene'},
+    });
   }
 
   // Add gene to request
@@ -38,12 +51,23 @@ router.route('/:geneName')
     return res.json(req.gene.view('public'));
   })
   .put(async (req, res) => {
+    // Validate request against schema
+    try {
+      validateAgainstSchema(updateSchema, req.body, false);
+    } catch (error) {
+      const message = `Error while validating gene update request ${error}`;
+      logger.error(message);
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({error: {message}});
+    }
+
     try {
       await req.gene.update(req.body, {userId: req.user.id});
       return res.json(req.gene.view('public'));
     } catch (error) {
       logger.error(`Unable to update gene ${error}`);
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({error: {message: 'Unable to update gene'}});
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+        error: {message: 'Unable to update gene'},
+      });
     }
   });
 
@@ -77,7 +101,7 @@ router.route('/')
       return res.json(results);
     } catch (error) {
       logger.error(`Unable to retrieve genes ${error}`);
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
         error: {message: 'Unable to retrieve genes'},
       });
     }
