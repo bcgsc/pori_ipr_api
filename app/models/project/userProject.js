266fd2abf5bccee55253bd3b91359e9c4e2a49c8
Brayden Pellegrini <bpellegrini@bpellegrini01.phage.bcgsc.ca>
2021-09-14 10:27:33 -0700

    Update user-project model name to be camel case

--- app/models/project/userProject.js
+++ app/models/project/userProject.js
@@ -1,7 +1,7 @@
 const {DEFAULT_MAPPING_COLUMNS, DEFAULT_MAPPING_OPTIONS} = require('../base');
 
 module.exports = (sequelize, Sq) => {
-  return sequelize.define('user_project', {
+  return sequelize.define('userProject', {
     ...DEFAULT_MAPPING_COLUMNS,
     user_id: {
       type: Sq.INTEGER,
