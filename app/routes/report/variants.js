e96bc3a8984924d56bc8fe529df38bb9937c77dc
rpletz <rpletz@bcgsc.ca>
2023-01-23 16:40:13 -0800

    Add filtering to variants endpoint

--- app/routes/report/variants.js
+++ app/routes/report/variants.js
@@ -3,39 +3,169 @@ const express = require('express');
 
 const router = express.Router({mergeParams: true});
 
-const {literal} = require('sequelize');
+const {Op, literal} = require('sequelize');
 const db = require('../../models');
 const logger = require('../../log');
+
 const {KB_PIVOT_MAPPING} = require('../../constants');
+
+const getVariants = async (tableName, variantType, reportId) => {
+  return db.models[tableName].scope('extended').findAll({
+    order: [['id', 'ASC']],
+    attributes: {
+      include: [[literal(`'${variantType}'`), 'variantType']],
+    },
+    where: {
+      reportId,
+    },
+    include: [
+      {
+        model: db.models.kbMatches,
+        attributes: ['ident', 'category'],
+      },
+    ],
+  });
+};
+
+const therapeuticAssociationFilter = {
+  id: {[Op.ne]: null},
+  [Op.or]: [{iprEvidenceLevel: ['IPR-A', 'IPR-B']}],
+  category: 'therapeutic',
+  matchedCancer: true,
+  variantType: {[Op.is]: literal('distinct from \'exp\'')},
+};
+
+// PSQL natively ignores null on equal checks.
+// Literal is used in order to accomodate NULL rows.
+const cancerRelevanceFilter = {
+  id: {[Op.ne]: null},
+  [Op.not]: {
+    [Op.or]: [
+      {iprEvidenceLevel: {[Op.is]: literal('not distinct from \'IPR-A\'')}},
+      {iprEvidenceLevel: {[Op.is]: literal('not distinct from \'IPR-B\'')}},
+    ],
+    category: 'therapeutic',
+    matchedCancer: true,
+  },
+  variantType: {[Op.is]: literal('distinct from \'exp\'')},
+};
+
+const unknownSignificanceIncludes = ['mut'];
+
+const unknownSignificanceGeneFilter = {
+  [Op.or]: [{oncogene: true}, {tumourSuppressor: true}],
+};
+
+const getRapidReportVariants = async (tableName, variantType, reportId, rapidTable) => {
+  const therapeuticAssociationResults = await db.models[tableName].scope('extended').findAll({
+    order: [['id', 'ASC']],
+    attributes: {
+      include: [[literal(`'${variantType}'`), 'variantType']],
+    },
+    where: {
+      reportId,
+    },
+    include: [
+      {
+        model: db.models.kbMatches,
+        attributes: ['ident', 'category'],
+        where: {...therapeuticAssociationFilter},
+      },
+    ],
+  });
+
+  if (rapidTable === 'therapeuticAssociation') {
+    return therapeuticAssociationResults;
+  }
+
+  const cancerRelevanceResultsFiltered = [];
+  const cancerRelevanceResults = await db.models[tableName].scope('extended').findAll({
+    order: [['id', 'ASC']],
+    attributes: {
+      include: [[literal(`'${variantType}'`), 'variantType']],
+    },
+    where: {
+      reportId,
+    },
+    include: [
+      {
+        model: db.models.kbMatches,
+        attributes: ['ident', 'category'],
+        where: {...cancerRelevanceFilter},
+      },
+    ],
+  });
+
+  for (const row of cancerRelevanceResults) {
+    if (!(therapeuticAssociationResults.find(
+      (e) => {return e.ident === row.ident;},
+    ))) {
+      cancerRelevanceResultsFiltered.push(row);
+    }
+  }
+
+  if (rapidTable === 'cancerRelevance') {
+    return cancerRelevanceResultsFiltered;
+  }
+
+  const unknownSignificanceResultsFiltered = [];
+  let unknownSignificanceResults = [];
+
+  if (unknownSignificanceIncludes.includes(variantType)) {
+    unknownSignificanceResults = await db.models[tableName].scope('extended').findAll({
+      order: [['id', 'ASC']],
+      attributes: {
+        include: [[literal(`'${variantType}'`), 'variantType']],
+      },
+      where: {
+        reportId,
+      },
+      include: [
+        {
+          model: db.models.kbMatches,
+          attributes: ['ident', 'category'],
+        },
+        {
+          model: db.models.genes.scope('minimal'),
+          as: 'gene',
+          where: unknownSignificanceGeneFilter,
+        },
+      ],
+    });
+  }
+
+  for (const row of unknownSignificanceResults) {
+    if (!(therapeuticAssociationResults.find(
+      (e) => {return e.ident === row.ident;},
+    )) && !(cancerRelevanceResultsFiltered.find(
+      (e) => {return e.ident === row.ident;},
+    ))) {
+      unknownSignificanceResultsFiltered.push(row);
+    }
+  }
+
+  return unknownSignificanceResultsFiltered;
+};
 
 // Routing for Alteration
 router.route('/')
   .get(async (req, res) => {
     // Get all small mutations for this report
     // TODO: Add comment on why cache was removed
+    const {query: {rapidTable}} = req;
 
     try {
-      const variantTables = Object.values(KB_PIVOT_MAPPING);
+      const variantTypes = Object.keys(KB_PIVOT_MAPPING);
       const variantsArray = [];
 
-      for (const table of variantTables) {
-        const variantsData = await db.models[table].scope('extended').findAll({
-          order: [['id', 'ASC']],
-          attributes: {
-            include: [[literal(`'${table}'`), 'variantType']],
-          },
-          where: {
-            reportId: req.report.id,
-          },
-          include: [
-            {
-              model: db.models.kbMatches,
-              attributes: ['ident', 'category'],
-            },
-          ],
-        });
+      for (const variantType of variantTypes) {
+        const tableName = KB_PIVOT_MAPPING[variantType];
 
-        variantsArray.push(variantsData);
+        if (rapidTable) {
+          variantsArray.push(await getRapidReportVariants(tableName, variantType, req.report.id, rapidTable));
+        } else {
+          variantsArray.push(await getVariants(tableName, variantType, req.report.id));
+        }
       }
 
       const results = variantsArray.flat(1);
