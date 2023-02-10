97daa61c9aa527ba6a490f3d87bdc995a4bd9f79
rpletz <rpletz@bcgsc.ca>
2023-02-10 14:31:27 -0800

    Run linter

--- migrations/20230208194424-DEVSU-1886-update-kbMatches-enum.js
+++ migrations/20230208194424-DEVSU-1886-update-kbMatches-enum.js
@@ -1,5 +1,5 @@
 module.exports = {
-  up: async (queryInterface, Sq) => {
+  up: async (queryInterface) => {
     queryInterface.sequelize.query('ALTER TYPE enum_reports_kb_matches_variant_type ADD VALUE \'msi\'');
   },
 
