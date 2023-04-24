847ec0749d74913a7e9f228b87fad555fd1f1e60
rpletz <rpletz@bcgsc.ca>
2023-04-24 10:50:44 -0700

    Remove and add new column for previously_reported

--- migrations/20230424173214-DEVSU-1830-fix-previouslyReported-type.js
+++ migrations/20230424173214-DEVSU-1830-fix-previouslyReported-type.js
@@ -3,7 +3,11 @@ const GERMLINE_VARIANTS = 'germline_small_mutations_variant';
 module.exports = {
   up: async (queryInterface, Sq) => {
     return queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.changeColumn(
+      await queryInterface.removeColumn(
+        GERMLINE_VARIANTS,
+        'previously_reported',
+      );
+      await queryInterface.addColumn(
         GERMLINE_VARIANTS,
         'previously_reported',
         {
