a55e362e933de8af586108a1e1428fb1a82ebe0e
Eleanor Lewis <elewis@bcgsc.ca>
2023-08-01 18:05:15 -0700

    reduce boilerplate in notifications

--- app/routes/notification/notification.js
+++ app/routes/notification/notification.js
@@ -4,227 +4,161 @@ const express = require('express');
 const db = require('../../models');
 const logger = require('../../log');
 
-const router = express.Router({ mergeParams: true });
+const {getUserProjects, isAdmin} = require('../../libs/helperFunctions');
+
+const router = express.Router({mergeParams: true});
+
+const pairs = {
+  user: db.models.user,
+  project: db.models.project,
+  template: db.models.template,
+  user_group: db.models.userGroup,
+};
+
+// for each entry in pairs, assumes the key-named value in
+// req.body is the ident, and gets the id of the corresponding object.
+router.use(async (req, res, next) => {
+  const operations = [];
+
+  for (const [key, value] of Object.entries(pairs)) {
+    if (req.body[key]) {
+      const operation = value.findOne({
+        where: {ident: req.body[key]},
+      }).then((obj) => {
+        if (!obj || !obj.id) {
+          logger.error(`Unable to find ${key} ${req.body[key]}`);
+          // Throw an error object that includes a status code
+          throw {message: `Unable to find ${key}`, statusCode: HTTP_STATUS.NOT_FOUND};
+        }
+        req.body[`${key}_id`] = obj.id;
+      });
+
+      operations.push(operation);
+    }
+  }
+
+  try {
+    await Promise.all(operations);
+    next();
+  } catch (error) {
+    logger.error(`Error while trying to find key: ${error.message}`);
+    // Use the status code from the error object, if it exists; otherwise, use 500
+    const statusCode = error.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR;
+    res.status(statusCode).json({
+      error: {message: `Error while trying to find key: ${error.message}`},
+    });
+  }
+});
 
 router.route('/')
   .get(async (req, res) => {
-    const userIdent = req.body.user;
-    const projectIdent = req.body.project;
-    const userGroupIdent = req.body.user_group;
-    const templateIdent = req.body.template;
-    let user;
-    if (userIdent) {
-      try {
-        user = await db.models.user.findOne({
-          where: { ident: req.body.user },
-        });
-        if (userIdent && !user) {
-          logger.error(`Unable to find user ${req.body.user}`);
-          return res.status(HTTP_STATUS.NOT_FOUND).json({ error: { message: 'Unable to find user' } });
-        }
-      } catch (error) {
-        logger.error(`Error while trying to find user  ${error}`);
-        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-          error: { message: 'Error while trying to find user' },
+    let projectAccess = false;
+
+    // if a specific project is named, check if the user has access to it
+    const userProjects = await getUserProjects(db.models.project, req.user);
+    const projectIdents = userProjects.map((project) => {return project.ident;});
+
+    if (req.body.project_id) {
+      if (isAdmin(req.user)) {
+        projectAccess = true;
+      }
+
+      if (projectIdents.includes(req.body.project)) {
+        projectAccess = true;
+      }
+
+      if (!projectAccess) {
+        logger.error(`user ${req.user.username} does not have access to project ${req.body.project}`);
+        return res.status(HTTP_STATUS.FORBIDDEN).json({
+          error: {message: `user ${req.user.username} does not have access to project ${req.body.project}`},
         });
       }
     }
-
-    let userGroup;
-    if (userGroupIdent) {
-      try {
-        userGroup = await db.models.userGroup.findOne({
-          where: { ident: req.body.user_group },
-        });
-        if (userGroupIdent && !userGroup) {
-          logger.error(`Unable to find user group ${req.body.user_group}`);
-          return res.status(HTTP_STATUS.NOT_FOUND).json({ error: { message: 'Unable to find user group' } });
-        }
-      } catch (error) {
-        logger.error(`Error while trying to find user group ${error}`);
-        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-          error: { message: 'Error while trying to find user group' },
-        });
-      }
-    }
-
-    let project;
-    if (projectIdent) {
-      try {
-        project = await db.models.project.findOne({
-          where: { ident: req.body.project },
-        });
-        if (projectIdent && !project) {
-          logger.error(`Unable to find project ${req.body.project}`);
-          return res.status(HTTP_STATUS.NOT_FOUND).json({ error: { message: 'Unable to find project' } });
-        }
-      } catch (error) {
-        logger.error(`Error while trying to find project ${error}`);
-        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-          error: { message: 'Error while trying to find project' },
-        });
-      }
-    }
-
-    let template;
-    if (templateIdent) {
-      try {
-        template = await db.models.template.findOne({
-          where: { ident: req.body.template },
-        });
-        if (templateIdent && !template) {
-          logger.error(`Unable to find template ${req.body.template}`);
-          return res.status(HTTP_STATUS.NOT_FOUND).json({ error: { message: 'Unable to find template' } });
-        }
-      } catch (error) {
-        logger.error(`Error while trying to find template ${error}`);
-        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-          error: { message: 'Error while trying to find template' },
-        });
-      }
-    }
-
     try {
       const whereClause = {
-        ...((user == null) ? {} : { user_id: user.id }),
-        ...((userGroup == null) ? {} : { user_group_id: userGroup.id }),
-        ...((template == null) ? {} : { template_id: template.id }),
-        ...((req.project == null) ? {} : { project_id: req.project.id }),
-      }
-      console.log(JSON.stringify(whereClause));
+        ...((req.body.user_id == null) ? {} : {user_id: req.body.user_id}),
+        ...((req.body.user_group_id == null) ? {} : {user_group_id: req.body.user_group_id}),
+        ...((req.body.template_id == null) ? {} : {template_id: req.body.template_id}),
+        ...((req.body.project_id == null) ? {} : {project_id: req.body.project_id}),
+      };
 
+      let results = await db.models.notification.scope('public').findAll({
+        where: whereClause,
+      });
+      if (!isAdmin(req.user)) {
+        results = results.filter((notif) => {return projectIdents.includes(notif.project.ident);});
+      }
       return res.json(results);
     } catch (error) {
-      console.log(`${error}`);
       logger.error(`${error}`);
       return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-        error: { message: 'Problem getting notification' },
+        error: {message: 'Problem getting notification'},
       });
     }
   })
   .post(async (req, res) => {
-    if (req.body.user && req.body.user_group) {
+    if (req.body.user_id && req.body.user_group_id) {
       return res.status(HTTP_STATUS.CONFLICT).json({
-        error: { message: 'Only one of user and user group should be specified' },
+        error: {message: 'Only one of user and user group should be specified'},
       });
     }
-    if (!req.body.user && !req.body.user_group) {
+    if (!req.body.user_id && !req.body.user_group_id) {
       return res.status(HTTP_STATUS.CONFLICT).json({
-        error: { message: 'Exactly one of user and user group should be specified' },
+        error: {message: 'Exactly one of user and user group should be specified'},
       });
     }
-    if (!req.body.project) {
+    if (!req.body.project_id) {
       return res.status(HTTP_STATUS.CONFLICT).json({
-        error: { message: 'Project must be specified' },
+        error: {message: 'Project must be specified'},
       });
     }
-    if (!req.body.template) {
+    if (!req.body.template_id) {
       return res.status(HTTP_STATUS.CONFLICT).json({
-        error: { message: 'Template must be specified' },
+        error: {message: 'Template must be specified'},
+      });
+    }
+    if (!req.body.event_type) {
+      return res.status(HTTP_STATUS.CONFLICT).json({
+        error: {message: 'Event type must be specified'},
       });
     }
 
-    let project;
-    try {
-      project = await db.models.project.findOne({
-        where: { ident: req.body.project },
-      });
-    } catch (error) {
-      logger.error(`Error while trying to find project ${error}`);
-      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-        error: { message: 'Error while trying to find project' },
-      });
-    }
-    if (!project) {
-      logger.error(`Unable to find project ${req.body.project}`);
-      return res.status(HTTP_STATUS.NOT_FOUND).json({ error: { message: 'Unable to find project' } });
-    }
-
-    let user;
-    let userGroup;
-    if (req.body.user) {
-      try {
-        user = await db.models.user.findOne({
-          where: { ident: req.body.user },
-        });
-      } catch (error) {
-        logger.error(`Error while trying to find user ${error}`);
-        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-          error: { message: 'Error while trying to find user' },
-        });
-      }
-      if (!user) {
-        logger.error(`Unable to find user ${req.body.user}`);
-        return res.status(HTTP_STATUS.NOT_FOUND).json({ error: { message: 'Unable to find user' } });
-      }
-
+    // check the given user, if any, is bound to the given project
+    if (req.body.user_id) {
       let projectBinding;
       try {
         projectBinding = await db.models.userProject.findOne({
-          where: { user_id: user.id, project_id: project.id },
+          where: {user_id: req.body.user_id, project_id: req.body.project_id},
         });
       } catch (error) {
         logger.error(`Error while trying to find user-project binding ${error}`);
         return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-          error: { message: 'Error while trying to find user-project binding' },
+          error: {message: 'Error while trying to find user-project binding'},
         });
       }
       if (!projectBinding) {
-        logger.error(`User ${user.ident} is not bound to project ${project.name} and can not receive notifications for it`);
+        logger.error(`User ${req.body.user} is not bound to project ${req.body.project} and can not receive notifications for it`);
         return res.status(HTTP_STATUS.CONFLICT).json({
-          error: { message: 'User is not bound to project and can not receive updates for it' },
+          error: {message: 'User is not bound to project and can not receive updates for it'},
         });
       }
-    } else {
-      try {
-        userGroup = await db.models.userGroup.findOne({
-          where: { ident: req.body.user_group },
-        });
-      } catch (error) {
-        logger.error(`Error while trying to find user group ${error}`);
-        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-          error: { message: 'Error while trying to find user group' },
-        });
-      }
-      if (!userGroup) {
-        logger.error(`Unable to find user group ${req.body.user_group}`);
-        return res.status(HTTP_STATUS.NOT_FOUND).json({ error: { message: 'Unable to find user group' } });
-      }
-    }
-
-    let template;
-    if (req.body.template) {
-      try {
-        template = await db.models.template.findOne({
-          where: { ident: req.body.template },
-        });
-      } catch (error) {
-        logger.error(`Error while trying to find template ${template}`);
-        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-          error: { message: 'Error while trying to find template' },
-        });
-      }
-    }
-    if (req.body.template && !template) {
-      logger.error(`Unable to find template ${req.body.template}`);
-      return res.status(HTTP_STATUS.NOT_FOUND).json({ error: { message: 'Unable to find template' } });
     }
 
     try {
       const newnotif = await db.models.notification.create({
-        projectId: project.id, userId: user ? user.id : null, userGroupId: userGroup ? userGroup.id : null, eventType: req.body.event_type, templateId: template.id,
+        projectId: req.body.project_id, userId: req.body.user_id ? req.body.user_id : null, userGroupId: req.body.user_group_id ? req.body.user_group_id : null, eventType: req.body.event_type, templateId: req.body.template_id,
       });
 
       // Load new notif with associations
       const result = await db.models.notification.scope('public').findOne({
-        where: { id: newnotif.id },
+        where: {id: newnotif.id},
       });
 
       return res.status(HTTP_STATUS.CREATED).json(result);
     } catch (error) {
       logger.error(`Error while creating notification ${error}`);
       return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-        error: { message: 'Error while creating notification ' },
+        error: {message: 'Error while creating notification '},
       });
     }
   })
@@ -232,20 +166,20 @@ router.route('/')
     let notification;
     try {
       notification = await db.models.notification.findOne({
-        where: { ident: req.body.ident },
+        where: {ident: req.body.ident},
         attributes: ['id', 'ident'],
       });
     } catch (error) {
       logger.error(`Error while trying to find notification ${error}`);
       return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-        error: { message: 'Error while trying to find notification' },
+        error: {message: 'Error while trying to find notification'},
       });
     }
 
     if (!notification) {
       logger.error(`Unable to find notification ${req.body.user}`);
       return res.status(HTTP_STATUS.NOT_FOUND).json({
-        error: { message: 'Unable to find the provided notification' },
+        error: {message: 'Unable to find the provided notification'},
       });
     }
 
@@ -255,7 +189,7 @@ router.route('/')
     } catch (error) {
       logger.error(`Error while deleting notification ${error}`);
       return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-        error: { message: 'Error while deleting notification' },
+        error: {message: 'Error while deleting notification'},
       });
     }
   });
