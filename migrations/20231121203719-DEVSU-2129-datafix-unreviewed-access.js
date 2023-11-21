7a70bdae396a861891983aad41c0c3cfccac8c57
rpletz <rpletz@bcgsc.ca>
2023-11-21 13:38:17 -0800

    Isolate queries in transaction

--- migrations/20231121203719-DEVSU-2129-datafix-unreviewed-access.js
+++ migrations/20231121203719-DEVSU-2129-datafix-unreviewed-access.js
@@ -2,43 +2,46 @@ const db = require('../app/models');
 
 module.exports = {
   up: async (queryInterface) => {
-    const user = await db.models.user.findOne({
-      where: {username: 'ipr-bamboo-admin'},
-      attributes: ['id', 'ident'],
-    });
-
-    const unreviewedAcessGroup = await db.models.userGroup.findOrCreate({
-      where: {
-        name: 'Unreviewed Access',
-        owner_id: user.id,
-      },
-    });
-
-    const userUpdateList = await queryInterface.sequelize.query(
-      // eslint-disable-next-line no-multi-str
-      'select distinct u.id from users u\
-        join user_group_members ugm on (u.id = ugm.user_id)\
-        join user_groups ug on (ugm.group_id = ug.id)\
-        where ug.name = \'Bioinformatician\' or ug.name = \'Report Manager\'',
-      {
-        type: queryInterface.sequelize.QueryTypes.SELECT,
-      },
-    );
-
-    const bulkInsertList = [];
-
-    for (const element of userUpdateList) {
-      bulkInsertList.push({
-        user_id: element.id,
-        group_id: unreviewedAcessGroup[0].id,
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      const user = await db.models.user.findOne({
+        where: {username: 'ipr-bamboo-admin'},
+        attributes: ['id', 'ident'],
       });
-    }
 
-    const insertReturn = await db.models.userGroupMember.bulkCreate(
-      bulkInsertList,
-    );
+      const unreviewedAcessGroup = await db.models.userGroup.findOrCreate({
+        where: {
+          name: 'Unreviewed Access',
+          owner_id: user.id,
+        },
+      }, {transaction});
 
-    console.log(`${insertReturn.length} users updated`);
+      const userUpdateList = await queryInterface.sequelize.query(
+        // eslint-disable-next-line no-multi-str
+        'select distinct u.id from users u\
+          join user_group_members ugm on (u.id = ugm.user_id)\
+          join user_groups ug on (ugm.group_id = ug.id)\
+          where ug.name = \'Bioinformatician\' or ug.name = \'Report Manager\'',
+        {
+          type: queryInterface.sequelize.QueryTypes.SELECT,
+        },
+      );
+
+      const bulkInsertList = [];
+
+      for (const element of userUpdateList) {
+        bulkInsertList.push({
+          user_id: element.id,
+          group_id: unreviewedAcessGroup[0].id,
+        });
+      }
+
+      const insertReturn = await db.models.userGroupMember.bulkCreate(
+        bulkInsertList,
+        {transaction},
+      );
+
+      console.log(`${insertReturn.length} users updated`);
+    });
   },
 
   down: async () => {
