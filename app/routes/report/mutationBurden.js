5b2362597f081bd5a3a7ba1055677d99a1764baf
Bill Nguyen <158210220+bnguyen-bcgsc@users.noreply.github.com>
2024-03-05 15:20:31 -0800

    DEVSU-2209 Update MutationBurden Route
    - Add 'for report' back to logger error in MutationBurden routes file line 113

--- app/routes/report/mutationBurden.js
+++ app/routes/report/mutationBurden.js
@@ -110,7 +110,7 @@ router.route('/')
 
       return res.json(results);
     } catch (error) {
-      logger.error(`Unable to lookup mutation burden ${req.report.ident} error: ${error}`);
+      logger.error(`Unable to lookup mutation burden for report ${req.report.ident} error: ${error}`);
       return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
         error: {message: `Unable to lookup the mutation burden for ${req.report.ident}`},
       });
