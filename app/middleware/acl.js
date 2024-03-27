d3a5a457ff4102d1503b525b4560a95e1404a884
rpletz <rpletz@bcgsc.ca>
2024-03-27 14:44:14 -0700

    Allow users to edit themselves

--- app/middleware/acl.js
+++ app/middleware/acl.js
@@ -146,7 +146,9 @@ module.exports = async (req, res, next) => {
       });
     }
 
-    if ((UPDATE_METHODS.includes(req.method) && !hasMasterAccess(req.user))) {
+    // Allow users to edit themselves for allowNotifications field
+    console.log(req.originalUrl.includes('/api/user'));
+    if ((UPDATE_METHODS.includes(req.method) && !hasMasterAccess(req.user)) && !req.originalUrl.includes('/api/user')) {
       logger.error(`User: ${req.user.username} is trying to make a ${req.method} request to ${req.originalUrl}`);
       return res.status(FORBIDDEN).json({
         error: {message: 'You do not have the correct permissions to access this'},
