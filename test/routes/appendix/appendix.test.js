1143bbbff627d00535e93439fc7d1169425f0e42
sshugsc <sshu@bcgsc.ca>
2024-05-13 12:14:22 -0700

    add unit test to appendix endpoint

---
+++ test/routes/appendix/appendix.test.js
@@ -0,0 +1,218 @@
+const HTTP_STATUS = require('http-status-codes');
+const supertest = require('supertest');
+const getPort = require('get-port');
+const db = require('../../../app/models');
+
+const CONFIG = require('../../../app/config');
+const {listen} = require('../../../app');
+
+CONFIG.set('env', 'test');
+const {username, password} = CONFIG.get('testing');
+
+let server;
+let request;
+
+const CREATE_DATA = {
+  text: '<h3>Title</h3><p>Test text</p>',
+};
+
+const UPDATE_DATA = {
+  text: '<h2>Updated Title</h2><p>Updated test text</p>',
+};
+
+const templateAppendixProperties = [
+  'ident', 'createdAt', 'updatedAt', 'text',
+];
+
+const checkTemplateAppendix = (appendixObject) => {
+  templateAppendixProperties.forEach((element) => {
+    expect(appendixObject).toHaveProperty(element);
+  });
+  expect(appendixObject).toEqual(expect.not.objectContaining({
+    id: expect.any(Number),
+    templateId: expect.any(Number),
+    updatedBy: expect.any(Number),
+    deletedAt: expect.any(String),
+  }));
+};
+
+// Start API
+beforeAll(async () => {
+  const port = await getPort({port: CONFIG.get('web:port')});
+  server = await listen(port);
+  request = supertest(server);
+});
+
+describe('/appendix', () => {
+  let template;
+  let project;
+  let body;
+
+  beforeAll(async () => {
+    // create a template to be used in tests
+    template = await db.models.template.create({
+      name: 'Test Template',
+      organization: 'Test Org',
+      sections: [
+        'microbial',
+        'msi',
+        'small-mutation',
+      ],
+    });
+
+    // Create project
+    project = await db.models.project.create({name: 'test-appendix-project'});
+
+    body = {
+      templateId: template.id,
+      projectId: project.id,
+    };
+  });
+
+  describe('GET', () => {
+    let getTestAppendix;
+
+    beforeEach(async () => {
+      // create a template to be used in tests
+      getTestAppendix = await db.models.templateAppendix.create({
+        ...CREATE_DATA,
+        templateId: template.id,
+        projectId: project.id,
+      });
+    });
+
+    afterEach(async () => {
+      return getTestAppendix.destroy({force: true});
+    });
+
+    test('/ - 200 Success', async () => {
+      const res = await request
+        .get('/api/appendix')
+        .auth(username, password)
+        .type('json')
+        .send(body)
+        .expect(HTTP_STATUS.OK);
+
+      expect(res.body).not.toBeNull();
+      expect(res.body).toEqual(expect.objectContaining(CREATE_DATA));
+    });
+  });
+
+  describe('PUT', () => {
+    let putTestAppendix;
+
+    beforeEach(async () => {
+      putTestAppendix = await db.models.templateAppendix.create({
+        ...CREATE_DATA,
+        templateId: template.id,
+        projectId: project.id,
+      });
+    });
+
+    afterEach(async () => {
+      return db.models.templateAppendix.destroy({
+        where: {ident: putTestAppendix.ident},
+        force: true,
+      });
+    });
+
+    test('/ - 200 Success', async () => {
+      const res = await request
+        .put('/api/appendix')
+        .auth(username, password)
+        .type('json')
+        .send({...UPDATE_DATA, ...body})
+        .expect(HTTP_STATUS.OK);
+
+      expect(res.body).not.toBeNull();
+      checkTemplateAppendix(res.body);
+      expect(res.body).toEqual(expect.objectContaining(UPDATE_DATA));
+    });
+
+    test('/ - 500 Internal Server Error', async () => {
+      // First soft-delete record
+      await putTestAppendix.destroy();
+
+      await request
+        .put('/api/appendix')
+        .send({
+          text: '<h3>Updated Title</h3><p>Updated text</p>',
+          projectId: 0,
+        })
+        .auth(username, password)
+        .type('json')
+        .expect(HTTP_STATUS.INTERNAL_SERVER_ERROR);
+    });
+
+    test('/ - 400 Bad Request - Invalid type', async () => {
+      await request
+        .put('/api/appendix')
+        .send({
+          text: {
+            data: 'TEST DATA',
+          },
+          projectId: 0,
+        })
+        .auth(username, password)
+        .type('json')
+        .expect(HTTP_STATUS.BAD_REQUEST);
+    });
+  });
+
+  describe('DELETE', () => {
+    let deleteTestAppendix;
+
+    beforeEach(async () => {
+      deleteTestAppendix = await db.models.templateAppendix.create({
+        ...CREATE_DATA,
+        templateId: template.id,
+        projectId: project.id,
+      });
+    });
+
+    afterEach(async () => {
+      return deleteTestAppendix.destroy({force: true});
+    });
+
+    test('/ - 204 No Content', async () => {
+      await request
+        .delete('/api/appendix')
+        .send(body)
+        .auth(username, password)
+        .type('json')
+        .expect(HTTP_STATUS.NO_CONTENT);
+
+      // Verify that the record was soft deleted
+      const result = await db.models.templateAppendix.findOne({
+        where: {id: deleteTestAppendix.id},
+        paranoid: false,
+      });
+      expect(result).not.toBeNull();
+      expect(result.deletedAt).not.toBeNull();
+    });
+
+    test('/ - 500 Internal Server Error', async () => {
+      // First soft-delete record
+      await deleteTestAppendix.destroy();
+
+      await request
+        .delete('/api/appendix')
+        .send({projectId: 0})
+        .auth(username, password)
+        .type('json')
+        .expect(HTTP_STATUS.INTERNAL_SERVER_ERROR);
+    });
+  });
+
+  // delete template
+  afterAll(async () => {
+    // delete newly created template and all of it's components
+    await template.destroy({force: true});
+    return project.destroy({force: true});
+  });
+});
+
+afterAll(async () => {
+  global.gc && global.gc();
+  await server.close();
+});
