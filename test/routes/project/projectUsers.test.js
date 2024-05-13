5797ff0dea09b5d20246695d0cc2d4f18638094e
Eleanor Lewis <elewis@bcgsc.ca>
2024-05-13 16:30:10 -0700

    remove manager/bioinf user setup from projectUser

--- test/routes/project/projectUsers.test.js
+++ test/routes/project/projectUsers.test.js
@@ -36,9 +36,7 @@ const checkProjectUsers = (users) => {
 let server;
 let request;
 let managerUser;
-let managerGroup;
 let bioinformaticianUser;
-let bioinformaticianGroup;
 
 // Start API
 beforeAll(async () => {
@@ -49,45 +47,9 @@ beforeAll(async () => {
   managerUser = await db.models.user.findOne({
     where: {username: managerUsername},
   });
-  if (!managerUser) {
-    managerUser = await db.models.user.create({
-      ident: uuidv4(),
-      username: 'ipr-bamboo-manager',
-      firstName: 'ipr-bamboo-manager',
-      lastName: 'ipr-bamboo-manager',
-      email: 'dat@bcgsc.ca',
-    });
-  }
-  managerGroup = await db.models.userGroup.findOne({
-    where: {name: 'manager'},
-  });
-  await db.models.userGroupMember.findOrCreate({
-    where: {user_id: managerUser.id, group_id: managerGroup.id},
-  });
 
   bioinformaticianUser = await db.models.user.findOne({
     where: {username: bioinformaticianUsername},
-  });
-  if (!bioinformaticianUser) {
-    bioinformaticianUser = await db.models.user.create({
-      ident: uuidv4(),
-      username: 'ipr-bamboo-bioinformatician',
-      firstName: 'ipr-bamboo-bioinformatician',
-      lastName: 'ipr-bamboo-bioinformatician',
-      email: 'dat@bcgsc.ca',
-    });
-  }
-  bioinformaticianGroup = await db.models.userGroup.findOne({
-    where: {name: 'Bioinformatician'},
-  });
-  if (!bioinformaticianGroup) {
-    bioinformaticianGroup = await db.models.userGroup.create({
-      ident: uuidv4(),
-      name: 'Bioinformatician',
-    });
-  }
-  await db.models.userGroupMember.findOrCreate({
-    where: {user_id: bioinformaticianUser.id, group_id: bioinformaticianGroup.id},
   });
 });
 
