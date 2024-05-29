53362a8469698f375f4b963f2e5a990a41ed083d
rpletz <rpletz@bcgsc.ca>
2024-05-29 10:25:31 -0700

    Lint

--- app/libs/createReport.js
+++ app/libs/createReport.js
@@ -204,7 +204,7 @@ const createReportVariantSections = async (report, content, transaction) => {
  */
 const createReportSections = async (report, content, transaction) => {
   // add images
-  const promises = (content.images || []).map(async ({path: imagePath, key, caption, title, width, height, category}) => {
+  const promises = (content.images || []).map(async ({path: imagePath, key, caption, title, category}) => {
     return uploadReportImage(report.id, key, imagePath, {
       filename: path.basename(imagePath), caption, title, transaction, category,
     });
