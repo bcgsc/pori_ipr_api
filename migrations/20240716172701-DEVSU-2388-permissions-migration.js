08132a32d127560d136d6ed45bc4f951a1660057
Eleanor Lewis <elewis@bcgsc.ca>
2024-07-16 15:39:52 -0700

    add permissions migration

---
+++ migrations/20240716172701-DEVSU-2388-permissions-migration.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const { v4: uuidv4 } = require('uuid');
+
+const germline = 'Germline Access'
+const nonprod = 'non-production access'
+const unreviewed = 'Unreviewed Access'
+const createAccess = 'create reports access'
+const reportAssign = 'report assignment access'
+
+const groupMappings = {
+  'Bioinformatician': [germline, nonprod, unreviewed, createAccess],
+  'Analyst': [germline, nonprod, unreviewed, createAccess],
+  'Report Manager': [reportAssign, germline, nonprod, unreviewed, createAccess],
+  'Collaborator': [],
+  'Clinician': [],
+  'External Analyst': [],
+  'Projects': [germline, nonprod, unreviewed],
+  'Pipelines': [germline, nonprod, unreviewed],
+  'Biopsies': [germline, nonprod, unreviewed],
+  'LIMS': [germline, nonprod, unreviewed],
+  'Research': [germline, nonprod, unreviewed],
+  'BioApps': [germline, nonprod, unreviewed],
+}
+
+module.exports = {
+  up: async (queryInterface) => {
+    return queryInterface.sequelize.transaction(async () => {
+
+      for (groupMapping in groupMappings) {
+        for (let key in groupMappings) {
+          let startingGroupName = key;
+          for (let targetGroupName of groupMappings[key]) {
+            let groupMembers = await queryInterface.sequelize.query(
+              // eslint-disable-next-line no-multi-str
+              `insert into user_group_members
+              (created_at, updated_at, user_id, group_id)
+              select now(), now(), user_id, target_group_id
+              from
+                (select distinct user_id
+                  from user_group_members
+                  join user_groups on group_id = user_groups.id
+                  where user_group_members.deleted_at is null
+                  and user_groups.name = '${startingGroupName}' ) starting_group_users
+              cross join (
+                  select id as target_group_id from user_groups
+                  where name = '${targetGroupName}'
+                  and deleted_at is null
+              ) t
+              where user_id not in (
+                select distinct user_id
+                  from user_group_members
+                  join user_groups on group_id = user_groups.id
+                  where user_group_members.deleted_at is null
+                  and user_groups.name = '${targetGroupName}'
+              );
+              `,
+              {
+                type: queryInterface.sequelize.QueryTypes.SELECT,
+              },
+            );
+          }
+        }
+      }
+    })
+  },
+
+  down: async () => {
+    throw new Error('Not Implemented!');
+  },
+};
