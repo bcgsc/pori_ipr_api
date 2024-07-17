752f1082f336ae9e2a7e48127a06902de207e782
bnguyen-bcgsc <bnguyen@bcgsc.ca>
2024-07-17 15:33:40 -0700

    - DEVSU-2386 - Update report test

--- test/routes/report/report.test.js
+++ test/routes/report/report.test.js
@@ -471,7 +471,7 @@ describe('/reports/{REPORTID}', () => {
 
     test('/ - key variant - 200 Success', async () => {
       const res = await request
-        .get(`/api/reports?keyVariant=${KEYVARIANT}&&matchingThreshold=1`)
+        .get(`/api/reports?keyVariant=${KEYVARIANT}&&matchingThreshold=0.8`)
         .auth(username, password)
         .type('json')
         .expect(HTTP_STATUS.OK);
