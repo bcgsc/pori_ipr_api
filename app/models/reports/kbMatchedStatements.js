1e9e44b7497eab82594036000721a20019256146
Bill Nguyen <158210220+bnguyen-bcgsc@users.noreply.github.com>
2024-10-01 16:11:56 -0700

    - Split Kb Matches table to Kb Matched Statements table that stores statements-related columns and new Kb Matches table that stores only variant-related columns - Create new Kb Match Join table for many-to-many relationship between kb statements and kb variants - Declare many-to-many association - Create new migration that performs data manipulations mentioned above
    Co-authored-by: Lara Matiello Pletz <Nithriel@users.noreply.github.com>

---
+++ app/models/reports/kbMatchedStatements.js
@@ -0,0 +1,177 @@
+// const {KB_PIVOT_COLUMN, KB_PIVOT_MAPPING} = require('../../constants');
+const {DEFAULT_COLUMNS, DEFAULT_REPORT_OPTIONS} = require('../base');
+
+module.exports = (sequelize, Sq) => {
+  const KbMatchedStatements = sequelize.define('kbMatchedStatements', {
+    ...DEFAULT_COLUMNS,
+    reportId: {
+      name: 'reportId',
+      field: 'report_id',
+      type: Sq.INTEGER,
+      references: {
+        model: 'reports',
+        key: 'id',
+      },
+    },
+    category: {
+      type: Sq.ENUM(
+        'therapeutic',
+        'prognostic',
+        'diagnostic',
+        'biological',
+        'unknown',
+        'novel',
+        'pharmacogenomic',
+        'cancer predisposition',
+      ),
+      allowNull: false,
+    },
+    approvedTherapy: {
+      name: 'approvedTherapy',
+      field: 'approved_therapy',
+      type: Sq.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+    disease: {
+      type: Sq.TEXT,
+    },
+    relevance: {
+      type: Sq.TEXT,
+    },
+    context: {
+      type: Sq.TEXT,
+    },
+    status: {
+      type: Sq.TEXT,
+    },
+    reference: {
+      type: Sq.TEXT,
+    },
+    sample: {
+      type: Sq.TEXT,
+    },
+    evidenceLevel: {
+      name: 'evidenceLevel',
+      field: 'evidence_level',
+      type: Sq.TEXT,
+    },
+    iprEvidenceLevel: {
+      name: 'iprEvidenceLevel',
+      field: 'ipr_evidence_level',
+      type: Sq.TEXT,
+      defaultValue: null,
+    },
+    matchedCancer: {
+      name: 'matchedCancer',
+      field: 'matched_cancer',
+      type: Sq.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+    pmidRef: {
+      name: 'pmidRef',
+      field: 'pmid_ref',
+      type: Sq.TEXT,
+    },
+    kbStatementId: {
+      name: 'kbStatementId',
+      field: 'kb_statement_id',
+      type: Sq.TEXT,
+    },
+    kbData: {
+      name: 'kbData',
+      field: 'kb_data',
+      type: Sq.JSONB,
+      jsonSchema: {
+        schema: {
+          type: 'object',
+          example: {inferred: true},
+        },
+      },
+    },
+    externalSource: {
+      name: 'externalSource',
+      field: 'external_source',
+      type: Sq.TEXT,
+    },
+    externalStatementId: {
+      name: 'externalStatementId',
+      field: 'external_statement_id',
+      type: Sq.TEXT,
+    },
+    reviewStatus: {
+      name: 'reviewStatus',
+      field: 'review_status',
+      type: Sq.TEXT,
+    },
+  }, {
+    ...DEFAULT_REPORT_OPTIONS,
+    tableName: 'reports_kb_matched_statements',
+    // scopes: {
+    //   public: {
+    //     attributes: {exclude: ['id', 'reportId', 'deletedAt', 'updatedBy']},
+    //     include: Object.values(KB_PIVOT_MAPPING).map((modelName) => {
+    //       return {model: sequelize.models[modelName].scope('public'), as: modelName};
+    //     }),
+    //   },
+    //   extended: {
+    //     attributes: {exclude: ['id', 'reportId', 'variantId', 'deletedAt', 'updatedBy']},
+    //     include: Object.values(KB_PIVOT_MAPPING).map((modelName) => {
+    //       return {model: sequelize.models[modelName].scope('extended'), as: modelName};
+    //     }),
+    //   },
+    // },
+    // hooks: {
+    //   ...DEFAULT_REPORT_OPTIONS.hooks,
+    //   afterFind: (findResult) => {
+    //     if (!Array.isArray(findResult)) {
+    //       findResult = [findResult];
+    //     }
+    //     for (const instance of findResult) {
+    //       const {[KB_PIVOT_COLUMN]: currentPivotValue} = instance;
+
+    //       for (const pivotType of Object.keys(KB_PIVOT_MAPPING)) {
+    //         const modelName = KB_PIVOT_MAPPING[pivotType];
+
+    //         if (instance[modelName] !== undefined) {
+    //           if (pivotType === currentPivotValue) {
+    //             instance.variant = instance[modelName];
+    //             instance.dataValues.variant = instance[modelName].dataValues;
+    //           }
+    //           // To prevent mistakes:
+    //           delete instance[modelName];
+    //           delete instance.dataValues[modelName];
+    //         }
+    //       }
+    //     }
+    //   },
+    // },
+    modelName: 'kbMatchedStatements',
+    sequelize,
+  });
+
+  //   // set instance methods
+  //   KbMatchedStatements.prototype.view = function (scope) {
+  //     if (scope === 'public') {
+  //       const {
+  //         id, reportId, variantId, deletedAt, updatedBy, ...publicView
+  //       } = this.dataValues;
+  //       return publicView;
+  //     }
+  //     return this;
+  //   };
+
+  //   KbMatchedStatements.prototype.getVariant = function (options) {
+  //     const targetModel = this.experimentalVariantType;
+
+  //     if (!targetModel) {
+  //       return Promise.resolve(null);
+  //     }
+
+  //     const mixinMethodName = `get${targetModel[0].toUpperCase()}${targetModel.slice(1)}`;
+  //     return this[mixinMethodName](options);
+  //   };
+
+  return KbMatchedStatements;
+};
