73eaf56d260c19768a71d0c3f72a0f14d817e01c
bnguyen-bcgsc <bnguyen@bcgsc.ca>
2025-04-08 14:43:50 -0700

    - Test migratedb shell script with github yml test

--- migrationTools/migratedb.sh
+++ migrationTools/migratedb.sh
@@ -5,7 +5,9 @@ export IPR_SERVICE_USER=$IPR_SERVICE_USER
 export IPR_SERVICE_PASS=$IPR_SERVICE_PASS
 export IPR_DATABASE_SERVER=$IPR_DATABASE_SERVER
 export IPR_DATABASE_NAME=$IPR_DATABASE_NAME
-echo "Migrating against $IPR_DATABASE_NAME"
+echo "Migrating against: $IPR_DATABASE_NAME"
+echo "Service user: $IPR_SERVICE_USER"
+echo "Server: $IPR_DATABASE_SERVER"
 
 parent_folder="migrations"
 
