06f1a2e55904e84a07a944efb6f1d2a02bd593f9
bnguyen-bcgsc <bnguyen@bcgsc.ca>
2025-04-08 13:57:25 -0700

    - DEVSU-2611 - Sort migration files by releases - Update migratedb shell script to loop through all migrations subdirectories and run all migrations - Update migratedb script env variables handling - Update gitignore to ignore db dump file

--- migrationTools/migratedb.sh
+++ migrationTools/migratedb.sh
@@ -1,10 +1,15 @@
-# Run the current migrations against the test DB
+# Run the current migrations against specified DB
+export IPR_SERVICE_USER=$IPR_SERVICE_USER
+export IPR_SERVICE_PASS=$IPR_SERVICE_PASS
+export IPR_DATABASE_SERVER=$IPR_DATABASE_SERVER
+export IPR_DATABASE_NAME=$IPR_DATABASE_NAME
+echo "Migrating against $IPR_DATABASE_NAME"
 
-export PGPASSWORD=$IPR_SERVICE_PASS
-TEMP_DB_NAME=$( git branch | grep '*' | cut -f 2 -d/ )
-TEMP_DB_NAME="${TEMP_DB_NAME}"
+parent_folder="migrations"
 
-SOURCE_DB=ipr-sync-dev
-echo $TEMP_DB_NAME
+mapfile -t subdirs < <(find "$parent_folder" -mindepth 1 -maxdepth 1 -type d)
 
-npx sequelize-cli db:migrate --url postgres://ipr_service@iprdevdb:5432/$TEMP_DB_NAME
+for dir in "${subdirs[@]}"; do
+  echo "$dir"
+  npx sequelize-cli db:migrate --migrations-path "$dir" --url "postgres://$IPR_SERVICE_USER:$IPR_SERVICE_PASS@$IPR_DATABASE_SERVER/$IPR_DATABASE_NAME"
+done
--- migrations/v8.0.0/20240530225858-DEVSU-2324-add-variant-text-table.js
+++ migrations/v8.0.0/20240530225858-DEVSU-2324-add-variant-text-table.js
@@ -1,5 +1,5 @@
 const TABLE = 'variant_texts';
-const {DEFAULT_COLUMNS} = require('../app/models/base');
+const {DEFAULT_COLUMNS} = require('../../app/models/base');
 
 module.exports = {
   up: (queryInterface, Sq) => {
