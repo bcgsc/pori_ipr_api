bbdbf94a0a80418a45d6ebdf0f0869deb926fec6
bnguyen-bcgsc <bnguyen@bcgsc.ca>
2025-07-28 14:29:46 -0700

    - Code lint

--- app/routes/report/report.js
+++ app/routes/report/report.js
@@ -26,7 +26,6 @@ const {REPORT_UPDATE_BASE_URI, NOTIFICATION_EVENT} = require('../../constants');
 const {BASE_EXCLUDE} = require('../../schemas/exclude');
 
 const reportGetSchema = require('../../schemas/report/retrieve/reportGetQueryParamSchema');
-const { orderBy } = require('lodash');
 // Generate schema's
 const reportUploadSchema = require('../../schemas/report/reportUpload')(true);
 
@@ -149,34 +148,34 @@ router.route('/:report/state-history')
         where: {ident: req.params.report},
         attributes: ['ident', 'updatedAt', 'state'],
         order: [
-          ['updatedAt', 'ASC']
+          ['updatedAt', 'ASC'],
         ],
         paranoid: false,
       });
-    } catch {
-      logger.error(`Error while trying to get report: ${ident} ${error}`);
+    } catch (error) {
+      logger.error(`Error while trying to get report: ${req.params.report} ${error}`);
       return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: {message: 'Error while trying to get report'}});
     }
 
     // Nothing found?
     if (reportHistory.length === 0) {
-      logger.error(`Unable to find the requested report ${ident}`);
+      logger.error(`Unable to find the requested report ${req.params.report}`);
       return res.status(HTTP_STATUS.NOT_FOUND).json({error: {message: 'Unable to find the requested report'}});
     }
 
     if (!hasAccessToUnreviewedReports(req.user) && (reportHistory[reportHistory.length - 1].state !== 'reviewed' && reportHistory[reportHistory.length - 1].state !== 'completed')) {
-      logger.error(`User does not have unreviewed access to ${ident}`);
+      logger.error(`User does not have unreviewed access to ${req.params.report}`);
       return res.status(HTTP_STATUS.FORBIDDEN).json({error: {message: 'User does not have access to Unreviewed reports'}});
     }
 
     if (!hasAccessToNonProdReports(req.user) && reportHistory[reportHistory.length - 1].state === 'nonproduction') {
-      logger.error(`User does not have non-production access to ${ident}`);
+      logger.error(`User does not have non-production access to ${req.params.report}`);
       return res.status(HTTP_STATUS.FORBIDDEN).json({error: {message: 'User does not have access to Non-Production reports'}});
     }
 
-    let filteredReportHistory = [reportHistory[0]];
-    for (let i = 1; i < reportHistory.length ; i++) {
-      if (reportHistory[i].state !== reportHistory[i-1].state) {
+    const filteredReportHistory = [reportHistory[0]];
+    for (let i = 1; i < reportHistory.length; i++) {
+      if (reportHistory[i].state !== reportHistory[i - 1].state) {
         filteredReportHistory.push(reportHistory[i]);
       }
     }
