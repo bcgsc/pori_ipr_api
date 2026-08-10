8de2bc258b907a5109359e9c4d0c38437a9524a2
sshugsc <sshu@bcgsc.ca>
2026-08-10 11:23:59 -0700

    move therapeuticTarget.js into a folder

--- app/routes/therapeuticTargets/index.js
+++ app/routes/therapeuticTargets/index.js
@@ -1,8 +1,8 @@
 const HTTP_STATUS = require('http-status-codes');
 const express = require('express');
 
-const db = require('../models');
-const logger = require('../log');
+const db = require('../../models');
+const logger = require('../../log');
 
 const router = express.Router({mergeParams: true});
 
