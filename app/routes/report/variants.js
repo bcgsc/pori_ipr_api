6c3f41a5c725d2cdb98dabcedf93b337d5c2b147
bnguyen-bcgsc <bnguyen@bcgsc.ca>
2026-08-25 15:53:09 -0700

    - DEVSU-3003 - Fix bug where variants with noTable tag gets included in table of unknown significance variants instead - Update unit test

--- app/routes/report/variants.js
+++ app/routes/report/variants.js
@@ -116,12 +116,18 @@ const getRapidReportVariants = async (tableName, variantType, reportId, rapidTab
   const unknownSignificanceFromAnnotation = [];
   const unknownSignificanceFromGeneProperty = [];
   const doNotReport = [];
+  const doNotReportIdents = new Set();
 
   const tumourSuppressorLofVariants = {};
 
   // separate out variants that qualify based on gene properties alone, if filtering for table 3
   if (rapidTable === 'unknownSignificance') {
     for (const variant of allKbMatches) {
+      if (variant?.observedVariantAnnotation?.annotations?.rapidReportTableTag === 'noTable') {
+        doNotReport.push(variant);
+        doNotReportIdents.add(variant.ident);
+        continue;
+      }
       if (variant.gene.cancerGeneListMatch
           || variant.gene.oncogene
           || variant.gene.tumourSuppressor) {
@@ -180,7 +186,9 @@ const getRapidReportVariants = async (tableName, variantType, reportId, rapidTab
         } else if (tableTag === 'unknownSignificance') {
           unknownSignificanceFromAnnotation.push(variant);
         } else if (tableTag === 'noTable') {
+          console.log(`Variant ${variant.ident} is tagged as noTable, will not be reported`);
           doNotReport.push(variant);
+          doNotReportIdents.add(variant.ident);
         }
       }
     }
@@ -356,7 +364,7 @@ const getRapidReportVariants = async (tableName, variantType, reportId, rapidTab
   const existingVariants = new Set(unknownSignificanceResults.map((variant) => {return variant.ident;}));
 
   unknownSignificanceFromGeneProperty.forEach((variant) => {
-    if (!existingVariants.has(variant.ident)) {
+    if (!existingVariants.has(variant.ident) && !doNotReportIdents.has(variant.ident)) {
       unknownSignificanceResults.push(variant);
       existingVariants.add(variant.ident);
     }
@@ -377,7 +385,9 @@ const getRapidReportVariants = async (tableName, variantType, reportId, rapidTab
   // reason there may be to exclude them
   unknownSignificanceResultsFiltered.push(...unknownSignificanceFromAnnotation);
 
-  return unknownSignificanceResultsFiltered;
+  return unknownSignificanceResultsFiltered.filter((variant) => {
+    return !doNotReportIdents.has(variant.ident);
+  });
 };
 
 const updateKbDataSummaryTableTag = (kbData, rapidTable, variantType, variantIdent) => {
